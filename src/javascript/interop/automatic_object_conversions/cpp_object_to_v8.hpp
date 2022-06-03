55f68cc8b6af2367f39071eb051a852abf06ebb1
Sam <samuelgardner101@gmail.com>
2022-06-03 11:35:29 +0100

    updated folder name

---
+++ src/javascript/interop/automatic_object_conversions/cpp_object_to_v8.hpp
@@ -0,0 +1,6 @@
+#ifndef SBROWSER2_CPP_OBJECT_TO_V8_HPP
+#define SBROWSER2_CPP_OBJECT_TO_V8_HPP
+
+namespace javascript::interop {template <typename T> auto cpp_object_to_v8()}
+
+#endif //SBROWSER2_CPP_OBJECT_TO_V8_HPP
