84fbaf27ba5e4266b454a144de55bc85fd633d00
Sam <samuelgardner101@gmail.com>
2022-06-04 15:20:46 +0100

    implemented the method

--- src/javascript/interop/automatic_object_conversions/cpp_object_to_v8.hpp
+++ src/javascript/interop/automatic_object_conversions/cpp_object_to_v8.hpp
@@ -1,6 +1,21 @@
 #ifndef SBROWSER2_CPP_OBJECT_TO_V8_HPP
 #define SBROWSER2_CPP_OBJECT_TO_V8_HPP
 
-namespace javascript::interop {template <typename T> auto cpp_object_to_v8()}
+#include <type_traits>
+#include <v8-isolate.h>
+#include <v8pp/class.hpp>
+
+#include <dom_object.hpp>
+
+namespace javascript::interop {template <typename T> auto cpp_object_to_v8(v8::Isolate* isolate) requires std::is_base_of_v<T, webapis::dom_object>;}
+
+
+template <typename T>
+auto javascript::interop::cpp_object_to_v8(v8::Isolate* isolate) requires std::is_base_of_v<T, webapis::dom_object>
+{
+    // get the v8 conversion of the class by calling the conversion method on a temporary object of type T
+    return T{}.v8(isolate).template to<v8pp::class_<T>>();
+}
+
 
 #endif //SBROWSER2_CPP_OBJECT_TO_V8_HPP
