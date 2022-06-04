f4a4dc146b0478cc302cf5fc112d0fe00ed444df
Sam <samuelgardner101@gmail.com>
2022-06-04 15:28:39 +0100

    testing other libraries

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
-#include <ext/number.hpp>
+#include <javascript/environment/environment_initialization.hpp>
+
+//#include <cppfs/FileHandle.h>
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
 
 
 int main(int argc, char** argv)
 {
-    ext::number<int> number_a {1};
-    ext::number<int> number_b {2};
-    ext::number<int> number_c {3};
+    /*
+    javascript::environment::initialize_v8_engine(argv);
+    auto* isolate = javascript::environment::create_new_isolate();
+    auto& context = javascript::environment::create_new_context(isolate, javascript::environment::module_t::WINDOW);
 
-    std::cout << (number_a + number_b) << std::endl;
+    // auto init_script = cppfs::FileHandle{}.open("src/javascript/initialization_scripts/module_to_global.js").readFile();
 
-    return 0;
+    javascript::environment::execute(isolate, context, "let a = new Window.A(5);"
+                                                       "let b = new Window.A(1);");
+
+    javascript::environment::execute(isolate, context, "a.a_ptr = b;"
+                                                       "b.a_int = 100;"
+                                                       "a.a_ptr.doubleA();");
+    javascript::environment::dispose_isolate(isolate);
+    javascript::environment::dispose_v8_engine();
+     */
+
+    QApplication app {argc, argv};
+    QPushButton button {"hello world!"};
+    button.show();
+
+    return QApplication::exec();
 }
