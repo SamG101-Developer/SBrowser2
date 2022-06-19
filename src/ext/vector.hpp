427cb606286554bc4a6dbe5fa7468a46c0cc1543
Sam <samuelgardner101@gmail.com>
2022-06-19 21:41:28 +0100

    Added types, operators, changed how elements are returned from accessors

--- src/ext/vector.hpp
+++ src/ext/vector.hpp
@@ -4,49 +4,75 @@
 
 #include <veque.hpp>
 
-namespace     {template <typename _Vt> using container_internal = veque::veque<_Vt>;}
+namespace     {template <typename _Vt> using vector_internal = veque::veque<_Vt>;}
 namespace ext {template <typename _Tx> class vector;}
+namespace ext {template <typename _Vt> using vector_view = vector<_Vt>;}
 
+#include <ranges>
 #include <ext/keywords.hpp>
 #include <ext/optional.hpp>
 
-
 template <typename _Tx>
 class ext::vector
-        : public container_internal<_Tx>
+        : public vector_internal<_Tx>
 {
 public aliases:
-    using reference = typename container_internal<_Tx>::reference;
-    using const_reference = typename container_internal<_Tx>::const_reference;
-    using value_type = _Tx;
-    using size_type = typename container_internal<_Tx>::size_type;
+    using reference = typename vector_internal<_Tx>::reference;
+    using const_reference = typename vector_internal<_Tx>::const_reference;
+    using outer_val_t = _Tx;
+    using size_type = typename vector_internal<_Tx>::size_type;
+
+    using iterator = typename vector_internal<_Tx>::iterator;
+    using const_iterator = typename vector_internal<_Tx>::const_iterator;
 
 public constructors:
-    vector() = default;
+    using vector_internal<_Tx>::vector_internal;
 
-    vector(const vector&) = default;
-    vector(vector&&) noexcept = default;
-    auto operator=(const vector&) -> vector& = default;
-    auto operator=(vector&&) noexcept -> vector& = default;
-
-    vector(size_type _N, const _Tx& _Val) : veque::veque<_Tx>{_N, _Val} {};
-    vector(size_type _N, _Tx&& _Val) : veque::veque<_Tx>{_N, std::forward<_Tx>(_Val)} {};
-    template <typename ..._Valty> vector(_Valty... _Val) : veque::veque<_Tx>{std::initializer_list{std::forward<_Valty>(_Val)...}} {};
+    template <typename ..._Valty> explicit vector(_Valty... _Val) : veque::veque<_Tx>{std::initializer_list{std::forward<_Valty>(_Val)...}} {};
 
 public cpp_methods:
-    auto front()       -> optional<      reference> {return (*this)[0];};
-    auto front() const -> optional<const_reference> {return (*this)[0];};
+    auto front()       -> optional<      value_type> {return (*this)[0];};
+    auto front() const -> optional<const value_type> {return (*this)[0];};
 
-    auto back()       -> optional<      reference> {return (*this)[this->size() - 1];};
-    auto back() const -> optional<const_reference> {return (*this)[this->size() - 1];};
+    auto back()       -> optional<      value_type> {return (*this)[this->size() - 1];};
+    auto back() const -> optional<const value_type> {return (*this)[this->size() - 1];};
 
-    auto at(size_type _Idx)       -> optional<      reference> {return (*this)[_Idx];};
-    auto at(size_type _Idx) const -> optional<const_reference> {return (*this)[_Idx];};
+    auto at(size_type _Idx)       -> optional<      value_type> {return (*this)[_Idx];};
+    auto at(size_type _Idx) const -> optional<const value_type> {return (*this)[_Idx];};
+
+    auto extend(const_iterator _Where, ext::vector_view<_Tx> _Other)
+    {
+        for (const auto& _Item: _Other | std::ranges::views::reverse)
+            this->insert(_Where, _Item);
+    }
 
 public cpp_operators:
-    auto operator[](size_type _Idx)       -> optional<      reference> {return optional{this->begin() + _Idx != nullptr ? this->begin() + _Idx : null};};
-    auto operator[](size_type _Idx) const -> optional<const_reference> {return optional{this->begin() + _Idx != nullptr ? this->begin() + _Idx : null};};
+    auto operator[](size_type _Idx)       -> optional<      value_type> {if (this->begin() + _Idx != this->end()) return optional<value_type>{*(this->begin() + _Idx)}; else return optional<value_type>{};};
+    auto operator[](size_type _Idx) const -> optional<const value_type> {if (this->begin() + _Idx != this->end()) return optional<value_type>{*(this->begin() + _Idx)}; else return optional<value_type>{};};
+
+    auto operator+=(vector_view<_Tx> _Other) -> vector&;
+    auto operator+ (vector_view<_Tx> _Other) -> vector;
 };
+
+
+template <typename _Tx>
+auto ext::vector<_Tx>::operator+=(vector_view<_Tx> _Other) -> vector<_Tx>&
+{
+    this->reserve(_Other.size());
+    for (const auto& _Item: _Other)
+        this->push_back(_Item);
+    return *this;
+}
+
+
+template <typename _Tx>
+auto ext::vector<_Tx>::operator+(vector_view<_Tx> _Other) -> vector<_Tx>
+{
+    vector<_Tx> _Out{this->size() + _Other.size()};
+    _Out += *this;
+    _Out += _Other;
+    return _Out;
+}
 
 
 #endif //SBROWSER2_VECTOR_HPP
