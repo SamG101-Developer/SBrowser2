1f89d9fbced6dbdfe5565c05cb1b167d05ed31c4
Sam <samuelgardner101@gmail.com>
2022-06-19 18:19:40 +0100

    Simplified const correctness (for now), style conformance

--- src/ext/casting.hpp
+++ src/ext/casting.hpp
@@ -2,14 +2,14 @@
 #ifndef SBROWSER2_CASTING_HPP
 #define SBROWSER2_CASTING_HPP
 
-namespace ext {template <typename ..._Valty, typename _Ty> auto multi_cast(const _Ty* _Pointer) -> bool;}
+namespace ext {template <typename ..._Valty, typename _Ty> auto multi_cast(_Ty* _Pointer) -> bool;}
 
 
 template <typename ..._Valty, typename _Ty>
-auto ext::multi_cast(const _Ty* const _Pointer) -> bool
+auto ext::multi_cast(_Ty* const _Pointer) -> bool
 {
     // check if the pointer can be cast into any of the types in _Valty template type pack
-    return ((dynamic_cast<_Valty>(_Pointer)) or ...);
+    return ((dynamic_cast<_Valty>(_Pointer)) || ...);
 }
 
 
