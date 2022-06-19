d8a8a174ec5e2f9d26abef47d4be781f13256f5e
Sam <samuelgardner101@gmail.com>
2022-06-19 18:18:48 +0100

    Added copy constructor for `v8pp` library, static `TRUE` and `FALSE` methods, and renamed the `not` operator to the `!` operator for style conformance

--- src/ext/boolean.hpp
+++ src/ext/boolean.hpp
@@ -3,6 +3,7 @@
 #define SBROWSER2_BOOLEAN_HPP
 
 namespace ext {class boolean;}
+namespace ext {using boolean_view = const boolean&;}
 
 #include <utility>
 #include <ext/keywords.hpp>
@@ -13,14 +14,18 @@ class ext::boolean final
 public:
     constexpr boolean() : _Val(false) {}
 
-    constexpr boolean(const bool&) = delete;
+    constexpr boolean(const bool& _Other) : _Val(_Other) {};
     constexpr boolean(bool&& _Other) : _Val(std::forward<bool>(_Other)) {}
     auto operator=(const bool& _Other) -> boolean& {_Val = _Other; return *this;};
     auto operator=(bool&& _Other) -> boolean& {_Val = std::forward<bool>(_Other); return *this;}
 
 public:
+    static auto FALSE() -> ext::boolean {return ext::boolean{false};};
+    static auto TRUE () -> ext::boolean {return ext::boolean{true };};
+
+public:
     operator bool() const {return _Val;}
-    auto operator not() const -> bool {return _Val ^ true;}
+    auto operator !() const -> bool {return _Val ^ true;}
 
 private:
     bool _Val;
