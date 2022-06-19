30d3487c3ef22389f99631dac21c433fa91a98c3
Sam <samuelgardner101@gmail.com>
2022-06-19 21:39:41 +0100

    Reorganised, added some operators

--- src/ext/string.hpp
+++ src/ext/string.hpp
@@ -15,47 +15,36 @@ namespace ext {class string_view;}
 #include <QtCore/QString>
 
 
+class ext::string_view
+        : public std::string_view
+{
+public constructors:
+    using std::string_view::string_view;
+    string_view(const char* _Other) : std::string_view{_Other} {}
+
+public cpp_methods: // TODO -> is .data() safe?
+    operator QString() const {return {data()};}
+    operator v8::Local<v8::String>() const {return v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), data()).ToLocalChecked();}
+};
+
+
 class ext::string
         : public std::string
 {
 public constructors:
-    string() = default;
-    ~string() {clear();}
+    using std::string::string;
+    string(const char* _Other) : std::string(_Other) {}
+    string(string_view _Other) {/* TODO */}
+    auto operator=(string_view _Other) -> string& {/* TODO */}
 
-    string(const string&) = default;
-    string(string&&) noexcept = default;
-    auto operator=(const string&) -> string& = default;
-    auto operator=(string&&) noexcept -> string& = default;
-
-    string(const char* _Other) : std::string(_Other) {};
-    auto operator=(const char* _Other) -> string& {std::string::operator=(_Other);};
-
-public cpp_operators:
-    [[nodiscard]] auto to_qt_string() const -> QString {return {c_str()};}
-    [[nodiscard]] auto to_v8_string() const -> v8::Local<v8::String> {return v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), c_str()).ToLocalChecked();}
+public cpp_methods:
+    operator QString() const {return {c_str()};}
+    operator v8::Local<v8::String>() const {return v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), c_str()).ToLocalChecked();}
+    operator bool() {return not empty();}
 
     [[nodiscard]] static auto from_qt_string(const QString& _That) -> ext::string {return string{_That.toStdString().c_str()};}
     [[nodiscard]] static auto from_v8_string(const v8::Local<v8::String> _That) -> ext::string {return string{*v8::String::Utf8Value{v8::Isolate::GetCurrent(), _That}};}
 };
 
-
-class ext::string_view
-        : public std::string_view
-{
-public constructors:
-    string_view() = default;
-
-    string_view(const string_view&) = default;
-    string_view(string_view&&) noexcept = default;
-    auto operator=(const string_view&) const -> string_view& = delete;
-    auto operator=(string_view&&) const noexcept -> string_view& = delete;
-
-    explicit string_view(const string& _Other) : std::string_view{_Other} {};
-    explicit string_view(string&& _Other) noexcept : std::string_view{std::forward<string>(_Other)} {};
-    auto operator=(const string&) const -> string_view& = delete;
-    auto operator=(string&&) const noexcept -> string_view& = delete;
-
-    explicit string_view(const char* _Other) : std::string_view{_Other} {};
-};
 
 #endif //SBROWSER2_STRING_HPP
