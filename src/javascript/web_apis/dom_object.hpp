e30d85aab8ef8c3ff57632a8188960209d1ae411
Sam <samuelgardner101@gmail.com>
2022-06-19 18:23:48 +0100

    Added constructors and a virtual destructor

--- src/javascript/web_apis/dom_object.hpp
+++ src/javascript/web_apis/dom_object.hpp
@@ -1,8 +1,13 @@
 #ifndef SBROWSER2_DOM_OBJECT_HPP
 #define SBROWSER2_DOM_OBJECT_HPP
 
+#include <memory>
+
 #include <ext/any.hpp>
+#include <ext/custom_operator.hpp>
 #include <ext/keywords.hpp>
+#include <ext/property.hpp>
+#include <ext/string.hpp>
 #include <v8-isolate.h>
 #include <v8pp/class.hpp>
 
@@ -11,6 +16,14 @@ namespace web_apis {class dom_object;}
 
 class web_apis::dom_object
 {
+public constructors:
+    dom_object() = default;
+    dom_object(const dom_object&) = default;
+    dom_object(dom_object&&) noexcept = default;
+    auto operator=(const dom_object&) -> dom_object& = default;
+    auto operator=(dom_object&&) noexcept -> dom_object& = default;
+    virtual ~dom_object() = default;
+
 public cpp_methods:
     virtual auto to_v8(v8::Isolate* isolate) const && -> ext::any = 0;
 };
