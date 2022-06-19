63770ea78b4dc06dbae518a06e3e586d5b8a1eed
Sam <samuelgardner101@gmail.com>
2022-06-19 21:38:19 +0100

    Added `ext:optional<T>::value_to_or<U>(other)`, for when an `ext::any` is being stored, so either cast it to the type, otherwise return a value; `value_or(other) can't work, as the case would be applied to either an `ext::any` or `other`, so allow the cast to be inbuilt to the method

--- src/ext/optional.hpp
+++ src/ext/optional.hpp
@@ -7,27 +7,29 @@ namespace ext {template <typename _Tx> class optional;}
 #include <optional>
 #include <ext/keywords.hpp>
 
-namespace ext {constexpr auto null = std::nullopt_t::_Tag{};};
+namespace ext {class any;}
+namespace ext {auto null = std::nullopt;}
 
 
 template <typename _Tx>
 class ext::optional final
 {
-public:
+public constructors:
+    optional() = default;
+
     optional(const optional&) = delete;
     optional(optional&&) noexcept = default;
     auto operator=(const optional&) -> optional& = delete;
     auto operator=(optional&&) noexcept -> optional& = default;
 
-    optional() = default;
     explicit optional(const _Tx& _Val) : _Opt(std::make_optional<_Tx>(_Val)) {}
     explicit optional(_Tx&& _Val) noexcept : _Opt(std::make_optional<_Tx>(std::forward<_Tx>(_Val))) {}
     auto operator=(const _Tx& _Val) -> optional& {_Opt.emplace(_Val); return *this;};
     auto operator=(_Tx&& _Val) -> optional& {_Opt.emplace(std::forward<_Tx>(_Val)); return *this;};
 
-    auto operator=(const std::nullopt_t&) -> optional& {_Opt.reset();}
+//    auto operator=(const std::nullopt_t&) -> optional& {_Opt.reset();}
 
-public:
+public cpp_methods:
     [[nodiscard]] constexpr auto empty() const -> bool {return not _Opt.has_value();}
     [[nodiscard]] constexpr auto has_value() const -> bool {return _Opt.has_value();}
 
@@ -36,10 +38,16 @@ public:
     constexpr auto value_or(_Tx&& _Other     ) const ->       _Tx  {return _Opt.value_or(std::forward<_Tx>(_Other));}
     constexpr auto value_or(const _Tx& _Other) const -> const _Tx& {return _Opt.value_or(_Other);}
 
-    constexpr auto not_value_or(_Tx&& _Other     ) const -> const _Tx& {return empty() ? null : std::forward<_Tx>(_Other);}
-    constexpr auto not_value_or(const _Tx& _Other) const -> const _Tx& {return empty() ? null : _Other;}
+    template <typename _Ty> constexpr auto value_to_or(_Ty&& _Other     ) const -> _Ty requires std::is_same_v<std::remove_cvref_t<_Tx>, ext::any> {return not empty() ? _Other : value().template to<_Ty>();};
+    template <typename _Ty> constexpr auto value_to_or(const _Ty& _Other) const -> _Ty requires std::is_same_v<std::remove_cvref_t<_Tx>, ext::any> {return not empty() ? _Other : value().template to<_Ty>();};
 
-private:
+    constexpr auto not_value_or(_Tx&& _Other     ) const -> const _Tx& {return empty() ? null : std::move(_Other);}
+    constexpr auto not_value_or(const _Tx& _Other) const -> const _Tx& {return empty() ? null : _Other;}
+    
+public cpp_operators:
+    auto operator->() const -> auto {return _Opt.operator->();}
+
+private cpp_properties:
     std::optional<_Tx> _Opt;
 };
 
