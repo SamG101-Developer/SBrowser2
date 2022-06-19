cadc2e30a788a057d4810d0aa6b73ee46d4b719a
Sam <samuelgardner101@gmail.com>
2022-06-19 21:34:57 +0100

    Switched back to the `std::map` from `tsl::robin_map` temporarily

--- src/ext/map.hpp
+++ src/ext/map.hpp
@@ -2,30 +2,40 @@
 #ifndef SBROWSER2_MAP_HPP
 #define SBROWSER2_MAP_HPP
 
-#include <tsl/robin_map.h>
-#include <robin_hood.h>
+#include <map>
 
-namespace     {template <typename _Vt1, typename _Vt2> using container_internal = tsl::robin_map<_Vt1, _Vt2, robin_hood::hash<_Vt1>>;}
+namespace     {template <typename _Vt1, typename _Vt2> using map_internal = std::map<_Vt1, _Vt2>;}
 namespace ext {template <typename _Vt1, typename _Vt2> class map;}
 
 #include <ext/keywords.hpp>
+#include <ext/optional.hpp>
+
+namespace ext {class any;}
+namespace ext {class string;}
+
+namespace ext {using string_any_map         = ext::map<ext::string, ext::any   >;}
+namespace ext {using string_any_map_view    = const string_any_map&;}
+namespace ext {using string_string_map      = ext::map<ext::string, ext::string>;}
+namespace ext {using string_string_map_view = const string_string_map&;}
 
 
 template <typename _Tx1, typename _Tx2>
 class ext::map
-        : public container_internal<_Tx1, _Tx2>
+        : public map_internal<_Tx1, _Tx2>
 {
 public aliases:
     using key_t = _Tx1;
     using value_t = _Tx2;
 
 public constructors:
-    map() = default;
-    map(const map&) = default;
-    map(map&&) noexcept = default;
-    auto operator=(const map&) -> map& = default;
-    auto operator=(map&&) noexcept -> map& = default;
-    ~map() {this->clear();}
+    using map_internal<_Tx1, _Tx2>::map_internal;
+
+public cpp_methods:
+    auto at(const _Tx1& _Key)       -> ext::optional<      _Tx2> {return optional(this->contains(_Key) ? map_internal<_Tx1, _Tx2>::at(_Key) : null);};
+    auto at(const _Tx1& _Key) const -> ext::optional<const _Tx2> {return optional(this->contains(_Key) ? map_internal<_Tx1, _Tx2>::at(_Key) : null);};
+
+    auto at(_Tx1&& _Key)       -> ext::optional<      _Tx2> {return optional(this->contains(_Key) ? map_internal<_Tx1, _Tx2>::at(_Key) : null);};
+    auto at(_Tx1&& _Key) const -> ext::optional<const _Tx2> {return optional(this->contains(_Key) ? map_internal<_Tx1, _Tx2>::at(_Key) : null);};
 };
 
 
