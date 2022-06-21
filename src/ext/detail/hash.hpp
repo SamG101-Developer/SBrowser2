dcf0f0855c2b42b86273d219ef8304272484024b
Sam <samuelgardner101@gmail.com>
2022-06-21 20:27:57 +0100

    Moved variables into method

--- src/ext/detail/hash.hpp
+++ src/ext/detail/hash.hpp
@@ -4,15 +4,12 @@
 
 #include <ext/type_traits.hpp>
 
-
-namespace             {constexpr ulong _A = 54059;}
-namespace             {constexpr ulong _B = 76963;}
-namespace             {constexpr ulong _C = 86969;}
-namespace             {constexpr ulong _D = 00037;}
 namespace ext::detail {auto constexpr _Hash(const char* _S) -> size_t;}
 
 auto constexpr ext::detail::_Hash(const char* _S) -> size_t
 {
+    constexpr ulong _A = 54059, _B = 76963, _C = 86969, _D = 37;
+
     auto _H = _D;
     while (*_S) {_H = (_H * _A) ^ (_S[0] * _B); _S++;}
     return _H;
