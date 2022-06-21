9e974c01eb25760f7c972f3a57336b7a46372b1d
Sam <samuelgardner101@gmail.com>
2022-06-21 20:30:02 +0100

    Defined all methods

---
+++ src/javascript/web_apis/dom/detail/node_internals.hpp
@@ -0,0 +1,88 @@
+#ifndef SBROWSER2_NODE_INTERNALS_HPP
+#define SBROWSER2_NODE_INTERNALS_HPP
+
+#include <ext/string.hpp>
+#include <ext/vector.hpp>
+#include <dom/nodes/document.hpp>
+#include <dom/nodes/element.hpp>
+#include <dom/nodes/text.hpp>
+
+
+namespace dom::detail::node_internals
+{
+    // node manipulation
+    template <typename T>
+    auto clone(
+            T* node,
+            nodes::document* document = nullptr,
+            ext::boolean deep = false)
+            -> dom::nodes::node* requires std::is_base_of_v<T, nodes::node>;
+
+    auto adopt(
+            nodes::node* node,
+            const nodes::document* document)
+            -> void;
+
+    // locating information on a node
+    auto locate_a_namespace_prefix(
+            const nodes::element* element,
+            ext::string_view namespace_)
+            -> ext::string;
+
+    auto locate_a_namespace(
+            const nodes::node* node,
+            ext::string_view prefix)
+            -> ext::string;
+
+    // list elements with certain features
+    auto list_of_elements_with_qualified_name(
+            const nodes::node* descendant_element,
+            ext::string_view qualified_name)
+            -> ext::vector<nodes::element*>;
+
+    auto list_of_elements_with_namespace_and_local_name(
+            const nodes::node* node,
+            ext::string_view namespace_,
+            ext::string_view local_name)
+            -> ext::vector<nodes::element*>;
+
+    auto list_of_elements_with_class_names(
+            const nodes::node* node,
+            ext::string_view class_names)
+            -> ext::vector<nodes::element*>;
+
+    // other
+    auto string_replace_all(
+            ext::string_view string,
+            nodes::node* parent)
+            -> void;
+
+    auto is_document_available_to_user(
+            nodes::document* document,
+            ext::string_view)
+            -> ext::boolean;  // TODO name [-1]
+
+    auto is_document_fully_active(
+            nodes::document* document)
+            -> ext::boolean;
+
+    auto is_html(
+            const nodes::element* element)
+            -> ext::boolean;
+
+    auto has_active_parser(
+            nodes::document* document)
+            -> ext::boolean;
+
+    auto document_has_style_sheets_blocking_scripts(
+            nodes::document* document)
+            -> ext::boolean;
+
+    template <typename ...nodes_or_strings_t>
+    auto convert_nodes_into_node(
+            const nodes::document* document,
+            nodes_or_strings_t... nodes)
+            -> nodes::node* requires (std::is_same_v<nodes_or_strings_t..., ext::string> || std::is_same_v<nodes_or_strings_t..., nodes::text*>);
+}
+
+#endif //SBROWSER2_NODE_INTERNALS_HPP
