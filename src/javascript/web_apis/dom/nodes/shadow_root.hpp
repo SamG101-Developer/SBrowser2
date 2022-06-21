82afb0da16a631b40d4e99b8658bfb1f8eda4036
Sam <samuelgardner101@gmail.com>
2022-06-21 20:27:41 +0100

    Added file

---
+++ src/javascript/web_apis/dom/nodes/shadow_root.hpp
@@ -0,0 +1,15 @@
+#ifndef SBROWSER2_SHADOW_ROOT_HPP
+#define SBROWSER2_SHADOW_ROOT_HPP
+
+#include <web_apis/dom/nodes/node.hpp>
+namespace dom::nodes {class shadow_root;}
+
+
+class dom::nodes::shadow_root
+        : public node
+{
+
+};
+
+
+#endif //SBROWSER2_SHADOW_ROOT_HPP
