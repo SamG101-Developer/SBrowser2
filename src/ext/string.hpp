50b9cd5a8220eb58e43dffca008cb968a7249600
Sam <samuelgardner101@gmail.com>
2022-06-21 22:11:17 +0100

    Simplified operators and conversions

--- src/ext/string.hpp
+++ src/ext/string.hpp
@@ -19,12 +19,13 @@ class ext::string_view
         : public std::string_view
 {
 public constructors:
-    using std::string_view::string_view;
+    using std::string_view::basic_string_view;
     string_view(const char* _Other) : std::string_view{_Other} {}
 
 public cpp_methods: // TODO -> is .data() safe?
     operator QString() const {return {data()};}
     operator v8::Local<v8::String>() const {return v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), data()).ToLocalChecked();}
+    operator bool() {return not empty();}
 };
 
 
@@ -32,18 +33,20 @@ class ext::string
         : public std::string
 {
 public constructors:
-    using std::string::string;
+    using std::string::basic_string;
     string(const char* _Other) : std::string(_Other) {}
     string(string_view _Other) {/* TODO */}
     auto operator=(string_view _Other) -> string& {/* TODO */}
 
 public cpp_methods:
+    [[nodiscard]] static auto from_qt_string(const QString& _That) -> ext::string {return string{_That.toStdString().c_str()};}
+    [[nodiscard]] static auto from_v8_string(v8::Local<v8::String> _That) -> ext::string {return string{*v8::String::Utf8Value{v8::Isolate::GetCurrent(), _That}};}
+
+public cpp_operators:
     operator QString() const {return {c_str()};}
     operator v8::Local<v8::String>() const {return v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), c_str()).ToLocalChecked();}
     operator bool() {return not empty();}
-
-    [[nodiscard]] static auto from_qt_string(const QString& _That) -> ext::string {return string{_That.toStdString().c_str()};}
-    [[nodiscard]] static auto from_v8_string(const v8::Local<v8::String> _That) -> ext::string {return string{*v8::String::Utf8Value{v8::Isolate::GetCurrent(), _That}};}
+    operator string_view() {return string_view{c_str()};};
 };
 
 
