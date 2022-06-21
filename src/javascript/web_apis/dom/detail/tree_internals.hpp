18c713a08bf0aeb07008761cbc76d0a286528e30
Sam <samuelgardner101@gmail.com>
2022-06-21 22:16:43 +0100

    Range-v3 based

--- src/javascript/web_apis/dom/detail/tree_internals.hpp
+++ src/javascript/web_apis/dom/detail/tree_internals.hpp
@@ -10,14 +10,18 @@
 
 #include <web_apis/dom/nodes/attr.hpp>
 #include <web_apis/dom/nodes/document.hpp>
+#include <web_apis/dom/nodes/element.hpp>
 #include <web_apis/dom/nodes/node.hpp>
 #include <web_apis/dom/nodes/text.hpp>
 
 #include <range/v3/to_container.hpp>
+#include <range/v3/algorithm/contains.hpp>
+#include <range/v3/algorithm/for_each.hpp>
 #include <range/v3/algorithm/find.hpp>
 #include <range/v3/view/concat.hpp>
 #include <range/v3/view/drop_while.hpp>
 #include <range/v3/view/filter.hpp>
+#include <range/v3/view/join.hpp>
 #include <range/v3/view/set_algorithm.hpp>
 #include <range/v3/view/take_while.hpp>
 #include <range/v3/view/transform.hpp>
@@ -30,15 +34,15 @@ namespace dom::detail::tree_internals
 {
     // ancestors and descendants
     auto root(
-            nodes::node* node)
+            nodes::node* node_a)
             -> nodes::node*;
 
     auto ancestors(
-            nodes::node* node)
+            nodes::node* node_a)
             -> range_v3_view auto;
 
     auto descendants(
-            nodes::node* node)
+            nodes::node* node_a)
             -> range_v3_view auto;
     
     // tree checks
@@ -147,92 +151,69 @@ namespace dom::detail::tree_internals
 }
 
 
-auto dom::detail::tree_internals::root(
-        nodes::node* node)
-        -> nodes::node*
+auto dom::detail::tree_internals::root(nodes::node* node_a) -> nodes::node*
 {
     // return the topmost ancestor, or nullptr if there is no node
-    return not node ? nullptr :
-           node->parent_node ? root(node->parent_node) : node;
+    return not node_a ? nullptr : node_a->parent_node() ? root(node_a->parent_node()) : node_a;
 }
 
 
-auto dom::detail::tree_internals::ancestors(
-        nodes::node* node)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::ancestors(nodes::node* node_a) -> range_v3_view auto
 {
     // iterate up through the parent_node attributes, until the root is reached, appending each element
-    ext::vector<nodes::node*> ancestor_nodes {node};
-    while ((node = node->parent_node)) ancestor_nodes.push_back(node);
+    ext::vector<nodes::node*> ancestor_nodes {node_a};
+    while ((node_a = node_a->parent_node())) ancestor_nodes.push_back(node_a);
     return ranges::ref_view{ancestor_nodes};
 }
 
 
-auto dom::detail::tree_internals::descendants(
-        nodes::node* node)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::descendants(nodes::node* node_a) -> range_v3_view auto
 {
     // iterate though the children, appending each child and the child of that child, until the last child is checked
-    ext::vector<nodes::node*> descendant_nodes {node};
-    for (const auto& child: descendant_nodes) descendant_nodes.extend(ranges::find(descendant_nodes, child), *(ext::vector<nodes::node*>*)child->child_nodes);
+    ext::vector<nodes::node*> descendant_nodes {node_a};
+    for (const auto& child: descendant_nodes) descendant_nodes.extend(ranges::find(descendant_nodes, child), *child->child_nodes());
     return ranges::ref_view(descendant_nodes);
 }
 
 
-auto dom::detail::tree_internals::is_ancestor(
-        nodes::node* node_a,
-        nodes::node* node_b)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_ancestor(nodes::node* node_a, nodes::node* node_b) -> ext::boolean
 {
     // 'node_a' is an ancestor of 'node_b' if it is in the ancestor() list for 'node_b'
+    if (!node_a || !node_b) return false;
     auto ancestors_of_b = ancestors(node_b);
-    return ranges::find(ancestors_of_b, node_a) != ancestors_of_b.end();
+    return ranges::contains(ancestors_of_b, node_a);
 }
 
 
-auto dom::detail::tree_internals::is_descendant(
-        nodes::node* node_a,
-        nodes::node* node_b)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_descendant(nodes::node* node_a, nodes::node* node_b) -> ext::boolean
 {
     // 'node_a' is a descendant of 'node_b' if 'node_b' is an ancestor of 'node_a' (faster than generating descendants)
     return is_ancestor(node_b, node_a);
 }
 
 
-auto dom::detail::tree_internals::is_sibling(
-        nodes::node* node_a,
-        nodes::node* node_b)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_sibling(nodes::node* node_a, nodes::node* node_b) -> ext::boolean
 {
     // 'node_a' is a sibling of 'node_b' if they have the same 'parent_node'
     return node_a->parent_node == node_b->parent_node;
 }
 
 
-auto dom::detail::tree_internals::is_preceding(
-        nodes::node* node_a,
-        nodes::node* node_b)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_preceding(nodes::node* node_a, nodes::node* node_b) -> ext::boolean
 {
     // 'node_a' precedes 'node_b' if its index is lower
     return index(node_a) < index(node_b);
 }
 
 
-auto dom::detail::tree_internals::is_following(
-        nodes::node* node_a,
-        nodes::node* node_b)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_following(nodes::node* node_a, nodes::node* node_b) -> ext::boolean
 {
     // 'node_a' follows 'node_b' if its index is higher
     return index(node_a) > index(node_b);
 }
 
 
-auto dom::detail::tree_internals::index(
-        nodes::node* node_a)
-        -> ext::number<ulong>
+auto dom::detail::tree_internals::index(nodes::node* node_a) -> ext::number<ulong>
 {
     auto descendant_nodes = descendants(root(node_a));
     auto where_iterator   = ranges::find(descendant_nodes, node_a);
@@ -241,20 +222,16 @@ auto dom::detail::tree_internals::index(
 }
 
 
-auto dom::detail::tree_internals::length(
-        nodes::node* node_a)
-        -> ext::number<ulong>
+auto dom::detail::tree_internals::length(nodes::node* node_a) -> ext::number<ulong>
 {
-    if (ext::multi_cast<nodes::attr*, nodes::document_type*>(node_a)) return 0;
+    if (!node_a || ext::multi_cast<nodes::attr*, nodes::document_type*>(node_a)) return 0;
     else if (ext::multi_cast<nodes::character_data*>(node_a)) return dynamic_cast<nodes::character_data*>(node_a)->length();
     else return node_a->child_nodes->size();
 }
 
 
 template <typename T>
-auto dom::detail::tree_internals::all_following(
-        nodes::node* node_a)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::all_following(nodes::node* node_a) -> range_v3_view auto
 {
     auto descendant_nodes = descendants(node_a);
     return descendant_nodes
@@ -265,9 +242,7 @@ auto dom::detail::tree_internals::all_following(
 
 
 template <typename T>
-auto dom::detail::tree_internals::all_preceding(
-        nodes::node* node_a)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::all_preceding(nodes::node* node_a) -> range_v3_view auto
 {
     auto descendant_nodes = descendants(node_a);
     return descendant_nodes
@@ -278,9 +253,7 @@ auto dom::detail::tree_internals::all_preceding(
 
 
 template <typename T>
-auto dom::detail::tree_internals::all_preceding_siblings(
-        nodes::node* node_a)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::all_preceding_siblings(nodes::node* node_a) -> range_v3_view auto
 {
     auto sibling_nodes = node_a->parent_node->child_nodes();
     return *sibling_nodes
@@ -291,9 +264,7 @@ auto dom::detail::tree_internals::all_preceding_siblings(
 
 
 template <typename T>
-auto dom::detail::tree_internals::all_following_siblings(
-        nodes::node* node_a)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::all_following_siblings(nodes::node* node_a) -> range_v3_view auto
 {
     auto sibling_nodes = node_a->parent_node->child_nodes();
     return *sibling_nodes
@@ -303,41 +274,31 @@ auto dom::detail::tree_internals::all_following_siblings(
 }
 
 
-auto dom::detail::tree_internals::is_element_node(
-        nodes::node* node_a)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_element_node(nodes::node* node_a) -> ext::boolean
 {
     return dynamic_cast<nodes::element*>(node_a);
 }
 
 
-auto dom::detail::tree_internals::is_text_node(
-        nodes::node* node_a)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_text_node(nodes::node* node_a) -> ext::boolean
 {
     return dynamic_cast<nodes::character_data*>(node_a);
 }
 
 
-auto dom::detail::tree_internals::is_exclusive_text_node(
-        nodes::node* node_a)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_exclusive_text_node(nodes::node* node_a) -> ext::boolean
 {
     return dynamic_cast<nodes::text*>(node_a);
 }
 
 
-auto dom::detail::tree_internals::is_document_type_node(
-        nodes::node* node_a)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_document_type_node(nodes::node* node_a) -> ext::boolean
 {
     return dynamic_cast<nodes::document_type*>(node_a);
 }
 
 
-auto dom::detail::tree_internals::contiguous_text_nodes(
-        nodes::node* node_a)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::contiguous_text_nodes(nodes::node* node_a) -> range_v3_view auto
 {
     auto sibling_nodes = node_a->parent_node->child_nodes();
 
@@ -362,9 +323,7 @@ auto dom::detail::tree_internals::contiguous_text_nodes(
 }
 
 
-auto dom::detail::tree_internals::descendant_text_nodes(
-        nodes::node* node_a)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::descendant_text_nodes(nodes::node* node_a) -> range_v3_view auto
 {
     auto descendant_nodes = descendants(node_a);
     return descendant_nodes
@@ -373,9 +332,7 @@ auto dom::detail::tree_internals::descendant_text_nodes(
 }
 
 
-auto dom::detail::tree_internals::child_text_nodes(
-        nodes::node* node_a)
-        -> range_v3_view auto
+auto dom::detail::tree_internals::child_text_nodes(nodes::node* node_a) -> range_v3_view auto
 {
     auto child_nodes = node_a->parent_node->child_nodes();
     return *child_nodes
@@ -384,9 +341,7 @@ auto dom::detail::tree_internals::child_text_nodes(
 }
 
 
-auto dom::detail::tree_internals::descendant_text_content(
-        nodes::node* node_a)
-        -> ext::string
+auto dom::detail::tree_internals::descendant_text_content(nodes::node* node_a) -> ext::string
 {
     return descendant_text_nodes(node_a)
             | ranges::views::transform([](nodes::text* descendant_text_node) {return descendant_text_node->data();})
@@ -394,9 +349,7 @@ auto dom::detail::tree_internals::descendant_text_content(
 }
 
 
-auto dom::detail::tree_internals::child_text_content(
-        nodes::node* node_a)
-        -> ext::string
+auto dom::detail::tree_internals::child_text_content(nodes::node* node_a) -> ext::string
 {
     return child_text_nodes(node_a)
             | ranges::views::transform([](nodes::text* child_text_node) {return child_text_node->data();})
@@ -404,19 +357,14 @@ auto dom::detail::tree_internals::child_text_content(
 }
 
 
-auto dom::detail::tree_internals::common_ancestor(
-        nodes::node* node_a,
-        nodes::node* node_b)
-        -> nodes::node*
+auto dom::detail::tree_internals::common_ancestor(nodes::node* node_a, nodes::node* node_b) -> nodes::node*
 {
     auto common_ancestor_nodes = ranges::views::set_intersection(ancestors(node_a), ancestors(node_b));
     return common_ancestor_nodes.front();
 }
 
 
-auto dom::detail::tree_internals::is_document_element(
-        nodes::node* node_a)
-        -> ext::boolean
+auto dom::detail::tree_internals::is_document_element(nodes::node* node_a) -> ext::boolean
 {
     return is_element_node(node_a) && dynamic_cast<nodes::document*>(node_a->parent_node());
 }
