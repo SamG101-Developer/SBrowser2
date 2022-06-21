4f0f2c13802e04f7ef51bd42ff2240fb06b4e9bd
Sam <samuelgardner101@gmail.com>
2022-06-21 20:26:39 +0100

    Added file, defined enum for element class

---
+++ src/javascript/web_apis/dom/detail/customization_internals.hpp
@@ -0,0 +1,9 @@
+#ifndef SBROWSER2_CUSTOMIZATION_INTERNALS_HPP
+#define SBROWSER2_CUSTOMIZATION_INTERNALS_HPP
+
+namespace dom::detail::customization_internals
+{
+    enum custom_element_state_t {CUSTOM};
+}
+
+#endif //SBROWSER2_CUSTOMIZATION_INTERNALS_HPP
