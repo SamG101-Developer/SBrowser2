02fb36dc6fa9b74fcfca7d1394a0492008792e0d
Sam <samuelgardner101@gmail.com>
2022-06-23 20:03:30 +0100

    Implemented

--- src/javascript/web_apis/dom/nodes/comment.cpp
+++ src/javascript/web_apis/dom/nodes/comment.cpp
@@ -1,1 +1,36 @@
 #include "comment.hpp"
+
+#include <web_apis/dom/nodes/window.hpp>
+
+
+dom::nodes::comment::comment()
+{
+    owner_document = javascript::environment::realms::realm<nodes::window*>::relevant_realm(this).get<document*>("associated_document");
+}
+
+
+dom::nodes::comment::comment(
+        ext::string_view new_data)
+        : comment()
+{
+    data = new_data;
+}
+
+
+auto dom::nodes::comment::get_node_name()
+        const -> ext::string
+{
+    return "#comment";
+}
+
+
+auto dom::nodes::comment::to_v8(
+        v8::Isolate* isolate)
+        const && -> ext::any
+{
+    return v8pp::class_<comment>{isolate}
+            .ctor<>()
+            .ctor<ext::string_view>()
+            .inherit<character_data>()
+            .auto_wrap_objects();
+}
--- src/javascript/web_apis/dom/nodes/comment.hpp
+++ src/javascript/web_apis/dom/nodes/comment.hpp
@@ -1,12 +1,22 @@
 #ifndef SBROWSER2_COMMENT_HPP
 #define SBROWSER2_COMMENT_HPP
 
-#include <web_apis/dom/nodes/text.hpp>
+#include <web_apis/dom/nodes/character_data.hpp>
+namespace dom::nodes {class comment;}
 
 
-class comment
+class dom::nodes::comment
+        : public character_data
 {
+public constructors:
+    comment();
+    comment(ext::string_view new_data);
 
+public cpp_methods:
+    auto to_v8(v8::Isolate* isolate) const && -> ext::any override;
+
+private cpp_accessors:
+    [[nodiscard]] auto get_node_name() const -> ext::string override;
 };
 
 
