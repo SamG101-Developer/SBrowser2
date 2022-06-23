298bc9120a3de3a0e684dcdc5e0c27d1a047e11c
Sam <samuelgardner101@gmail.com>
2022-06-23 20:02:07 +0100

    Define class (empty at the moment)

---
+++ src/javascript/web_apis/dom/nodes/document_fragment.hpp
@@ -0,0 +1,14 @@
+#ifndef SBROWSER2_DOCUMENT_FRAGMENT_HPP
+#define SBROWSER2_DOCUMENT_FRAGMENT_HPP
+
+#include <web_apis/dom/nodes/node.hpp>
+namespace dom::nodes {class document_fragment;}
+
+
+class dom::nodes::document_fragment : public node
+{
+
+};
+
+
+#endif //SBROWSER2_DOCUMENT_FRAGMENT_HPP
---
+++ src/javascript/web_apis/dom/nodes/window.hpp
@@ -0,0 +1,15 @@
+#ifndef SBROWSER2_WINDOW_HPP
+#define SBROWSER2_WINDOW_HPP
+
+#include <web_apis/dom/nodes/event_target.hpp>
+namespace dom::nodes {class window;}
+
+
+class dom::nodes::window
+        : public event_target
+{
+
+};
+
+
+#endif //SBROWSER2_WINDOW_HPP
