45376c7c20c17d4724ff40fde5413e50eb5f71d3
Sam <samuelgardner101@gmail.com>
2022-06-25 23:43:23 +0100

    Added `ext::cross_cast<T>` which mimics `dynamic_cast<T>`, but is for mixin classes - easier to see what's happening

--- src/ext/casting.hpp
+++ src/ext/casting.hpp
@@ -2,14 +2,23 @@
 #ifndef SBROWSER2_CASTING_HPP
 #define SBROWSER2_CASTING_HPP
 
-namespace ext {template <typename ..._Valty, typename _Ty> auto multi_cast(_Ty* _Pointer) -> bool;}
+namespace ext {template <typename ..._Valty> auto multi_cast(auto* _Pointer) -> bool;}
+namespace ext {template <typename       _Ty> auto cross_cast(auto* _Pointer) -> _Ty;}
 
 
-template <typename ..._Valty, typename _Ty>
-auto ext::multi_cast(_Ty* const _Pointer) -> bool
+template <typename ..._Valty>
+auto ext::multi_cast(auto* _Pointer) -> bool
 {
     // check if the pointer can be cast into any of the types in _Valty parameter pack
     return ((dynamic_cast<_Valty>(_Pointer)) || ...);
+}
+
+
+template <typename _Ty>
+auto ext::cross_cast(auto* _Pointer) -> bool
+{
+    // perform a dynamic_cast TODO : error if cast is nullptr?
+    return dynamic_cast<_Ty>(_Pointer);
 }
 
 
