8fa1dc3e99bd3c0a6295dba83b799e99af09f5ec
Sam <samuelgardner101@gmail.com>
2022-06-25 23:49:16 +0100

    Changed to `std::hash` (simpler implementation & to see what's happening, probably faster too)

--- src/ext/detail/hash.hpp
+++ src/ext/detail/hash.hpp
@@ -2,17 +2,15 @@
 #ifndef SBROWSER2_HASH_HPP
 #define SBROWSER2_HASH_HPP
 
+#include <functional>
 #include <ext/type_traits.hpp>
 
-namespace ext::detail {auto constexpr _Hash(const char* _S) -> size_t;}
+namespace ext::detail {auto constexpr _Hash(std::string_view _S) -> size_t;}
 
-auto constexpr ext::detail::_Hash(const char* _S) -> size_t
+auto constexpr ext::detail::_Hash(std::string_view _S) -> size_t
 {
-    constexpr ulong _A = 54059, _B = 76963, _C = 86969, _D = 37;
-
-    auto _H = _D;
-    while (*_S) {_H = (_H * _A) ^ (_S[0] * _B); _S++;}
-    return _H;
+    std::hash<std::string_view> _Hasher;
+    return _Hasher(_S);
 }
 
 
