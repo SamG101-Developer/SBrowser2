35dbf0cb71f3d24b8fe0c6ceacb0cdd3c5ebc375
Sam <samuelgardner101@gmail.com>
2022-06-25 23:59:45 +0100

    Added property accessor

--- src/ext/vector_like.hpp
+++ src/ext/vector_like.hpp
@@ -2,7 +2,7 @@
 #ifndef SBROWSER2_VECTOR_LIKE_HPP
 #define SBROWSER2_VECTOR_LIKE_HPP
 
-namespace ext {template <typename _Tx1> class vector_like;}
+namespace ext {template <typename _Tx> class vector_like;}
 
 #include <ext/keywords.hpp>
 #include <ext/number.hpp>
@@ -10,20 +10,23 @@ namespace ext {template <typename _Tx1> class vector_like;}
 #include <ext/vector.hpp>
 
 
-template <typename _Tx1>
+template <typename _Tx>
 class ext::vector_like
 {
 public constructors:
-    explicit vector_like(ext::vector<_Tx1>* _Container) : _LinkedContainer{_Container} {bind_get(length, _LinkedContainer.size);};
+    explicit vector_like(ext::vector<_Tx>* _Container) : _LinkedContainer{_Container} {bind_get(length);};
 
 public cpp_properties:
     property<number<size_t>> length;
 
 public cpp_operators:
-    auto operator[](const number<size_t>& _Idx) -> optional<_Tx1&> {return _LinkedContainer->at(_Idx);};
+    auto operator[](const number<size_t>& _Idx) -> optional<_Tx&> {return _LinkedContainer->at(_Idx);};
 
 private cpp_properties:
-    std::shared_ptr<ext::vector<_Tx1>> _LinkedContainer;
+    std::shared_ptr<ext::vector<_Tx>> _LinkedContainer;
+
+private cpp_accessors:
+    [[nodiscard]] auto get_length() const -> ext::number<size_t> {return _LinkedContainer->size();};
 };
 
 
