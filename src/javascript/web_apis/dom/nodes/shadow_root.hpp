21aa334fb7ec7f83b7eed406d88131cc79396a89
Sam <samuelgardner101@gmail.com>
2022-06-25 23:58:45 +0100

    Added properties

--- src/javascript/web_apis/dom/nodes/shadow_root.hpp
+++ src/javascript/web_apis/dom/nodes/shadow_root.hpp
@@ -8,7 +8,13 @@ namespace dom::nodes {class shadow_root;}
 class dom::nodes::shadow_root
         : public node
 {
+public constructors:
+    shadow_root();
 
+public js_properties:
+    ext::property<ext::string> mode;
+    ext::property<ext::string> slot_assignment;
+    ext::property<ext::boolean> delegates_focus;
 };
 
 
