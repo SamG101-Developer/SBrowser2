fb4ed7b5fc2e0a32cc5d402cd770488b258ebf73
Sam <samuelgardner101@gmail.com>
2022-06-25 23:59:31 +0100

    Replaced SFINAE with concepts

--- src/ext/type_traits.hpp
+++ src/ext/type_traits.hpp
@@ -21,85 +21,72 @@ using ulong     = unsigned long;
 using ulonglong = unsigned long long;
 
 
-// check if a mixin is a base class of a type ie is any templated version of ext::number the inherited into a class etc TODO : switch params ie should be ...<BaseMixin, DerivedClass>
-namespace ext {template <typename _Tx1, template <typename, typename...> typename _Tx2> struct _is_template_base_of;}
-namespace ext {template <typename _Tx1, template <typename, typename...> typename _Tx2> using is_template_base_of = _is_template_base_of<std::remove_cvref_t<_Tx1>, _Tx2>;}
-namespace ext {template <typename _Tx1, template <typename, typename...> typename _Tx2> constexpr bool is_template_base_of_v = is_template_base_of<_Tx1, _Tx2>::value;}
-
-template <typename _Tx1, template <typename, typename...> typename _Tx2>
-struct ext::_is_template_base_of final : public std::false_type{};
-
-template <typename _Tx1, template <typename, typename...> typename _Tx2>
-struct ext::_is_template_base_of<_Tx2<_Tx1>, _Tx2> final : public std::true_type{};
+// check if a templated class is a base of another class (don't need template type to check)
+template <template <typename> typename _Base, typename _Derived>
+concept templated_base_of = requires(_Derived _Obj)
+{
+    []<typename _Ty>(const _Base<_Ty>&){}(_Obj);
+};
 
 
 // check if a type is iterable ie does it have .begin() and .end() iterator access
-namespace ext {template <typename _Tx, typename = void> struct _is_iterable;}
-namespace ext {template <typename _Tx> using is_iterable = _is_iterable<std::remove_cvref_t<_Tx>>;}
-namespace ext {template <typename _Tx> constexpr bool is_iterable_v = is_iterable<_Tx>::value;}
-
-template <typename _Tx, typename>
-struct ext::_is_iterable : public std::false_type{};
-
 template <typename _Tx>
-struct ext::_is_iterable<_Tx, std::void_t<decltype(std::begin(std::declval<_Tx>())), decltype(std::begin(std::declval<_Tx>()))>> : std::true_type{};
+concept iterable = requires (_Tx _Obj)
+{
+    _Obj.begin(); _Obj.end();
+};
 
 
 // check if a type is a smart pointer
-namespace ext {template <typename _Tx> struct _is_smart_pointer;}
-namespace ext {template <typename _Tx> using is_smart_pointer = _is_smart_pointer<std::remove_cvref_t<_Tx>>;}
-namespace ext {template <typename _Tx> constexpr bool is_smart_pointer_v = is_smart_pointer<_Tx>::value;}
-
 template <typename _Tx>
-struct ext::_is_smart_pointer : public std::false_type{};
+concept smart_pointer = requires (_Tx _Obj)
+{
+    _Obj.get() == &*_Obj;
+};
 
-template <typename _Tx>
-struct ext::_is_smart_pointer<std::shared_ptr<_Tx>> : std::true_type{};
 
-template <typename _Tx>
-struct ext::_is_smart_pointer<std::unique_ptr<_Tx>> : std::true_type{};
+// check if a pointer can be dynamically cast to another pointer
+template <typename _Tx, typename _Tx1>
+concept dynamically_castable_to = requires (_Tx _PtrL)
+{
+    dynamic_cast<_Tx1>(_PtrL);
+};
+
+
+// check if a type is a range_v3 type
+template <typename T>
+concept range_v3_view = requires(const T& range)
+{
+    ranges::view_<T>;
+};
+
+
+// check if a type is one of many
+template <typename ..._Valty, typename _Tx>
+concept same_as_any = requires(_Valty... _Val)
+{
+    (std::same_as<_Tx, _Valty> || ...);
+};
+
+
+// check if a type is a primitive numeric type
+template <typename T>
+concept primitive_numeric = requires(T)
+{
+    std::integral<T> || std::floating_point<T>;
+};
 
 
 // pointer deduction for raw and smart pointers together
 namespace ext {template <typename _Tx> struct _unwrap_smart_pointer;}
-namespace ext {template <typename _Vt> using unwrap_smart_pointer = _unwrap_smart_pointer<std::remove_cvref_t<_Vt>>;}
-namespace ext {template <typename _Vt> using unwrap_smart_pointer_t = typename unwrap_smart_pointer<_Vt>::type;}
+namespace ext {template <typename _Tx> using unwrap_smart_pointer = _unwrap_smart_pointer<_Tx>;}
+namespace ext {template <typename _Tx> using unwrap_smart_pointer_t = typename unwrap_smart_pointer<_Tx>::type;}
 
-template <typename _Tx> requires (ext::is_smart_pointer_v<_Tx>)
+template <smart_pointer _Tx>
 struct ext::_unwrap_smart_pointer<_Tx> {using type = typename _Tx::pointer;};
 
-template <typename _Tx> requires (!ext::is_smart_pointer_v<_Tx>)
-struct ext::_unwrap_smart_pointer<_Tx> {using type = _Tx;};
-
-
-//
-namespace ext {template <typename _Base, typename _Derived> using is_dynamically_castable_to = std::is_base_of<std::remove_pointer_t<_Base>, std::remove_pointer_t<_Derived>>;}
-namespace ext {template <typename _Base, typename _Derived> constexpr bool is_dynamically_castable_to_v = is_dynamically_castable_to<_Base, _Derived>::value;}
-
-
-// equality checks
-template <typename _Fx>
-auto operator==(const _Fx& _FunctionA, const _Fx& _FunctionB) requires std::is_invocable_v<_Fx> {return &_FunctionA == &_FunctionB;}
-
-
-// additional functionality to ranges v3
-template <typename _Rng, typename _ViewFn>
-auto operator|=(_Rng& _Container, ranges::views::view_closure<_ViewFn> _ViewFunction) requires ext::is_iterable_v<_Rng>
-{
-    _Container = _Container | _ViewFunction | ranges::to<_Rng>();
-    return _Container;
-}
-
-
-// TODO : temporary for ranges v3 until i can get their concepts working
-template <typename T>
-concept range_v3_view = requires(const T& range)
-{
-    {ranges::view_<T>};
-};
-
-
-
+template <typename _Tx>
+struct ext::_unwrap_smart_pointer {using type = _Tx;};
 
 
 #endif //SBROWSER2_TYPE_TRAITS_HPP
