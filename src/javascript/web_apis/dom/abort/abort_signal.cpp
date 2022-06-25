a584e81480fec8cc8d0ea104fda4cc7e242db938
Sam <samuelgardner101@gmail.com>
2022-06-25 23:40:59 +0100

    Done some work on timeout() method

--- src/javascript/web_apis/dom/abort/abort_signal.cpp
+++ src/javascript/web_apis/dom/abort/abort_signal.cpp
@@ -1,7 +1,8 @@
 #include "abort_signal.hpp"
 
-#include <javascript/environment/realms.hpp>
+#include <javascript/environment/realms_2.hpp>
 #include <web_apis/dom/detail/exception_internals.hpp>
+#include <web_apis/dom/detail/observer_internals.hpp>
 #include <web_apis/dom/other/dom_exception.hpp>
 
 
@@ -31,9 +32,11 @@ auto dom::abort::abort_signal::timeout(
 
     // create a signal, and abort it after a timer has executed (TODO https://dom.spec.whatwg.org/#dom-abortsignal-timeout)
     abort_signal signal;
-    auto global = javascript::environment::realms::realm::relevant_realm(&signal).global_object<>();
-    auto callback = [] {html::detail::queueing_internals::queue_global_task();};
-    html::detail::timers::run_steps_after_timeout(global, "AbortSignal-timeout", milliseconds, callback);
+    JS_REALM_GET_RELEVANT(signal)
+
+    auto timeout_error_callback = [] {detail::exception_internals::throw_v8_exception<TIMEOUT_ERR>("Timeout");}
+    auto callback = [&relevant_global_object, timeout_error_callback] {detail::observer_internals::queue_global_task(html::detail::task_internals::timer_task_source(), relevant_global_object, timeout_error_callback);};
+    html::detail::timers::run_steps_after_timeout(relevant_global_object, "AbortSignal-timeout", milliseconds, callback);
 
     // return the signal
     return signal;
