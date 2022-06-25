f614d213d7ea083471bcb7ded3ee0311c9b5c7f0
Sam <samuelgardner101@gmail.com>
2022-06-25 23:47:18 +0100

    Created files

---
+++ src/javascript/web_apis/dom/nodes/document_type.hpp
@@ -0,0 +1,13 @@
+#ifndef SBROWSER2_DOCUMENT_TYPE_HPP
+#define SBROWSER2_DOCUMENT_TYPE_HPP
+
+#include <web_apis/dom/nodes/node.hpp>
+namespace dom::nodes {class document_type;}
+
+class dom::nodes::document_type : public node
+{
+
+};
+
+
+#endif //SBROWSER2_DOCUMENT_TYPE_HPP
