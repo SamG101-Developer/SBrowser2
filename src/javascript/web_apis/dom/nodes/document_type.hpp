bfe3fa40fd66b2a21c40cf282fc1374e1e323561
Sam <samuelgardner101@gmail.com>
2022-06-26 23:58:15 +0100

    Implemented definitions

--- src/javascript/web_apis/dom/nodes/document_type.hpp
+++ src/javascript/web_apis/dom/nodes/document_type.hpp
@@ -2,11 +2,28 @@
 #define SBROWSER2_DOCUMENT_TYPE_HPP
 
 #include <web_apis/dom/nodes/node.hpp>
+#include <web_apis/dom/mixins/child_node.hpp>
 namespace dom::nodes {class document_type;}
 
-class dom::nodes::document_type : public node
+class dom::nodes::document_type
+        : public node
+        , public mixins::child_node
 {
+public js_methods:
+    ext::property<ext::string> name;
+    ext::property<ext::string> public_id;
+    ext::property<ext::string> system_id;
 
+public cpp_methods:
+    auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
+
+private cpp_accessors:
+    [[nodiscard]] auto get_node_name() const -> ext::string override {return "#document-fragment";};
+    [[nodiscard]] auto get_node_value() const -> ext::string override {return "";};
+    [[nodiscard]] auto get_text_content() const -> ext::string override {return "";};
+
+    auto set_node_value(ext::string_view val) -> void override {};
+    auto set_text_content(ext::string_view val) -> void override {};
 };
 
 
