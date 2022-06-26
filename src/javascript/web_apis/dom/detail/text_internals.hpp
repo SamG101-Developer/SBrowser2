1d216fc049fa307099fb4caf239df98c0e5579b5
Sam <samuelgardner101@gmail.com>
2022-06-27 00:02:28 +0100

    Begin implementation

--- src/javascript/web_apis/dom/detail/text_internals.hpp
+++ src/javascript/web_apis/dom/detail/text_internals.hpp
@@ -4,6 +4,9 @@
 #include <ext/number.hpp>
 #include <ext/string.hpp>
 #include <ext/type_traits.hpp>
+
+#include <web_apis/dom/detail/exception_internals.hpp>
+#include <web_apis/dom/detail/tree_internals.hpp>
 namespace dom::nodes {class character_data;}
 namespace dom::nodes {class text;}
 
@@ -28,5 +31,21 @@ namespace dom::detail::text_internals
             ext::number_view<ulong> offset)
             -> nodes::text*;
 }
+
+
+auto dom::detail::text_internals::replace_data(
+        nodes::character_data* text_node, ext::number_view<ulong> offset, ext::number_view<ulong> count,
+        ext::string_view data) -> void
+{
+    auto length = tree_internals::length(text_node);
+
+    exception_internals::throw_v8_exception_formatted<INDEX_SIZE_ERR>(
+            "The 'offset' must be less than the length of the node",
+            {"Length of the node is smaller than it should be", "Offset is too big"},
+            {"Check that the node contains the information expected", "Make the offset smaller"},
+            {{"Length", length}, {"Offset", offset}},
+            [length, offset] {return length > offset;});
+}
+
 
 #endif //SBROWSER2_TEXT_INTERNALS_HPP
