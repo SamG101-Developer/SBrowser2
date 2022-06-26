fa0a60023845c675345ef2f03b4dc2d2b4ff3cd6
Sam <samuelgardner101@gmail.com>
2022-06-27 00:01:55 +0100

    Implemented

--- src/javascript/web_apis/dom/mixins/slottable.cpp
+++ src/javascript/web_apis/dom/mixins/slottable.cpp
@@ -1,1 +1,32 @@
 #include "slottable.hpp"
+
+#include <ext/casting.hpp>
+#include <web_apis/dom/detail/shadow_internals.hpp>
+
+
+dom::mixins::slottable::slottable()
+{
+    bind_get(assigned_slot);
+}
+
+
+auto dom::mixins::slottable::get_assigned_slot()
+        const -> html::elements::html_slot_element*
+{
+
+    // find a slot for the 'base' cast version of this class
+    auto* base = ext::cross_cast<nodes::node*>(this);
+    auto* slot = detail::shadow_internals::find_slot(base, true);
+    return slot;
+}
+
+
+auto dom::mixins::slottable::to_v8(
+        v8::Isolate* isolate)
+        const && -> ext::any
+{
+    return v8pp::class_<slottable>{isolate}
+        .inherit<web_apis::dom_object>()
+        .var("assignedSlot", &slottable::assigned_slot, false)
+        .auto_wrap_objects();
+}
