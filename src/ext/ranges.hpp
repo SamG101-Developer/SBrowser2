38568ec71d4467d1028303d7a143f70374f7e35f
Sam <samuelgardner101@gmail.com>
2022-06-27 00:01:06 +0100

    Defined new shorthand range views (`uppercase()`, `lowercase()`, `cast_to_all<T>()`, etc)

--- src/ext/ranges.hpp
+++ src/ext/ranges.hpp
@@ -3,10 +3,13 @@
 
 #include <ext/string.hpp>
 #include <ext/type_traits.hpp>
+
 #include <range/v3/range/conversion.hpp>
+#include <range/v3/algorithm/all_of.hpp>
+#include <range/v3/algorithm/any_of.hpp>
+#include <range/v3/algorithm/contains.hpp>
 #include <range/v3/view/join.hpp>
-#include <range/v3/view/enumerate.hpp>
-#include <range/v3/view/map.hpp>
+#include <range/v3/view/remove.hpp>
 #include <range/v3/view/split.hpp>
 #include <range/v3/view/transform.hpp>
 
@@ -21,13 +24,65 @@ auto operator|=(Rng& _Container, ranges::views::view_closure<_ViewFn> _ViewFunct
 }
 
 
-namespace ranges::views {auto split_string(::ext::string_view delimiter) -> range_v3_view auto;}
+namespace ranges::views {struct lowercase_fn;}
+namespace ranges::views {struct uppercase_fn;}
+namespace ranges::views {struct split_string_fn;}
+namespace ranges::views {template <typename T> struct cast_all_to_fn;}
+namespace ranges {struct contains_all_fn;}
 
-auto ranges::views::split_string(::ext::string_view delimiter) -> range_v3_view auto
+
+struct ranges::views::lowercase_fn
 {
-    return ranges::views::split(delimiter)
-            | ranges::views::transform([](range_v3_view auto&& sub_range) {return sub_range | ranges::to<::ext::string>();});
-}
+    constexpr auto operator()() const
+    {
+        return ranges::views::transform([](char c) {return std::tolower(c);});
+    }
+};
+
+
+struct ranges::views::uppercase_fn
+{
+    constexpr auto operator()() const
+    {
+        return ranges::views::transform([](char c) {return std::toupper(c);});
+    }
+};
+
+
+struct ranges::views::split_string_fn
+{
+    constexpr auto operator()(const char* delimiter) const
+    {
+        return ranges::views::split(delimiter) | ranges::views::transform([](range_v3_view auto&& sub_range) {return sub_range | ranges::to<::ext::string_view>();});
+    }
+};
+
+
+template <typename T>
+struct ranges::views::cast_all_to_fn
+{
+    constexpr auto operator()(::ext::boolean remove_nullptr = true) const // TODO : apply parameter
+    {
+        return ranges::views::transform([](auto* pointer) {return dynamic_cast<T*>(pointer);}) | ranges::views::remove(nullptr);
+    }
+};
+
+
+struct ranges::contains_all_fn
+{
+    template <typename Rng1, typename Rng2>
+    constexpr auto operator()(Rng1&& range1, Rng2&& range2) const
+    {
+        return ranges::all_of(range2, [range1](auto&& item_2) {ranges::contains(range1, item_2);});
+    }
+};
+
+
+namespace ranges::views {constexpr split_string_fn split_string;}
+namespace ranges::views {constexpr lowercase_fn lowercase;}
+namespace ranges::views {constexpr uppercase_fn uppercase;}
+namespace ranges::views {template <typename T> constexpr cast_all_to_fn<T> cast_all_to;}
+namespace ranges {constexpr contains_all_fn contains_all;}
 
 
 #endif //SBROWSER2_RANGES_HPP
