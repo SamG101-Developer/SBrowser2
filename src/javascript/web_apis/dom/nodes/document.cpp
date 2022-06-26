a1333b7cce2370dd2d78ed4aa653ae29503326e6
Sam <samuelgardner101@gmail.com>
2022-06-26 23:58:30 +0100

    Began constructor definition

--- src/javascript/web_apis/dom/nodes/document.cpp
+++ src/javascript/web_apis/dom/nodes/document.cpp
@@ -1,1 +1,13 @@
 #include "document.hpp"
+
+#include <url/url.hpp>
+
+
+dom::nodes::document::document()
+        : url{std::make_unique<url::url>("about:blank")}
+        , implementation{std::make_smart<other::dom_implementation>()}
+        , content_type{"application/xml"}
+        , ready_state{"complete"}
+{
+
+}
