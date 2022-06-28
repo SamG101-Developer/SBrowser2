0290e1e17219c83a16a3a3b35d18276fad30c765
Sam <samuelgardner101@gmail.com>
2022-06-28 19:55:35 +0100

    Moved declarations to .cpp file

--- src/javascript/web_apis/dom/detail/namespace_internals.hpp
+++ src/javascript/web_apis/dom/detail/namespace_internals.hpp
@@ -1,22 +1,18 @@
 #ifndef SBROWSER2_NAMESPACE_INTERNALS_HPP
 #define SBROWSER2_NAMESPACE_INTERNALS_HPP
 
-#include <ext/ranges.hpp>
 #include <ext/string.hpp>
-#include <dom/detail/exception_internals.hpp>
-#include <range/v3/algorithm/contains.hpp>
-using namespace std::literals::string_view_literals;
 
 
 namespace dom::detail::namespace_internals
 {
-    inline static ext::string_view HTML   = "http://www.w3.org/1999/xhtml";
-    inline static ext::string_view MATHML = "http://www.w3.org/1998/Math/MathML";
-    inline static ext::string_view SVG    = "http://www.w3.org/2000/svg";
-    inline static ext::string_view XLINK  = "http://www.w3.org/1999/xlink";
-    inline static ext::string_view XML    = "http://www.w3.org/XML/1998/namespace";
-    inline static ext::string_view XMLNS  = "http://www.w3.org/2000/xmlns/";
-    inline static ext::string_view NONE   = "";
+    inline static constexpr const char* HTML   = "http://www.w3.org/1999/xhtml";
+    inline static constexpr const char* MATHML = "http://www.w3.org/1998/Math/MathML";
+    inline static constexpr const char* SVG    = "http://www.w3.org/2000/svg";
+    inline static constexpr const char* XLINK  = "http://www.w3.org/1999/xlink";
+    inline static constexpr const char* XML    = "http://www.w3.org/XML/1998/namespace";
+    inline static constexpr const char* XMLNS  = "http://www.w3.org/2000/xmlns/";
+    inline static constexpr const char* NONE   = "";
 
     // validation checks
     auto validate(
@@ -27,50 +23,6 @@ namespace dom::detail::namespace_internals
             ext::string_view namespace_,
             ext::string_view qualified_name)
             -> std::tuple<ext::string, ext::string>;
-}
-
-
-auto dom::detail::namespace_internals::validate_and_extract(
-        ext::string_view namespace_, ext::string_view qualified_name) -> std::tuple<ext::string, ext::string>
-{
-    // validate the name, and initialize the prefix to the empty string, and the local name defaults to the qualified
-    // name - if there is no prefix, then the local name is the whole qualified name
-    validate(qualified_name);
-    ext::string prefix, local_name = qualified_name;
-
-    // if there is a colon in the qualified name, then there is a prefix and a local name contained in the qualified
-    // name, so split the qualified name on the colon, and set the prefix and local name to the first 2 results
-    if (ranges::contains(qualified_name, ":"sv))
-    {
-        auto split_on_colon = qualified_name | ranges::views::split_string(":");
-        prefix     = *(  split_on_colon.begin());
-        local_name = *(++split_on_colon.begin());
-    }
-
-
-    // perform some checks on the prefix, namespace and qualified names, to make sure everything is valid, such as the
-    // namespace and prefix can't both be empty, and certain variables have to conform to certain constraints,
-    // especially concerning xml / xmlns prefixing
-
-    exception_internals::throw_v8_exception_formatted<NAMESPACE_ERR>(
-            [prefix, namespace_] {return prefix.empty() && namespace_.empty();},
-            "Prefix and namespace must not both be empty");
-
-    exception_internals::throw_v8_exception_formatted<NAMESPACE_ERR>(
-            [prefix, namespace_] {return prefix == "xml" && namespace_ == namespace_internals::XML;},
-            "Prefix and namespace must match (xml prefix)");
-
-    exception_internals::throw_v8_exception_formatted<NAMESPACE_ERR>(
-            [prefix, namespace_, qualified_name] {return (prefix == "xmlns" || qualified_name == "xmlns") && namespace_ != namespace_internals::XMLNS;},
-            "Prefix / qualified_name and namespace must match (xmlns prefix / qualified_name)");
-
-    exception_internals::throw_v8_exception_formatted<NAMESPACE_ERR>(
-            [prefix, namespace_, qualified_name] {return (prefix != "xmlns" && qualified_name != "xmlns") && namespace_ == namespace_internals::XMLNS;},
-            "prefix / qualified_name and namespace must match (xmlns prefix / qualified_name)");
-
-    // return the prefix and local name - namespace_ and qualified name don't change, so they can be got from wherever
-    // the function was called from
-    return {prefix, local_name};
 }
 
 
--- src/javascript/web_apis/dom/detail/observer_internals.hpp
+++ src/javascript/web_apis/dom/detail/observer_internals.hpp
@@ -1,18 +1,14 @@
 #ifndef SBROWSER2_OBSERVER_INTERNALS_HPP
 #define SBROWSER2_OBSERVER_INTERNALS_HPP
 
-#include <ext/ranges.hpp>
-#include <javascript/environment/realms_2.hpp>
-
-#include <web_apis/dom/nodes/node.hpp>
-#include <web_apis/dom/nodes/window.hpp>
-#include <web_apis/dom/mutations/mutation_observer.hpp>
-#include <web_apis/dom/mutations/mutation_record.hpp>
-
-#include <magic_enum.hpp>
-#include <range/v3/algorithm/contains.hpp>
-#include <range/v3/view/remove.hpp>
-namespace html::elements {class html_element; class html_media_element;} // TODO <- IMPORT
+#include <memory>
+#include <ext/string.hpp>
+#include <ext/vector.hpp>
+#include <v8-platform.h>
+namespace dom::mutations {class mutation_observer;}
+namespace dom::nodes {class node;}
+namespace dom::nodes {class document;}
+namespace html::elements {class html_element; class html_media_element;}
 
 
 namespace dom::detail::observer_internals
@@ -51,16 +47,16 @@ namespace dom::detail::observer_internals
             ext::string_view name,
             ext::string_view namespace_,
             ext::string_view old_value,
-            ext::vector<nodes::node*>& added_nodes,
-            ext::vector<nodes::node*>& removed_nodes,
+            const ext::vector<nodes::node*>& added_nodes,
+            const ext::vector<nodes::node*>& removed_nodes,
             nodes::node* previous_sibling,
             nodes::node* next_sibling)
             -> void;
 
     auto queue_tree_mutation_record(
             nodes::node* target,
-            ext::vector<nodes::node*>& added_nodes,
-            ext::vector<nodes::node*>& removed_nodes,
+            const ext::vector<nodes::node*>& added_nodes,
+            const ext::vector<nodes::node*>& removed_nodes,
             nodes::node* previous_sibling,
             nodes::node* next_sibling)
             -> void;
@@ -92,195 +88,5 @@ namespace dom::detail::observer_internals
             steps_t&& steps)
             -> void;
 }
-
-
-auto dom::detail::observer_internals::notify_mutation_observers() -> void
-{
-    // set the 'mutation_observer_microtask_queued' variable to false, and get the 'notify_set' (MutationObserver set),
-    // and the signal set (HTMLSlotElement set). clear the 'signal_set' in the environment (clone is saved locally)
-
-    JS_REALM_GET_SURROUNDING(nullptr)
-    using notify_set_t = ext::set<mutations::mutation_observer*>;
-    using signal_set_t = ext::set<html::elements::html_slot_element*>;
-
-    javascript::environment::realms_2::set(surrounding_global_object, "mutation_observer_microtask_queued", false);
-    auto notify_set = javascript::environment::realms_2::get<notify_set_t>(surrounding_global_object, "notify_observers");
-    auto signal_set = javascript::environment::realms_2::get<signal_set_t>(surrounding_global_object, "signal_slots");
-    javascript::environment::realms_2::set(surrounding_global_object, "signal_slots", signal_set_t{});
-
-    // iterate every MutationObserver in the 'notify_set'
-    for (mutations::mutation_observer* mo: notify_set)
-    {
-
-        // empty the record queue in each MutationObserver, by swapping the list held in the pointer with an empty list;
-        // the queue of records is now stored locally
-        std::queue<mutations::mutation_record> records;
-        mo->m_record_queue->swap(records);
-
-        // iterate each node in the MutationObserver's node list, and for each transient registered observer (a
-        // registered observer who has a source) linked to the node, remove it if its observer is 'mo' - this means that
-        // any observers whose 'observer' link is 'mo', and who belong to a node that is tied to 'mo' are removed
-        // TODO : why?
-        for (nodes::node* node: *mo->m_node_list)
-            *node->m_registered_observer_list
-                    |= ranges::views::cast_all_to<transient_registered_observer>()
-                    | ranges::views::remove_if([mo](transient_registered_observer* observer) {return observer->observer.get() == mo;});
-
-        // if there are any MutationRecords from the JavaScript environment, call 'mo''s callback with the list of
-        // records, reporting any exceptions that are caught during the process
-        if (!records.empty())
-        {
-            JS_EXCEPTION_HANDLER;
-            mo->m_callback(records, mo);
-
-            if (JS_EXCEPTION_HAS_THROWN)
-                console::reporting::report_exception(JS_EXCEPTION_MESSAGE)
-        }
-    }
-
-    // fire a "slotchange" event at every slot in the JavaScript environment list TODO : why
-    for (html::elements::html_slot_element* slot: signal_set)
-        event_internals::fire_event<>(slot, "slotchange", {"bubbles", true});
-}
-
-
-auto dom::detail::observer_internals::queue_microtask(
-        steps_t&& steps, v8::Isolate* event_loop, nodes::document* document) -> void
-{
-    JS_BLOCK_ENTER
-        JS_REALM_GET_IMPLIED
-        // set the 'event loop' and 'document' to the implied objects (via the implied realm) TODO
-        event_loop = event_loop ? event_loop : implied_agent;
-        document = document ? document : implied_document();
-
-        // create a microtask and assign it the relevant data TODO
-        v8::Local<v8::Function> microtask = v8::Function::New(event_loop->GetCurrentContext(), steps);
-        microtask->Set(event_loop->GetCurrentContext(), v8pp::to_v8(event_loop, "source")  , microtask->TaskSource());
-        microtask->Set(event_loop->GetCurrentContext(), v8pp::to_v8(event_loop, "document"), v8pp::to_v8(event_loop, document));
-        microtask->Set(event_loop->GetCurrentContext(), v8pp::to_v8(event_loop, "set"), v8pp::to_v8(event_loop, ext::set<void*>{}));
-
-        // enqueue the microtask to the event loop
-        event_loop->EnqueueMicrotask(microtask);
-    JS_BLOCK_EXIT
-}
-
-
-auto dom::detail::observer_internals::queue_mutation_record(
-        mutation_type_t type, nodes::node* target, ext::string_view name, ext::string_view namespace_,
-        ext::string_view old_value, ext::vector<nodes::node*>& added_nodes, ext::vector<nodes::node*>& removed_nodes,
-        nodes::node* previous_sibling, nodes::node* next_sibling) -> void
-{
-    // created a map of MutationObservers to strings, to save observers of interest, and get all the ancestors of the
-    // target node, as these nodes' registered observer lists contains the MutationObserver objects that may be of
-    // interest
-    ext::map<mutations::mutation_observer*, ext::string> interested_observers {};
-    auto nodes = tree_internals::ancestors(target);
-
-    // iterate each MutationObserver for each node
-    for (nodes::node* node: nodes)
-    {
-        for (registered_observer* registered: *node->m_registered_observer_list)
-        {
-            // if the conditions based on the 'type' and 'options' dictionary are valid, then modify the
-            // 'interested_observers' map
-            auto options = registered->options;
-            if (type == ATTRIBUTES && options.at("attributes").has_value() && (ranges::contains(options.at("attributeFilter").value_to_or<ext::string_vector>({}), name) || namespace_)
-                    || node != target && options.at("subtree").has_value_and_equals(false)
-                    || type == CHILD_LIST && options.at("childList").has_value_and_equals(false)
-                    || type == ATTRIBUTES && options.at("attributes").empty_or_equals(false)
-                    || type == CHARACTER_DATA && options.at("characterData").empty_or_equals(false))
-            {
-                auto mo = registered->observer.get();
-
-                // if the MutationObserver is not in the map, then insert it with an empty string as the corresponding
-                // value
-                if (!ranges::contains(interested_observers | ranges::views::keys, mo))
-                    interested_observers.insert_or_assign(mo, "");
-
-                // if there is a valid old value supplied in the 'options' dictionary that matches the 'type', then set
-                // the value of the MutationObserver to the old value
-                if (type == ATTRIBUTES && options.at("attributeOldValue").has_value_and_equals(true) || type == CHARACTER_DATA && options.at("characterDataOldValue").has_value_and_equals(true))
-                    interested_observers.insert_or_assign(mo, old_value);
-            }
-        }
-    }
-
-    // iterate over each interested observer, and emplace a mutation record into the observer based on the parameters
-    // and the 'mapped_old_value' string
-    for (const auto& [observer, mapped_old_value]: interested_observers)
-        observer->m_record_queue->emplace(
-                magic_enum::enum_name(type) | ranges::views::transform([](char c) {return tolower(c);}),
-                name, namespace_, mapped_old_value, target,
-                previous_sibling, next_sibling, added_nodes, removed_nodes);
-
-    // queue a mutation observers microtask
-    queue_mutation_observer_microtask();
-}
-
-
-auto dom::detail::observer_internals::queue_tree_mutation_record(
-        nodes::node* target, ext::vector<nodes::node*>& added_nodes, ext::vector<nodes::node*>& removed_nodes,
-        nodes::node* previous_sibling, nodes::node* next_sibling) -> void
-{
-    // there has to be an element in either the 'added_nodes' or 'removed_nodes' list, as otherwise there is no mutation
-    // happening; this method is just a wrapper around 'queue_mutation_record', so call that method with some parameters
-    // set to fixed values
-    assert(!added_nodes.empty() || !removed_nodes.empty());
-    queue_mutation_record(CHILD_LIST, target, "", "", "", added_nodes, removed_nodes, previous_sibling, next_sibling);
-}
-
-
-auto dom::detail::observer_internals::queue_mutation_observer_microtask() -> void
-{
-    JS_REALM_GET_SURROUNDING(nullptr)
-
-    // get if the surrounding global object's 'mutation_observer_microtask_queued' attribute is set to true; return
-    // early if it is, otherwise set it to true, and queue a microtask to 'notify_mutation_observers()'
-    auto queued = javascript::environment::realms_2::get<ext::boolean>(surrounding_global_object, "mutation_observer_microtask_queued");
-    return_if(queued);
-    javascript::environment::realms_2::set(surrounding_global_object, "mutation_observer_microtask_queued", true);
-
-    queue_microtask(&notify_mutation_observers);
-}
-
-
-auto dom::detail::observer_internals::queue_task(
-        v8::Task& task_source, steps_t&& steps, v8::Isolate* event_loop, nodes::document* document) -> void
-{
-    JS_BLOCK_ENTER
-        JS_REALM_GET_IMPLIED
-        event_loop = event_loop ? event_loop : implied_agent;
-        document = document ? document : implied_document();
-
-        v8::Local<v8::Function> microtask = v8::Function::New(event_loop->GetCurrentContext(), steps);
-        microtask->Set(event_loop->GetCurrentContext(), v8pp::to_v8(event_loop, "source"), task_source);
-        microtask->Set(event_loop->GetCurrentContext(), v8pp::to_v8(event_loop, "document"), v8pp::to_v8(event_loop, document));
-        microtask->Set(event_loop->GetCurrentContext(), v8pp::to_v8(event_loop, "set"), v8pp::to_v8(event_loop, ext::set<void*>{}));
-
-        // enqueue the microtask to the event loop
-        event_loop->EnqueueMicrotask(microtask);
-    JS_BLOCK_EXIT
-}
-
-
-auto dom::detail::observer_internals::queue_global_task(
-        v8::Task& task_source, v8::Local<v8::Object> global_object, steps_t&& steps) -> void
-{
-    JS_REALM_GET_RELEVANT(global_object);
-    auto* document = javascript::environment::realms_2::get<ext::string>(global_object, "type") == "Window"
-            ? javascript::environment::realms_2::get<nodes::document*>(global_object, "associated_document")
-            : nullptr;
-
-    queue_task(task_source, std::forward<steps_t>(steps), relevant_agent, document);
-}
-
-
-auto dom::detail::observer_internals::queue_element_task(
-        v8::Task& task_source, html::elements::html_element* element, steps_t&& steps) -> void
-{
-    JS_REALM_GET_RELEVANT(element)
-    queue_global_task(task_source, relevant_global_object, std::forward<steps_t>(steps));
-}
-
 
 #endif //SBROWSER2_OBSERVER_INTERNALS_HPP
