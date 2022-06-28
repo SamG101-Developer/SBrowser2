1d9851ec5a6c8fd56a4f9d834bac5dbcdf9a8007
Sam <samuelgardner101@gmail.com>
2022-06-28 19:55:15 +0100

    Moved declarations to .cpp file, Simplified types (`ext::vector<ext::string>&&` -> `ext::string_vector&&`)

--- src/javascript/web_apis/dom/detail/exception_internals.hpp
+++ src/javascript/web_apis/dom/detail/exception_internals.hpp
@@ -1,11 +1,8 @@
 #ifndef SBROWSER2_EXCEPTION_INTERNALS_HPP
 #define SBROWSER2_EXCEPTION_INTERNALS_HPP
 
-#include <functional>
-#include <tuple>
 #include <ext/string.hpp>
-
-#include <range/v3/algorithm/for_each.hpp>
+#include <ext/vector.hpp>
 
 #define NO_CONDITION [](){return true;}
 #define P(key, val) std::make_pair(key, val)
@@ -43,70 +40,10 @@ namespace dom::detail::exception_internals
     auto throw_v8_exception_formatted(
             exception_condiditional_t&& condition = NO_CONDITION,
             ext::string_view description = "",
-            ext::vector<ext::string>&& possible_causes = {},
-            ext::vector<ext::string>&& possible_fixes = {},
+            ext::string_vector&& possible_causes = {},
+            ext::string_vector&& possible_fixes = {},
             auto&&... object_information)
             -> void;
-}
-
-
-template <v8_primitive_error_t exception_type>
-auto dom::detail::exception_internals::throw_v8_exception(
-        exception_condiditional_t&& condition, ext::string_view exception_message) -> void
-{
-    if (std::move(condition)())
-    {
-        // create the v8 primitive exception object, and set its type based on the enum value
-        v8::Local<v8::Value> v8_primitive_exception_object;
-        switch(exception_type)
-        {
-            case V8_TYPE_ERROR: v8_primitive_exception_object = v8::Exception::TypeError(exception_message);
-            case V8_RANGE_ERROR: v8_primitive_exception_object = v8::Exception::RangeError(exception_message);
-            case V8_REFERENCE_ERROR: v8_primitive_exception_object = v8::Exception::ReferenceError(exception_message);
-            case V8_SYNTAX_ERROR: v8_primitive_exception_object = v8::Exception::SyntaxError(exception_message);
-            case V8_WASM_COMPILE_ERROR: v8_primitive_exception_object = v8::Exception::WasmCompileError(exception_message);
-            case V8_WASM_LINK_ERROR: v8_primitive_exception_object = v8::Exception::WasmLinkError(exception_message);
-            case V8_WASM_RUNTIME_ERROR: v8_primitive_exception_object = v8::Exception::WasmRuntimeError(exception_message);
-            default: v8_primitive_exception_object = v8::Exception::Error(exception_message);
-        }
-
-        // the condition has already been asserted, so throw the exception
-        v8::Isolate::GetCurrent()->ThrowException(v8_primitive_exception_object);
-    }
-}
-
-
-template <v8_custom_error_t exception_type>
-auto dom::detail::exception_internals::throw_v8_exception_formatted(
-        exception_condiditional_t&& condition, ext::string_view description, ext::vector<ext::string>&& possible_causes,
-        ext::vector<ext::string>&& possible_fixes, auto&& ...object_information) -> void
-{
-    if (std::move(condition)())
-    {
-        // start the error message with the description
-        ext::string exception_message = description;
-
-        // bullet point the possible causes (if there are any)
-        if (!possible_causes.empty())
-        {
-            exception_message += "\n\nPossible causes:\n";
-            ranges::for_each(std::move(possible_causes), [&exception_message](ext::string&& cause) {exception_message += "\t-" + cause + "\n";});
-        }
-
-        // bullet point the possible fixes (if there are any)
-        if (!possible_fixes.empty())
-        {
-            exception_message += "\n\nPossible fixes:\n";
-            ranges::for_each(std::move(possible_fixes), [&exception_message](ext::string&& fix) {exception_message += "\t-" + fix + "\n";});
-        }
-
-        // bullet point the relevant information / memory addresses of objects
-        exception_message += "\n\nRelevant information / memory addresses:\n";
-        ranges::for_each(object_information..., [&exception_message]<typename K, typename V>(std::pair<K, V>&& info) {exception_message += std::move(info).first + ": " + std::move(info).second;});
-
-        // the 'condition' has already been asserted, so throw the exception
-        v8::Isolate::GetCurrent()->ThrowError(exception_message);
-    }
 }
 
 
