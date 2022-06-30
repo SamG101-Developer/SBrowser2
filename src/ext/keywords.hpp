bd048a2c449ecd4b6b9b726cdc9ccb27d679e953
Sam <samuelgardner101@gmail.com>
2022-06-30 23:59:27 +0100

    Added `for_if` as syntactic sugar

--- src/ext/keywords.hpp
+++ src/ext/keywords.hpp
@@ -37,6 +37,7 @@
 #define return_if(condition) if (condition) return
 #define break_if(condition) if (condition) break
 #define continue_if(condition) if (condition) continue
+#define for_if(if_statement, for_statement) if (if_statement) for (for_statement)
 
 
 #endif //SBROWSER2_KEYWORDS_HPP
