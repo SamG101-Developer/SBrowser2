0ceb39acd3680ac77265ac3c236064cebddccc22
Sam <samuelgardner101@gmail.com>
2022-07-03 23:35:26 +0100

    Renamed concepts, got rid of uneccessary `requires`

--- src/ext/concepts.hpp
+++ src/ext/concepts.hpp
@@ -5,19 +5,16 @@
 
 
 // check if a templated class is a base of another class (don't need template type to check)
-template <template <typename> typename _Base, typename _Derived>
+template <template <typename> typename _BaseMixin, typename _Derived>
 concept inherit_template = requires(_Derived _Obj)
 {
-    []<typename _Ty>(const _Base<_Ty>&){}(_Obj);
+    []<typename _Ty>(const _BaseMixin<_Ty>&){}(_Obj);
 };
 
 
 // check if a class is a base of another class
 template <typename _Base, typename _Derived>
-concept inherit = requires (_Derived)
-{
-    std::is_base_of_v<_Base, _Derived>;
-};
+concept inherit = std::is_base_of_v<_Base, _Derived>;
 
 
 // check if a type is iterable ie does it have .begin() and .end() iterator access
@@ -46,50 +43,27 @@ concept dynamically_castable_to = requires (_Tx _PtrL)
 
 // check if a type is a range_v3 type
 template <typename _Tx>
-concept range_v3_view = requires(_Tx&& range)
-{
-    ranges::view_<_Tx>;
-};
+concept range_v3_view = ranges::view_<_Tx>;
 
 
-// check if 1 type matches any types
-template <typename ..._TypesToCheckAgainst, typename ..._TypesToCheck>
-concept same_as_any = requires(_TypesToCheck...)
-{
-    ((std::same_as<_TypesToCheck, _TypesToCheckAgainst> || ...) && ...);
-};
+// check if a type(s) is another type, works for parameter packs too - are all arguments an integer
+template <typename TypeToCheck, typename TypeToCheckAgainst>
+concept type_is = std::same_as<std::remove_cvref_t<TypeToCheck>, TypeToCheckAgainst>;
 
 
-// check if all types match 1 type
-template <typename _Tx, typename ..._Valty>
-concept all_same_as = requires(_Valty...)
-{
-    (std::same_as<_Valty, _Tx> && ...);
-};
-
-
-// check if any types match 1 type
-template <typename _Tx, typename ..._Valty>
-concept any_same_as = requires(_Valty...)
-{
-    (std::same_as<_Valty, _Tx> || ...);
-};
+// check if a type(s) is in a list of types, works for parameter pack too - are all arguments a string or node?
+template <typename TypeToCheck, typename ...TypesToCheckAgainst>
+concept type_in = (std::same_as<std::remove_cvref_t<TypeToCheck>, TypesToCheckAgainst> || ...);
 
 
 // check if a type is a primitive numeric type
 template <typename _Tx>
-concept primitive_numeric = requires(_Tx)
-{
-    std::integral<_Tx> || std::floating_point<_Tx>;
-};
+concept primitive_numeric = std::integral<_Tx> || std::floating_point<_Tx>;
 
 
 // check if a type is callable
 template <typename _Tx>
-concept callable = requires(_Tx)
-{
-    std::invocable<_Tx>;
-};
+concept callable = std::invocable<_Tx>;
 
 
 #endif //SBROWSER2_CONCEPTS_HPP
