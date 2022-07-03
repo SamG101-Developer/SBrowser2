d2f1cb293885b8ea9b7293e189e2f1627b850c69
Sam <samuelgardner101@gmail.com>
2022-07-03 23:37:52 +0100

    Fixed all operators in the class

--- src/ext/number.hpp
+++ src/ext/number.hpp
@@ -5,6 +5,9 @@
 #include <ext/type_traits.hpp>
 namespace ext {template <primitive_numeric _Tx> class number;}
 namespace ext {template <primitive_numeric _Vt> using number_view = const number<_Vt>&;}
+namespace ext {template <bool _InclusiveLo = false, bool _InclusiveHi = false> auto is_between(auto&& _Val, auto&& _Lo, auto&& _Hi) -> ext::boolean;}
+namespace ext {template <typename T, typename U, typename ...V> auto min(T&& first, U&& second, V&&... values);}
+namespace ext {template <typename T, typename U, typename ...V> auto max(T&& first, U&& second, V&&... values);}
 
 #include <algorithm>
 #include <limits>
@@ -21,57 +24,127 @@ template <primitive_numeric _Tx>
 class ext::number final
 {
 public friends:
-    template<primitive_numeric _Vt> friend class number;
+    template <primitive_numeric _Vt> friend class number;
 
 public constructors:
-    number(): _Val(0) {};
-    ~number() {_Val = 0;};
+    number() = default;
+    template <primitive_numeric _Ty> number(_Ty _Primitive);
+    template <primitive_numeric _Ty> auto operator=(_Ty _Primitive) -> number&;
 
-    number(const number& _Other): _Val(_Other._Val) {}
-    number(number&& _Other) noexcept: _Val(std::move(_Other._Val)) {}
-    auto operator=(const number& _Other) -> number& {_Val = _Other._Val; return *this;}
-    auto operator=(number&& _Other) noexcept -> number& {_Val = std::move(_Other._Val); return *this;}
-
-    number(const _Tx& _Other) : _Val(_Other) {};
-    number(_Tx&& _Other) noexcept : _Val(std::forward<_Tx>(_Other)) {}
-    auto operator=(const _Tx&) = delete;
-    auto operator=(_Tx&& _Other) noexcept -> number& {_Val = std::forward<_Tx>(_Other);}
-
-    template <primitive_numeric _Ty> explicit number(const number<_Ty>& _Other): _Val(static_cast<_Tx>(_Other._Val)) {}
-    template <primitive_numeric _Ty> explicit number(number<_Ty>&& _Other) noexcept: _Val(static_cast<_Tx>(std::move(_Other._Val))) {}
-    template <primitive_numeric _Ty> auto operator=(const number<_Ty>& _Other) -> number& {_Val = static_cast<_Tx>(_Other._Val); return *this;}
-    template <primitive_numeric _Ty> auto operator=(number<_Ty>&& _Other) noexcept -> number& {_Val = static_cast<_Tx>(std::move(_Other._Val)); return *this;}
-
+    template <primitive_numeric _Ty> explicit number(const number<_Ty>& _Other);
+    template <primitive_numeric _Ty> explicit number(number<_Ty>&& _Other) noexcept;
+    template <primitive_numeric _Ty> auto operator=(const number<_Ty>& _Other) -> number&;
+    template <primitive_numeric _Ty> auto operator=(number<_Ty>&& _Other) noexcept -> number&;
+    
 public cpp_methods:
-    auto min() const -> ext::number<_Tx> {return std::numeric_limits<_Tx>::min();}
-    auto max() const -> ext::number<_Tx> {return std::numeric_limits<_Tx>::max();}
+    auto min() const -> number<_Tx> {return std::numeric_limits<_Tx>::min();}
+    auto max() const -> number<_Tx> {return std::numeric_limits<_Tx>::max();}
 
 public cpp_static_methods:
     static auto INF() -> detail::infinity<_Tx> {return detail::infinity<_Tx>{};};
 
 public cpp_operators:
-    auto operator+(primitive_numeric auto _Other) const -> auto {return number{_Val + _Other};}
-    auto operator-(primitive_numeric auto _Other) const -> auto {return number{_Val - _Other};}
-    auto operator*(primitive_numeric auto _Other) const -> auto {return number{_Val * _Other};}
-    auto operator/(primitive_numeric auto _Other) const -> auto {return number{_Val / _Other};}
-    auto operator%(primitive_numeric auto _Other) const -> auto {return number{_Val % _Other};}
-
-    auto operator+=(primitive_numeric auto _Other) -> auto& {_Val += _Other; return *this;}
-    auto operator-=(primitive_numeric auto _Other) -> auto& {_Val -= _Other; return *this;}
-    auto operator*=(primitive_numeric auto _Other) -> auto& {_Val *= _Other; return *this;}
-    auto operator/=(primitive_numeric auto _Other) -> auto& {_Val /= _Other; return *this;}
-    auto operator%=(primitive_numeric auto _Other) -> auto& {_Val %= _Other; return *this;}
-
-    auto operator== (primitive_numeric auto _Other) const -> ext::boolean {return _Val == static_cast<_Tx>(_Other);}
-    auto operator<=>(primitive_numeric auto _Other) const -> auto {return _Val <=> static_cast<_Tx>(_Other);}
-
+    // conversion operators
     explicit operator std::string() const {return std::to_string(_Val);}
-    operator _Tx() const {return _Val;}
-    template <primitive_numeric _Ty> operator number<_Ty>() {return number<_Ty>{static_cast<_Ty>(_Val)};}
+
+    auto operator+(auto&& other) const {return number{_Val + std::forward<_Tx>(other)};}
+    auto operator-(auto&& other) const {return number{_Val - std::forward<_Tx>(other)};}
+    auto operator*(auto&& other) const {return number{_Val * std::forward<_Tx>(other)};}
+    auto operator/(auto&& other) const {return number{_Val / std::forward<_Tx>(other)};}
+    auto operator%(auto&& other) const {return number{_Val % std::forward<_Tx>(other)};}
+
+    auto operator+=(auto&& other) {_Val += std::forward<_Tx>(other); return *this;}
+    auto operator-=(auto&& other) {_Val -= std::forward<_Tx>(other); return *this;}
+    auto operator*=(auto&& other) {_Val *= std::forward<_Tx>(other); return *this;}
+    auto operator/=(auto&& other) {_Val /= std::forward<_Tx>(other); return *this;}
+    auto operator%=(auto&& other) {_Val %= std::forward<_Tx>(other); return *this;}
+
+    auto operator==(const auto& other) const -> bool {return _Val == other;}
+    auto operator<=>(const auto& other) const {return _Val <=> other;}
+
+    template <primitive_numeric _Ty> operator number<_Ty>() const {return number<_Ty>(_Val);}
+    template <primitive_numeric _Ty> operator _Ty() {return _Val;}
+
+private:
+    auto operator*() const -> _Tx {return _Val;}
 
 private cpp_properties:
     _Tx _Val;
 };
+
+
+template <primitive_numeric _Tx>
+template <primitive_numeric _Ty>
+ext::number<_Tx>::number(_Ty _Primitive)
+        : _Val(_Primitive)
+{}
+
+
+template <primitive_numeric _Tx>
+template <primitive_numeric _Ty>
+auto ext::number<_Tx>::operator=(_Ty _Primitive) -> number&
+{
+    _Val = _Primitive;
+    return *this;
+}
+
+
+template <primitive_numeric _Tx>
+template <primitive_numeric _Ty>
+ext::number<_Tx>::number(const number<_Ty>& _Other)
+        : _Val(*_Other)
+{}
+
+
+template <primitive_numeric _Tx>
+template <primitive_numeric _Ty>
+ext::number<_Tx>::number(number<_Ty>&& _Other) noexcept
+        : _Val(*std::move(_Other))
+{}
+
+
+template <primitive_numeric _Tx>
+template <primitive_numeric _Ty>
+auto ext::number<_Tx>::operator=(const ext::number<_Ty>& _Other) -> number&
+{
+    _Val = *_Other;
+    return *this;
+}
+
+
+template <primitive_numeric _Tx>
+template <primitive_numeric _Ty>
+auto ext::number<_Tx>::operator=(number<_Ty>&& _Other) noexcept -> number&
+{
+    _Val = *std::move(_Other);
+    return *this;
+}
+
+
+
+template <bool _InclusiveLo, bool _InclusiveHi>
+auto ext::is_between(auto&& _Val, auto&& _Lo, auto&& _Hi) -> ext::boolean
+{
+    auto _Condition1 = _InclusiveLo ? _Val >= _Lo : _Val > _Lo; // default to >
+    auto _Condition2 = _InclusiveHi ? _Val <= _Hi : _Val < _Hi; // default to <
+    return _Condition1 && _Condition2;
+}
+
+
+template <typename T, typename U, typename ...V>
+auto ext::min(T&& first, U&& second, V&&... values)
+{
+    if constexpr(sizeof...(values) == 0) return first < second ? first : number{second};
+    else return min(min(std::forward<T>(first), std::forward<U>(second)), std::forward<V>(values)...);
+}
+
+
+template <typename T, typename U, typename ...V>
+auto ext::max(T&& first, U&& second, V&&... values)
+{
+    if constexpr(sizeof...(values) == 0) return first > second ? first : number{second};
+    else return max(max(std::forward<T>(first), std::forward<U>(second)), std::forward<V>(values)...);
+}
 
 
 #endif //SBROWSER2_NUMBER_HPP
