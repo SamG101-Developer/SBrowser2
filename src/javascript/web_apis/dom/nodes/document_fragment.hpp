78f25e3d3f27f9646e4300405642bd4168809de4
Sam <samuelgardner101@gmail.com>
2022-07-04 11:50:16 +0100

    Added override methods so DocumentFragment isn't an abstract class

--- src/javascript/web_apis/dom/nodes/document_fragment.hpp
+++ src/javascript/web_apis/dom/nodes/document_fragment.hpp
@@ -12,7 +12,16 @@ class dom::nodes::document_fragment
         , public mixins::non_element_parent_node
         , public mixins::parentable_node
 {
+public cpp_methods:
+    auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
 
+private cpp_accessors:
+    [[nodiscard]] auto get_node_name() const -> ext::string override {return "#document-fragment";};
+    [[nodiscard]] auto get_node_value() const -> ext::string override;
+    [[nodiscard]] auto get_text_content() const -> ext::string override {return "";}
+
+    auto set_node_value(ext::string_view val) -> void override {};
+    auto set_text_content(ext::string_view val) -> void override;
 };
 
 
