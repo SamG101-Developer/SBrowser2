500eb413facb478ac4f2d0442f04079cd0cc9b0a
Sam <samuelgardner101@gmail.com>
2022-07-06 00:00:29 +0100

    Implemented definitions

--- src/javascript/web_apis/url/url.hpp
+++ src/javascript/web_apis/url/url.hpp
@@ -7,8 +7,14 @@ namespace url {class url_object;}
 
 class url::url_object : public virtual web_apis::dom_object
 {
+public constructors:
+    url_object(ext::string_view url_string);
+
 public cpp_operators:
     auto operator=(ext::string_view url) -> url_object&;
+
+public cpp_methods:
+    auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
 };
 
 
