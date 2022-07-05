db72cf1a2d1e00500687b0f4d070926093924988
Sam <samuelgardner101@gmail.com>
2022-07-05 23:59:41 +0100

    Implemented last of declarations

--- src/javascript/web_apis/dom/nodes/text.cpp
+++ src/javascript/web_apis/dom/nodes/text.cpp
@@ -1,6 +1,10 @@
 #include "text.hpp"
 
 #include <web_apis/dom/detail/text_internals.hpp>
+#include <web_apis/dom/detail/tree_internals.hpp>
+
+#include <range/v3/view/transform.hpp>
+#include <range/v3/to_container.hpp>
 
 #include <QtWidgets/QVBoxLayout>
 
@@ -30,8 +34,19 @@ auto dom::nodes::text::split_text(
         ext::number_view<ulong> offset)
         -> text*
 {
-    // split the text node at the index 'offset'
+    // split the text node at the index 'offset', and return the 2nd half of the split, ie a Text node containing all
+    // data after the split (this Text node remains the same)
     return dom::detail::text_internals::split(this, offset);
+}
+
+
+auto dom::nodes::text::get_whole_text()
+        const -> ext::string
+{
+    // the whole text of a Text node is the contiguous node data ie the combined data of all the contiguous Text nodes
+    dom::detail::tree_internals::contiguous_text_nodes(this)
+            | ranges::views::transform([](nodes::text* node) {return node->data();})
+            | ranges::to<ext::string>();
 }
 
 
