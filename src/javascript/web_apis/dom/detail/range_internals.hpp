9738efa89d1d0de292c01e7a9ecc97f2eee472fb
Sam <samuelgardner101@gmail.com>
2022-07-05 23:59:20 +0100

    Removed useless methods, parameter name/order adjustments

--- src/javascript/web_apis/dom/detail/range_internals.hpp
+++ src/javascript/web_apis/dom/detail/range_internals.hpp
@@ -3,13 +3,16 @@
 
 #include <ext/number.hpp>
 #include <ext/vector.hpp>
+#include <range/v3/view/any_view.hpp>
+namespace dom::nodes {class character_data;}
 namespace dom::nodes {class document_fragment;}
 namespace dom::nodes {class node;}
 namespace dom::node_ranges {class range;}
 
 namespace dom::detail::range_internals
 {
-    enum boundary_point_comparison_position {EQUALS = -1, BEFORE = 0, AFTER = 1};
+    enum boundary_point_comparison_position : short {EQUALS = -1, BEFORE = 0, AFTER = 1};
+    enum append_action {EXTRACT, CLONE};
 
     // containment and position checks
     auto contains(
@@ -37,35 +40,29 @@ namespace dom::detail::range_internals
             -> boundary_point_comparison_position;
 
     // other general helper methods for ranges
-    auto get_range_helpers_variables(
+    auto get_range_containment_children(
             node_ranges::range* range,
             nodes::node* start_container,
             nodes::node* end_container)
-            -> std::tuple<nodes::node*, nodes::node*, ext::vector<nodes::node*>>;
+            -> std::tuple<nodes::node*, nodes::node*, ranges::any_view<nodes::node*>>;
 
-    auto check_parent_exists(
-            nodes::node* node)
-            -> nodes::node*;
-
-    auto is_textual_based_range_container(
-            nodes::node* node)
-            -> bool;
-
-    auto clone_character_data_and_append(
-            nodes::node* node,
+    auto copy_data(
+            nodes::node* child,
             nodes::document_fragment* fragment,
+            nodes::character_data* container,
             ext::number_view<ulong> start_offset,
             ext::number_view<ulong> end_offset,
             ext::boolean_view replace)
             -> nodes::document_fragment*;
 
     auto append_to_sub_fragment(
-            nodes::node* node,
+            nodes::node* child,
             nodes::document_fragment* fragment,
             nodes::node* start_container,
-            nodes::node* end_container,
             ext::number_view<ulong> start_offset,
-            ext::number_view<ulong> end_offset)
+            nodes::node* end_container,
+            ext::number_view<ulong> end_offset,
+            append_action what)
             -> nodes::document_fragment*;
 
     auto create_new_node_and_offset(
