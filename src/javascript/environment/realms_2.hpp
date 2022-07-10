23c55aaaa745f5ba49fd709797e991bd3ad924a2
Sam <samuelgardner101@gmail.com>
2022-07-10 13:19:13 +0100

    Changed to allow multiple contexts to be accessed in the same method

--- src/javascript/environment/realms_2.hpp
+++ src/javascript/environment/realms_2.hpp
@@ -6,24 +6,27 @@
 #include <v8-maybe.h>
 #include <v8-persistent-handle.h>
 
+#include <javascript/interop/manual_primitive_conversions/convert_boolean.hpp>
 #include <javascript/interop/manual_primitive_conversions/convert_nullptr_t.hpp>
+#include <javascript/interop/manual_primitive_conversions/convert_number_integer.hpp>
+#include <javascript/interop/manual_primitive_conversions/convert_number_float.hpp>
 #include <javascript/interop/manual_primitive_conversions/convert_string.hpp>
 #include <v8pp/convert.hpp>
 
 
 #define _JS_GLOBAL_OBJECT_FROM_REALM(type) \
-    type##_realm->Global()->Get(type##_realm, v8pp::to_v8(type##_agent, "Global")).ToLocalChecked().As<v8::Object>()
+    type##_realm->Global()->Get(type ## _realm, v8pp::to_v8(type ## _agent, "Global")).ToLocalChecked().As<v8::Object>()
 
 #define JS_REALM_GET_CURRENT                                 \
     auto current_agent = v8::Isolate::GetCurrent();          \
     auto current_realm = current_agent->GetCurrentContext(); \
     auto current_global_object = _JS_GLOBAL_OBJECT_FROM_REALM(current);
 
-#define JS_REALM_GET_ENTRY(object)                                                         \
-    JS_REALM_GET_CURRENT                                                                   \
-    auto creation_realm = v8pp::to_v8(current_agent, object)->GetCreationContextChecked(); \
-    auto creation_agent = creation_realm->GetIsolate();                                    \
-    auto creation_global_object = _JS_GLOBAL_OBJECT_FROM_REALM(creation);
+#define JS_REALM_GET_ENTRY(object)                                                                               \
+    JS_REALM_GET_CURRENT                                                                                         \
+    auto object ## _creation_realm = v8pp::to_v8(object ## _current_agent, object)->GetCreationContextChecked(); \
+    auto object ## _creation_agent = object ## _creation_realm->GetIsolate();                                    \
+    auto object ## _creation_global_object = _JS_GLOBAL_OBJECT_FROM_REALM(creation);
 
 #define JS_REALM_GET_IMPLIED /* Same as current (unambiguous in spec) */ \
     auto implied_agent = v8::Isolate::GetCurrent();                      \
@@ -35,29 +38,43 @@
     auto incumbent_realm = incumbent_agent->GetIncumbentContext(); \
     auto incumbent_global_object = _JS_GLOBAL_OBJECT_FROM_REALM(current);
 
-#define JS_REALM_GET_RELEVANT(object)                                                                                                                                          \
-    JS_REALM_GET_CURRENT                                                                                                                                                       \
-    auto relevant_realm = v8pp::to_v8(current_agent, object).As<v8::Object>()->Get(current_realm, v8pp::to_v8(current_agent, "[[Realm]]")).ToLocalChecked().As<v8::Context>(); \
-    auto relevant_agent = relevant_realm->GetIsolate();                                                                                                                        \
-    auto relevant_global_object = _JS_GLOBAL_OBJECT_FROM_REALM(relevant).As<v8::Object>();
+#define JS_REALM_GET_RELEVANT(object)                                                                                                                                                     \
+    JS_REALM_GET_CURRENT                                                                                                                                                                  \
+    auto object ## _relevant_realm = v8pp::to_v8(current_agent, object).As<v8::Object>()->Get(current_realm, v8pp::to_v8(current_agent, "[[Realm]]")).ToLocalChecked().As<v8::Context>(); \
+    auto object ## _relevant_agent = object ## _relevant_realm->GetIsolate();                                                                                                             \
+    auto object ## _relevant_global_object = _JS_GLOBAL_OBJECT_FROM_REALM(object ## _relevant).As<v8::Object>();
 
-#define JS_REALM_GET_SURROUNDING(object) /* Same as relevant */                                                                                                                   \
-    JS_REALM_GET_CURRENT                                                                                                                                                          \
-    auto surrounding_realm = v8pp::to_v8(current_agent, object).As<v8::Object>()->Get(current_realm, v8pp::to_v8(current_agent, "[[Realm]]")).ToLocalChecked().As<v8::Context>(); \
-    auto surrounding_agent = surrounding_realm->GetIsolate();                                                                                                                     \
-    auto surrounding_global_object = _JS_GLOBAL_OBJECT_FROM_REALM(surrounding).As<v8::Object>();
+#define JS_REALM_GET_SURROUNDING(object) /* Same as relevant */                                                                                                                              \
+    JS_REALM_GET_CURRENT                                                                                                                                                                     \
+    auto object ## _surrounding_realm = v8pp::to_v8(current_agent, object).As<v8::Object>()->Get(current_realm, v8pp::to_v8(current_agent, "[[Realm]]")).ToLocalChecked().As<v8::Context>(); \
+    auto object ## _surrounding_agent = object ## _surrounding_realm->GetIsolate();                                                                                                          \
+    auto object ## _surrounding_global_object = _JS_GLOBAL_OBJECT_FROM_REALM(object ## _surrounding).As<v8::Object>();
 
-#define JS_REALM_GET_ASSOCIATED(object)     \
-    JS_REALM_GET_RELEVANT(object)           \
-    auto associated_agent = relevant_agent; \
-    auto associated_realm = relevant_realm; \
-    auto associated_global_object = relevant_global_object;
-
-
-namespace javascript::environment::realms_2 {template <typename T> auto get(v8::Local<v8::Object> global, ext::string_view cpp_attribute) -> T;}
-namespace javascript::environment::realms_2 {template <typename T> auto set(v8::Local<v8::Object> global, ext::string_view cpp_attribute, T cpp_value) -> ext::boolean;}
+#define JS_REALM_GET_ASSOCIATED(object)                                          \
+    JS_REALM_GET_RELEVANT(object)                                                \
+    auto object ## _associated_agent = object ## _relevant_agent;                \
+    auto object ## _associated_realm = object ## _relevant_realm;                \
+    auto object ## _associated_global_object = object ## _relevant_global_object;
 
 
+namespace javascript::environment::realms_2 {
+
+    template <typename T>
+    auto get(
+            v8::Local<v8::Object> global,
+            ext::string_view cpp_attribute)
+            -> T;
+
+    template <typename T>
+    auto set(
+            v8::Local<v8::Object> global,
+            ext::string_view cpp_attribute,
+            T cpp_value)
+            -> ext::boolean;
+}
+
+
+// TODO : isolate as variables in methods (reduce code duplication with calling ...->GetIsolate()
 template <typename T>
 auto javascript::environment::realms_2::get(v8::Local<v8::Object> global, ext::string_view cpp_attribute) -> T
 {
