6038d813b16254e91480119054f826ef53af0083
Sam <samuelgardner101@gmail.com>
2022-07-10 13:13:24 +0100

    Added HTML methods and properties

--- src/javascript/web_apis/dom/nodes/document.cpp
+++ src/javascript/web_apis/dom/nodes/document.cpp
@@ -1,10 +1,20 @@
 #include "document.hpp"
+#include "dom/detail/mutation_internals.hpp"
+#include "dom/detail/tree_internals.hpp"
+#include "range/v3/view/filter.hpp"
+#include "svg/elements/svg_element.hpp"
+#include "v8pp/class.hpp"
 
+#include <ctime>
+#include <sstream>
 #include <ext/casting.hpp>
 #include <ext/ranges.hpp>
 
+#include <iomanip>
 #include <javascript/environment/realms_2.hpp>
 
+#include <variant>
+#include <web_apis/dom/detail/event_internals.hpp>
 #include <web_apis/dom/detail/exception_internals.hpp>
 #include <web_apis/dom/detail/customization_internals.hpp>
 #include <web_apis/dom/detail/namespace_internals.hpp>
@@ -20,9 +30,24 @@
 
 #include <web_apis/dom/other/dom_implementation.hpp>
 #include <web_apis/dom/ranges/range.hpp>
+
+#include <web_apis/high_resolution_time/detail/time_internals.hpp>
+
+#include <web_apis/html/detail/document_internals.hpp>
+#include <web_apis/html/elements/html_body_element.hpp>
+#include <web_apis/html/elements/html_head_element.hpp>
+#include <web_apis/html/elements/html_html_element.hpp>
+#include <web_apis/html/elements/html_script_element.hpp>
+#include <web_apis/html/elements/html_title_element.hpp>
+
+#include <web_apis/infra/detail/infra_strings_internals.hpp>
+
+#include <web_apis/svg/elements/svg_title_element.hpp>
+
 #include <web_apis/url/url.hpp>
 
 #include <range/v3/algorithm/contains.hpp>
+#include <range/v3/range/operations.hpp>
 
 #include <QtCore/QPointer>
 #include <QtWidgets/QVBoxLayout>
@@ -33,14 +58,32 @@ dom::nodes::document::document()
         : url{std::make_unique<url::url_object>("about:blank")}
         , implementation{std::make_unique<other::dom_implementation>()}
         , content_type{"application/xml"}
+        , ready_state{"complete"}
+        , current_script{nullptr}
 {
     bind_get(compat_mode);
     bind_get(character_set);
     bind_get(doctype);
     bind_get(document_element);
 
+    bind_get(last_modified);
+    bind_get(cookie);
+    bind_get(body);
+    bind_get(head);
+    bind_get(title);
+    bind_get(images);
+    bind_get(links);
+    bind_get(forms);
+    bind_get(scripts);
+    bind_get(dir);
+
+    bind_set(ready_state);
+    bind_set(cookie);
+    bind_set(title);
+    bind_set(body);
+
     JS_REALM_GET_SURROUNDING(this)
-    m_origin = javascript::environment::realms_2::get<ext::string>(surrounding_global_object, "origin");
+    m_origin = javascript::environment::realms_2::get<ext::string>(this_surrounding_global_object, "origin");
 
     auto rendered_widget = QPointer<QScrollArea>{};
     rendered_widget->setWidget(QPointer<QWidget>{});
@@ -190,7 +233,7 @@ auto dom::nodes::document::create_range()
     node_ranges::range live_range;
 
     std::tie(live_range.start_container, live_range.start_offset) = std::make_tuple(const_cast<document*>(this), 0);
-    std::tie(live_range.end_container  , live_range.end_offset  ) = std::make_tuple(const_cast<document*>(this), 0);
+    std::tie(live_range.end_container, live_range.end_offset) = std::make_tuple(const_cast<document*>(this), 0);
     return live_range;
 }
 
@@ -229,4 +272,264 @@ auto dom::nodes::document::adopt_node(
         // takes place
         return detail::node_internals::adopt(new_node, this);
     ce_reactions_method_exe
+}
+
+
+auto dom::nodes::document::get_last_modified()
+        const -> ext::string
+{
+    // get the string epoch time (eg "1563566272"), and convert it to a 'long long' number type. convert this into a c++
+    // time object with a strict format, push this into a stream, and finally return the string that the
+    // std::stringstream holds
+    std::time_t decimal_time = std::stoll(*last_modified);
+    auto formatted_time = std::stringstream{} << std::put_time(std::localtime(&decimal_time), "%m/%d/%Y %H:%M:%S");
+    return formatted_time.str();
+}
+
+
+auto dom::nodes::document::get_cookie()
+        const -> ext::string
+{
+    // if this Document is cookie averse, then it is not in the correct condition to return the value of the cookie, so
+    // return the empty string instead.
+    return_if (html::detail::document_internals::is_cookie_averse_document(this)) "";
+
+    // if the origin of this Document is opaque, then throw a security error, because the security of the cookie cannot
+    // be guaranteed, despite the Document not being cookie averse
+    detail::exception_internals::throw_v8_exception_formatted<SECURITY_ERR>(
+            [this] {return m_origin.is_opaque();},
+            "Can not get the cookie of a Document whose origin is opaque");
+
+    // return the true value of the cookie
+    return *cookie;
+}
+
+
+auto dom::nodes::document::get_head()
+        const -> html::elements::html_head_element*
+{
+    // the head element is  the first child of the HTMLHtmlElement that is a HTMLHeadElement; if there are no head
+    // elements, then return nullptr - this is handled by returning a deference of the begin pointer
+    auto html_element_child_nodes = *get_m_html_element()->child_nodes();
+    auto head_elements = html_element_child_nodes | ranges::views::cast_all_to<html::elements::html_head_element>();
+    return *head_elements.begin();
+}
+
+
+auto dom::nodes::document::get_title()
+        const -> ext::string
+{
+    // the title element is either the first SVGTitleElement of the document element, if the document element is an
+    // SvgElement. otherwise it is the first HTMLTitleElement of this document
+    auto* title_element = dynamic_cast<svg::elements::svg_element*>(document_element())
+            ? ranges::front(*document_element->child_nodes() | ranges::views::cast_all_to<svg::elements::svg_title_element>())
+            : get_m_title_element();
+
+    // the value of the xxxTitleElement is the child text content of it, with the ascii whitespace stripped and
+    // collapsed from the string
+    auto value = detail::tree_internals::child_text_content(title_element);
+    value = infra::detail::infra_string_internals::strip_and_collapse_ascii_whitespace(value);
+    return value;
+}
+
+
+auto dom::nodes::document::get_body()
+        const -> html::elements::html_body_element*
+{
+    // the body element is the first child of the HTMLHtmlElement that is a HTMLBodyElement; if there are no body
+    // elements, then return nullptr - this is handled by returning a deference of the begin pointer
+    auto html_element_child_nodes = *get_m_html_element()->child_nodes();
+    auto body_children = html_element_child_nodes | ranges::views::cast_all_to<html::elements::html_body_element>();
+    return *body_children.begin();
+}
+
+
+auto dom::nodes::document::get_images()
+        const -> ranges::any_view<html::elements::html_image_element*>
+{
+    // the HTMLImageElements in this Document are all the HTMLImageElements that are descendants of this Document (live
+    // collection)
+    return detail::tree_internals::descendants(this)
+            | ranges::views::cast_all_to<html::elements::html_image_element>();
+}
+
+
+auto dom::nodes::document::get_links()
+        const -> ranges::any_view<html::elements::html_link_element*>
+{
+    // the HTMLLinkElements in this Document are all the HTMLLinkElements that are descendants of this Document (live
+    // collection), and have their href attribute set
+    return detail::tree_internals::descendants(this)
+            | ranges::views::cast_all_to<html::elements::html_link_element>()
+            | ranges::views::filter([](html::elements::html_link_element* element) {return element->href();});
+}
+
+
+auto dom::nodes::document::get_forms()
+        const -> ranges::any_view<html::elements::html_form_element*>
+{
+    // the HTMLFormElements in this Document are all the HTMLFormElements that are descendants of this Document (live
+    // collection)
+    return detail::tree_internals::descendants(this)
+            | ranges::views::cast_all_to<html::elements::html_form_element>();
+}
+
+
+auto dom::nodes::document::get_scripts()
+        const -> ranges::any_view<html::elements::html_script_element*>
+{
+    // the HTMLScriptElements in this Document are all the HTMLScriptElements that are descendants of this Document
+    // (live collection)
+    return detail::tree_internals::descendants(this)
+            | ranges::views::cast_all_to<html::elements::html_script_element>();
+}
+
+
+auto dom::nodes::document::get_dir()
+        const -> ext::string
+{
+    // if the 'document_element' exists, and is a HTMLElement based object, then forward its 'dir' attribute value as
+    // the Document's 'dir' attribute value, otherwise the empty string
+    auto* html_document_element = dynamic_cast<html::elements::html_element*>(document_element());
+    return html_document_element ? html_document_element->dir() : "";
+}
+
+
+auto dom::nodes::document::set_cookie(
+        ext::string_view val)
+        -> void
+{
+    // if this Document is cookie averse, then it is not in the correct condition to return the value of the cookie, so
+    // return the empty string instead.
+    return_if (html::detail::document_internals::is_cookie_averse_document(this));
+
+    // if the origin of this Document is opaque, then throw a security error, because the security of the cookie cannot
+    // be guaranteed, despite the Document not being cookie averse
+    detail::exception_internals::throw_v8_exception_formatted<SECURITY_ERR>(
+            [this] {return m_origin.is_opaque();},
+            "Can not get the cookie of a Document whose origin is opaque");
+
+    // set the true value of the cookie t the new cookie value passed in as a parameter
+    *cookie = val;
+}
+
+
+auto dom::nodes::document::set_ready_state(
+        ext::string_view val)
+        -> void
+{
+    *ready_state = val;
+    if (/* TODO : HTML parser association */ false)
+    {
+        JS_REALM_GET_RELEVANT(this)
+        auto now = high_resolution_time::detail::time_internals::current_high_resolution_time(this_relevant_global_object);
+        if (val == "complete" && m_load_timing_info.dom_complete_time == 0)
+            m_load_timing_info.dom_complete_time = now;
+
+        if (val == "interactive" && m_load_timing_info.dom_interactive_time == 0)
+            m_load_timing_info.dom_interactive_time = now;
+    }
+    detail::event_internals::fire_event("readystatechange", this);
+}
+
+
+auto dom::nodes::document::set_title(
+        ext::string_view val)
+        -> void
+{
+    using namespace detail::namespace_internals;
+
+    // if the document element is an SvgTitleElement, handle the title updating steps for the SVG namespace
+    if (dynamic_cast<svg::elements::svg_element*>(document_element()))
+    {
+        // set the 'title_element' to the first child of the 'document_element's child node that is an SvgTitleElement;
+        // if there is no such element, then create the element, and insert it as the first child of the
+        // 'document_element' (call insert not pre_insert, because the newly created 'title_element' can't be the first
+        // child when it has just been created)
+        auto* title_element = ranges::front(*document_element->child_nodes() | ranges::views::cast_all_to<svg::elements::svg_title_element>());
+        if (!title_element)
+        {
+            title_element = detail::customization_internals::create_an_element(document_element->owner_document(), "title", SVG);
+            detail::mutation_internals::insert(title_element, document_element(), document_element->first_child());
+        }
+
+        // replace all the text in the SvgTitleElement with the new title value 'val' parameter
+        detail::node_internals::string_replace_all(val, title_element);
+    }
+
+    else if (document_element->namespace_uri() == HTML)
+    {
+        auto* title_element = get_m_title_element();
+        return_if(!title_element && !head());
+        if (!title_element)
+        {
+            title_element = dynamic_cast<html::elements::html_title_element*>(detail::customization_internals::create_an_element(document_element->owner_document(), "title", HTML));
+            detail::mutation_internals::append(title_element, head());
+        }
+
+        // replace all the text in the HtmlTitleElement with the new title value 'val' parameter
+        detail::node_internals::string_replace_all(val, title_element);
+    }
+}
+
+
+auto dom::nodes::document::set_body(
+        html::elements::html_body_element* val)
+        -> void
+{
+    // don't do anything id the new body value is the same as the current body, ie the first HTMLBodyElement of the
+    // HTMLHtmlElement in this Document
+    return_if (body() == val);
+
+    // if there is a current HTMLBodyElement (and an implied 'document_element'), then replace it with the new
+    // HTMLBodyElement
+    if (body())
+        detail::mutation_internals::replace(body(), body->parent_node(), val);
+
+    // if there is no implied HTMLBodyElement, and no 'document_element', throw a hierarchy request error, because it's
+    // not possible to append the HTMLBodyElement to a non-existent 'document_element'
+    detail::exception_internals::throw_v8_exception_formatted<HIERARCHY_REQUEST_ERR>(
+            [this] {return !document_element();},
+            "Document must have a document element, if a new HTMLBodyElement is being added to the Document");
+
+    // append the HTMLBodyElement to this Document's 'document_element'
+    detail::mutation_internals::append(val, document_element());
+}
+
+
+auto dom::nodes::document::get_m_html_element()
+        const -> html::elements::html_html_element*
+{
+    // the HTMLHtmlElement of a Document is the 'document_element', if it is a HTMLHtmlElement, otherwise a nullptr; the
+    // dynamic cast will either return the cast document element, or nullptr if the document element isn't a
+    // HTMLHtmlElement
+    return dynamic_cast<html::elements::html_html_element*>(document_element());
+}
+
+
+auto dom::nodes::document::get_m_title_element()
+        const -> html::elements::html_title_element*
+{
+    // the HTMLTitleElement of a Document is the first HTMLTitleElement in the Document's descendants; if there isn't
+    // one, then nullptr is returned, as the de-referenced begin() iterator
+    return ranges::front(detail::tree_internals::descendants(this) | ranges::views::cast_all_to<html::elements::html_title_element*>());
+}
+
+
+auto dom::nodes::document::operator[](
+        const ext::string& name)
+        -> ranges::any_view<element*>
+{
+    // return all descendant element in this tree whose name attribute (only present on certain element interfaces) is
+    // equal to the 'name' parameter; TODO: named_element mixin with property `name` -> cast all, filter, and cast back
+    return detail::tree_internals::descendants(this)
+            | ranges::views::filter([name](element* descendant) {return descendant->name() == name;});
+}
+
+auto dom::nodes::document::to_v8(
+        v8::Isolate* isolate)
+        const && -> ext::any
+{
+    return v8pp::class_<document>{isolate}
+            .auto_wrap_objects();
 }
--- src/javascript/web_apis/dom/nodes/document.hpp
+++ src/javascript/web_apis/dom/nodes/document.hpp
@@ -1,15 +1,22 @@
 #ifndef SBROWSER2_DOCUMENT_HPP
 #define SBROWSER2_DOCUMENT_HPP
 
+#include "range/v3/view/any_view.hpp"
 #include <web_apis/dom/nodes/node.hpp>
 #include <web_apis/dom/mixins/document_or_element_node.hpp>
 #include <web_apis/dom/mixins/document_or_shadow_root.hpp>
 #include <web_apis/dom/mixins/non_element_parent_node.hpp>
 #include <web_apis/dom/mixins/parentable_node.hpp>
 #include <web_apis/dom/xpath/xpath_evaluator_base.hpp>
+#include <ext/map_like.hpp>
 namespace dom::nodes {class document;}
 
+#include <ext/concepts.hpp>
 #include <ext/map.hpp>
+#include <ext/set.hpp>
+#include <url/url.hpp>
+#include <ext/vector.hpp>
+#include <range/v3/view/any_view.hpp>
 namespace dom::nodes {class attr;}
 namespace dom::nodes {class cdata_section;}
 namespace dom::nodes {class comment;}
@@ -17,12 +24,33 @@ namespace dom::nodes {class document_fragment;}
 namespace dom::nodes {class document_type;}
 namespace dom::nodes {class processing_instruction;}
 namespace dom::nodes {class text;}
+namespace dom::nodes {class window_proxy;}
+
 namespace dom::node_ranges {class range;}
 namespace dom::node_iterators {class node_filter;}
 namespace dom::node_iterators {class node_iterator;}
 namespace dom::node_iterators {class tree_walker;}
 namespace dom::other {class dom_implementation;}
-namespace url {class url_object;}
+
+namespace html::detail::policy_internals {struct cross_origin_opener_policy_value;}
+namespace html::detail::policy_internals {struct policy_container;}
+namespace html::detail::context_internals {struct browsing_context;}
+namespace html::detail::document_internals {struct document_load_timing_info;}
+namespace html::detail::document_internals {struct document_unload_timing_info;}
+
+namespace html::elements {class html_body_element;}
+namespace html::elements {class html_head_element;}
+namespace html::elements {class html_html_element;}
+namespace html::elements {class html_image_element;}
+namespace html::elements {class html_link_element;}
+namespace html::elements {class html_form_element;}
+namespace html::elements {class html_script_element;}
+namespace html::elements {class html_title_element;}
+namespace html::elements {class html_element;}
+namespace html::other {class location;}
+
+namespace permissions_policy {class permissions_policy;}
+namespace svg::elements {class svg_script_element;}
 
 
 class dom::nodes::document
@@ -32,7 +60,17 @@ class dom::nodes::document
         , public mixins::non_element_parent_node
         , public mixins::parentable_node
         , public xpath::xpath_evaluator_base
+        , public ext::map_like<ext::string, ranges::any_view<element*>>
 {
+public aliases:
+    using module_map = ext::map<
+            std::tuple<ext::string, url::url_object>,
+            ext::string>;
+
+    using html_or_svg_script_element = std::variant<
+            std::unique_ptr<html::elements::html_script_element>,
+            std::unique_ptr<svg::elements::svg_script_element>>;
+
 public friends:
     friend struct mixins::document_or_element_node;
 
@@ -40,6 +78,7 @@ public constructors:
     document();
 
 public js_methods:
+    /* DOM */
     [[nodiscard]] auto create_element(ext::string_view local_name, ext::string_any_map_view options = {}) const -> element;
     [[nodiscard]] auto create_element_ns(ext::string_view namespace_, ext::string_view qualified_name, ext::string_any_map_view options) const -> element;
     [[nodiscard]] auto create_document_fragment() const -> document_fragment;
@@ -57,7 +96,22 @@ public js_methods:
     auto import_node(node* new_node, ext::boolean_view deep = false) -> node*;
     auto adopt_node(node* new_node) -> node*;
 
+    /* HTML */
+    auto open() -> document*;
+    auto open(ext::string_view url, ext::string_view name, ext::string_view features) -> window_proxy*;
+    auto close() -> void;
+    auto write(type_is<ext::string> auto&&... text) -> void;
+    auto write_ln(type_is<ext::string> auto&&... text) -> void;
+
+    auto has_focus() -> ext::boolean;
+    auto queryCommandEnabled(ext::string_view commandId) -> ext::boolean;
+    auto queryCommandIndeterm(ext::string_view commandId) -> ext::boolean;
+    auto queryCommandState(ext::string_view commandId) -> ext::boolean;
+    auto queryCommandSupported(ext::string_view commandId) -> ext::boolean;
+    auto queryCommandValue(ext::string_view commandId) -> ext::boolean;
+
 public js_properties:
+    /* DOM */
     ext::property<std::unique_ptr<url::url_object>> url;
     ext::property<ext::string> compat_mode;
     ext::property<ext::string> character_set;
@@ -66,16 +120,64 @@ public js_properties:
     ext::property<std::unique_ptr<element>> document_element;
     ext::property<std::unique_ptr<other::dom_implementation>> implementation;
 
+    /* HTML */
+    ext::property<std::unique_ptr<html::other::location>> location;
+    ext::property<ext::string> domain;
+    ext::property<ext::string> referrer;
+    ext::property<ext::string> cookie;
+    ext::property<ext::string> last_modified;
+    ext::property<ext::string> ready_state;
+    ext::property<ext::string, _T> title;
+    ext::property<ext::string, _T> dir;
+
+    ext::property<std::unique_ptr<html::elements::html_body_element>, _T> body;
+    ext::property<std::unique_ptr<html::elements::html_head_element>, _T> head;
+    ext::property<ranges::any_view<html::elements::html_image_element*>> images;
+    ext::property<ranges::any_view<html::elements::html_link_element*>> links;
+    ext::property<ranges::any_view<html::elements::html_form_element*>> forms;
+    ext::property<ranges::any_view<html::elements::html_script_element*>> scripts;
+    ext::property<html_or_svg_script_element> current_script;
+
+    ext::property<std::unique_ptr<window_proxy>> default_view;
+    ext::property<ext::string> design_mode;
+    ext::property<ext::boolean> hidden;
+    ext::property<ext::string> visibility_state;
+
+    /* PERMISSIONS_POLICY */
+    ext::property<std::unique_ptr<permissions_policy::permissions_policy>> permissions_policy;
+
 public cpp_methods:
     auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
 
+public cpp_operators:
+    virtual auto operator[](const ext::string& name) -> ranges::any_view<element*> override;
+
+private cpp_methods:
+    auto get_m_html_element() const -> html::elements::html_html_element*;
+    auto get_m_title_element() const -> html::elements::html_title_element*;
+
 private cpp_properties:
+    /* DOM */
 //    std::unique_ptr<encoding::encoding> m_encoding = nullptr;
     ext::string m_type = "xml";
     ext::string m_mode = "no-quirks";
-    ext::string m_origin;
+    url::url_object m_origin;
+
+    /* HTML */
+    std::unique_ptr<html::detail::policy_internals::policy_container> m_policy_container;
+    std::unique_ptr<html::detail::policy_internals::cross_origin_opener_policy_value> m_cross_origin_opener_policy;
+    std::unique_ptr<permissions_policy::permissions_policy> m_permissions_policy;
+    module_map m_module_map;
+    ext::boolean m_is_initial = false;
+
+    html::detail::document_internals::document_load_timing_info& m_load_timing_info;
+    html::detail::document_internals::document_unload_timing_info& m_unload_timing_info;
+
+    ext::set<element*> m_render_blocking_elements;
+    std::shared_ptr<html::detail::context_internals::browsing_context*> m_browsing_context;
 
 private cpp_accessors:
+    /* DOM */
     [[nodiscard]] auto get_node_type() const -> ext::number<ushort> override {return DOCUMENT_NODE;}
     [[nodiscard]] auto get_node_name() const -> ext::string override {return "#document";}
     [[nodiscard]] auto get_node_value() const -> ext::string override {return "";}
@@ -87,6 +189,23 @@ private cpp_accessors:
     [[nodiscard]] auto get_character_set() const -> ext::string;
     [[nodiscard]] auto get_doctype() const -> document_type*;
     [[nodiscard]] auto get_document_element() const -> element*;
+
+    /* HTML */
+    [[nodiscard]] auto get_last_modified() const -> ext::string;
+    [[nodiscard]] auto get_cookie() const -> ext::string;
+    auto get_body() const -> html::elements::html_body_element*;
+    auto get_head() const -> html::elements::html_head_element*;
+    auto get_title() const -> ext::string;
+    auto get_images() const -> ranges::any_view<html::elements::html_image_element*>;
+    auto get_links() const -> ranges::any_view<html::elements::html_link_element*>;
+    auto get_forms() const -> ranges::any_view<html::elements::html_form_element*>;
+    auto get_scripts() const -> ranges::any_view<html::elements::html_script_element*>;
+    auto get_dir() const -> ext::string;
+
+    auto set_ready_state(ext::string_view val) -> void;
+    auto set_cookie(ext::string_view val) -> void;
+    auto set_title(ext::string_view val) -> void;
+    auto set_body(html::elements::html_body_element* val) -> void;
 };
 
 
