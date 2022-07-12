aec1943f89631226322b69b5a0ba7925884822c1
Sam <samuelgardner101@gmail.com>
2022-07-12 12:57:12 +0100

    Added some methods

--- src/javascript/web_apis/url/url.hpp
+++ src/javascript/web_apis/url/url.hpp
@@ -1,7 +1,7 @@
 #ifndef SBROWSER2_URL_HPP
 #define SBROWSER2_URL_HPP
 
-#include <web_apis/dom_object.hpp>
+#include "dom_object.hpp"
 namespace url {class url_object;}
 
 
@@ -10,14 +10,20 @@ class url::url_object : public virtual web_apis::dom_object
 public constructors:
     url_object(ext::string_view url_string = "");
 
+public js_methods:
+    /* FILE_API */
+    static auto create_object_url(blob& blob_object) -> ext::string;
+    static auto revoke_object_url(ext::string_view url);
+
+public js_properties:
+    ext::property<ext::string> scheme;
+
 public cpp_operators:
     auto operator=(ext::string_view url) -> url_object&;
 
 public cpp_methods:
     auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
-    auto is_opaque() const -> ext::boolean;
-
-    auto scheme() -> ext::string;
+    auto is_opaque() const -> ext::boolean; // TODO : move to `detail` helper
 };
 
 
