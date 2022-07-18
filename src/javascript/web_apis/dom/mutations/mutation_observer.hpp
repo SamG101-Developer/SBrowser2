df3260947b55ff0220838e0683454b7fcdd94cc9
Sam <samuelgardner101@gmail.com>
2022-07-18 23:41:14 +0100

    Use new `ext` classes

--- src/javascript/web_apis/dom/mutations/mutation_observer.hpp
+++ src/javascript/web_apis/dom/mutations/mutation_observer.hpp
@@ -5,32 +5,34 @@
 namespace dom::mutations {class mutation_observer;}
 
 #include "ext/map.hpp"
+#include "ext/functional.hpp"
+#include "ext/queue.hpp"
 #include "ext/vector.hpp"
 namespace dom::mutations {class mutation_record;}
 
 
 class dom::mutations::mutation_observer
-        : public virtual web_apis::dom_object
+        : public virtual dom_object
 {
 public aliases:
-    using mutation_callback_t = std::function<void(const ext::vector<mutation_record*>&, mutation_observer*)>;
+    using mutation_callback_t = ext::function<void(const ext::vector<mutation_record*>&, mutation_observer*)>;
 
 public constructors:
     mutation_observer() = default;
     mutation_observer(mutation_callback_t&& callback);
 
 public js_methods:
-    auto observe(const nodes::node* target, ext::string_any_map_view options = {}) -> void;
+    auto observe(const nodes::node* target, ext::map<ext::string, ext::any>&& options = {}) -> void;
     auto disconnect() -> void;
     auto take_records() -> ext::vector<mutation_record*>;
 
 public cpp_methods:
-    auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
+    auto to_v8(v8::Isolate* isolate) const && -> ext::any override;
 
 private cpp_properties:
     mutation_callback_t m_callback;
     std::unique_ptr<ext::vector<nodes::node*>> m_node_list;
-    std::unique_ptr<std::queue<mutation_record>> m_record_queue;
+    std::unique_ptr<ext::queue<mutation_record>> m_record_queue;
 };
 
 
