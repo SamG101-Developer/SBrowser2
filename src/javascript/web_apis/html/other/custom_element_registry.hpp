521baacd78e1a9d290d1040063ee76724456dddf
Sam <samuelgardner101@gmail.com>
2022-07-18 23:28:16 +0100

    Fix `#include`s

--- src/javascript/web_apis/html/other/custom_element_registry.hpp
+++ src/javascript/web_apis/html/other/custom_element_registry.hpp
@@ -4,20 +4,22 @@
 #include "dom_object.hpp"
 namespace html::other {class custom_element_registry;}
 
+#include "ext/functional.hpp"
 #include "ext/map.hpp"
 #include "ext/optional.hpp"
+#include <future>
 namespace dom::nodes {class node;}
 namespace html::elements {class html_element;}
 
 
 class html::other::custom_element_registry
-        : public virtual web_apis::dom_object
+        : public virtual dom_object
 {
 public aliases:
-    using custom_element_constructor = std::function<elements::html_element*()>;
+    using custom_element_constructor = ext::function<elements::html_element*()>;
 
 public js_methods:
-    auto define(ext::string_view name, custom_element_constructor&& cosntructor, ext::string_any_map_view options = {});
+    auto define(ext::string_view name, custom_element_constructor&& cosntructor, ext::map<ext::string, ext::any> options = {});
     auto get(ext::string_view name) -> ext::optional<custom_element_constructor>;
     auto when_defined(ext::string_view name) -> std::promise<custom_element_constructor>;
     auto upgrade(dom::nodes::node* root) -> void;
