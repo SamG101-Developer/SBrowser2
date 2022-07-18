0657ee26c105e734dba10cd70552126100d475d0
Sam <samuelgardner101@gmail.com>
2022-07-18 23:39:56 +0100

    Switched to `robin_map` map interface

--- src/ext/map.hpp
+++ src/ext/map.hpp
@@ -2,49 +2,11 @@
 #ifndef SBROWSER2_MAP_HPP
 #define SBROWSER2_MAP_HPP
 
-#include <map>
+#include <tsl/robin_map.h>
 
-namespace     {template <typename _Vt1, typename _Vt2> using map_internal = std::map<_Vt1, _Vt2>;}
-namespace ext {template <typename _Vt1, typename _Vt2> class map;}
+namespace ext {template <typename _Vt1, typename _Vt2> using map = tsl::robin_map<_Vt1, _Vt2>;}
+namespace ext {template <typename _Vt1, typename _Vt2> using map_view = const map<_Vt1, _Vt2>&;}
 
-#include "ext/keywords.hpp"
-#include "ext/optional.hpp"
-
-namespace ext {class any;}
-namespace ext {class string;}
-
-namespace ext {using string_any_map         = ext::map<ext::string, ext::any   >;}
-namespace ext {using string_any_map_view    = const string_any_map&;}
-namespace ext {using string_string_map      = ext::map<ext::string, ext::string>;}
-namespace ext {using string_string_map_view = const string_string_map&;}
-
-
-template <typename _Tx1, typename _Tx2>
-class ext::map
-        : public map_internal<_Tx1, _Tx2>
-{
-public aliases:
-    using key_t = _Tx1;
-    using value_t = _Tx2;
-
-public constructors:
-    using map_internal<_Tx1, _Tx2>::map_internal;
-
-public cpp_methods:
-    auto at(_Tx1&& _Key) -> optional<_Tx2>
-    {
-        if (this->contains(std::forward<_Tx1>(_Key)))
-            return optional<_Tx2>{map_internal<_Tx1, _Tx2>::at(std::forward<_Tx1>(_Key))};
-        return optional<_Tx2>{null};
-    }
-
-    auto at(_Tx1&& _Key) const -> optional<const _Tx2>
-    {
-        if (this->contains(std::forward<_Tx1>(_Key)))
-            return optional<const _Tx2>{map_internal<_Tx1, _Tx2>::at(std::forward<_Tx1>(_Key))};
-        return optional<const _Tx2>{null};
-    }
-};
 
 
 #endif //SBROWSER2_MAP_HPP
