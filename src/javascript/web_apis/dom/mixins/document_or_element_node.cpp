92d0d71cb749360f014dff54e0f052985ddc149c
Sam <samuelgardner101@gmail.com>
2022-07-18 23:31:01 +0100

    Corrected usage of `range-v3` library

--- src/javascript/web_apis/dom/mixins/document_or_element_node.cpp
+++ src/javascript/web_apis/dom/mixins/document_or_element_node.cpp
@@ -1,6 +1,7 @@
 #include "document_or_element_node.hpp"
 
 #include "ext/casting.hpp"
+#include "ext/functional.hpp"
 #include "ext/ranges.hpp"
 #include "dom/nodes/document.hpp"
 #include "dom/detail/tree_internals.hpp"
@@ -14,30 +15,30 @@
 
 
 auto dom::mixins::document_or_element_node::get_elements_by_class_name(
-        ext::string_view class_names)
-        -> range_v3_view auto
+        const ext::string_view class_names)
+        const -> ranges::any_view<nodes::element*>
 {
-    auto* base = ext::cross_cast<nodes::node*>(this);
+    const auto* const base = ext::cross_cast<const nodes::node*>(this);
 
     // split the class names of a Node by spaces, and determine 'lower', which causes everything to be compared in
     // lowercase; if 'lower' is true, then convert the 'class_names' list into lowercase strings
-    auto lower = base->owner_document->m_mode == "quirks";
-    auto class_list = class_names | ranges::views::split_string(' ');
-    class_list |= ranges::actions::transform_if(lower, ranges::actions::lowercase);
+    const ext::boolean lower = base->owner_document()->m_mode == "quirks";
+    auto class_list = class_names
+            | ranges::views::split_string(' ')
+            | ranges::views::transform_if(std::move(lower), ranges::actions::lowercase);
 
     // the 'match_callback' checks if all the class list items from an element are contained in the list that was passed
     // into the function as a parameter. the element's class list is converted into lowercase if 'lower' is set
-    auto match_callback = [&class_list, lower](nodes::element* element)
+    auto match_callback = [&class_list, lower](const nodes::element* const element)
     {
-        ext::string_vector& this_class_list = *element->class_list();
-        this_class_list |= ranges::actions::transform_if(lower, ranges::actions::lowercase);
+        auto this_class_list = *element->class_list() | ranges::views::transform_if(std::move(lower), ranges::actions::lowercase);
         return ranges::contains_all(class_list, this_class_list);
     };
 
     // filter the elements by applying the 'match_callback' onto each Element; if all the Element's class list items are
     // in the 'class_names' parameter, then keep the element, otherwise discard it
     auto matches = detail::tree_internals::descendants(base)
-            | ranges::views::cast_all_to<nodes::element>()
+            | ranges::views::cast_all_to<nodes::element*>()
             | ranges::views::filter(match_callback);
 
     return matches;
@@ -46,30 +47,31 @@ auto dom::mixins::document_or_element_node::get_elements_by_class_name(
 
 auto dom::mixins::document_or_element_node::get_elements_by_tag_name(
         ext::string_view qualified_name)
-        -> range_v3_view auto
+        const -> ranges::any_view<nodes::element*>
 {
     // cross cast this node to a Node
-    auto* base = ext::cross_cast<nodes::node*>(this);
+    const auto* const base = ext::cross_cast<const nodes::node*>(this);
 
     // determine 'lower', which causes everything to be compared in lowercase; if 'lower' is true, then convert the
     // 'qualified_name' into a lowercase string
-    auto lower = base->owner_document->m_mode == "quirks";
-    if (lower) qualified_name |= ranges::views::lowercase();
+    const auto lower = base->owner_document()->m_mode == "quirks";
+    ext::string that_qualified_name = ext::string{qualified_name};
+    if (lower) that_qualified_name |= ranges::actions::lowercase();
 
     // the 'match_callback' checks if the qualified name of en element matches the qualified name that was passed into
     // the function as parameter. the element's qualified name is concerned into lowercase is 'lower' is set
-    auto match_callback = [&qualified_name, lower](nodes::element* element)
+    auto match_callback = [&that_qualified_name, lower](const nodes::element* const element)
     {
         auto this_qualified_name = element->qualified_name();
-        if (lower) this_qualified_name | ranges::views::lowercase();
-        return this_qualified_name == "*" || this_qualified_name == qualified_name;
+        if (lower) this_qualified_name |= ranges::actions::lowercase();
+        return this_qualified_name == "*" || this_qualified_name == that_qualified_name;
     };
 
     // filter the elements by applying the 'match_callback' onto each Element; if the Element's qualified name equals
     // the 'qualified_name' parameter, then keep the element, otherwise discard it
-    using f_t = std::function<bool(nodes::element*)>;
+    using f_t = ext::function<bool(nodes::element*)>;
     auto matches = detail::tree_internals::descendants(base)
-            | ranges::views::cast_all_to<nodes::element>()
+            | ranges::views::cast_all_to<nodes::element*>()
             | ranges::views::filter(match_callback);
 
     return matches;
@@ -77,16 +79,16 @@ auto dom::mixins::document_or_element_node::get_elements_by_tag_name(
 
 
 auto dom::mixins::document_or_element_node::get_elements_by_tag_name_ns(
-        ext::string_view namespace_,
-        ext::string_view local_name)
-        -> range_v3_view auto
+        const ext::string_view namespace_,
+        const ext::string_view local_name)
+        const -> ranges::any_view<nodes::element*>
 {
     // cross cast this node to a Node
-    auto* base = ext::cross_cast<nodes::node*>(this);
+    const auto* const base = ext::cross_cast<const nodes::node*>(this);
 
     // the 'match_callback' checks if the 'namespace_' and 'local_name' of an element matches the namespace and local
     // name that were passed into the function as parameters. no html qualification is required for lowercase conversion
-    auto match_callback = [&namespace_, &local_name](nodes::element* element)
+    auto match_callback = [&namespace_, &local_name](const nodes::element* const element)
     {
         auto this_namespace = element->namespace_uri();
         auto this_local_name = element->local_name();
