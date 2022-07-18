de43c1fb3d5a2dd893930473fa2a5b5eeb1b0a42
Sam <samuelgardner101@gmail.com>
2022-07-18 23:52:44 +0100

    Made code more concise

--- src/javascript/web_apis/dom/nodes/text.cpp
+++ src/javascript/web_apis/dom/nodes/text.cpp
@@ -9,10 +9,12 @@
 #include <QtWidgets/QVBoxLayout>
 
 
-dom::nodes::text::text()
+dom::nodes::text::text(ext::string&& new_data)
 {
     bind_get(whole_text);
-    bind_qt(data, m_rendered_widget, QLabel::setText);
+    bind_qt(data, m_rendered_widget, QLabel::setText)
+
+    data = std::move(new_data);
 
     auto widget = QPointer<QLabel>{};
     widget->setLayout(new QVBoxLayout{});
@@ -22,17 +24,9 @@ dom::nodes::text::text()
 }
 
 
-dom::nodes::text::text(
-        ext::string_view new_data)
-        : text()
-{
-    data = new_data;
-}
-
-
 auto dom::nodes::text::split_text(
         ext::number_view<ulong> offset)
-        -> text*
+        -> text
 {
     // split the text node at the index 'offset', and return the 2nd half of the split, ie a Text node containing all
     // data after the split (this Text node remains the same)
@@ -44,7 +38,7 @@ auto dom::nodes::text::get_whole_text()
         const -> ext::string
 {
     // the whole text of a Text node is the contiguous node data ie the combined data of all the contiguous Text nodes
-    dom::detail::tree_internals::contiguous_text_nodes(this)
+    dom::detail::tree_internals::contiguous_text_nodes(this) // TODO -> create contiguous_text_content(...) in detail::tree_internals
             | ranges::views::transform([](nodes::text* node) {return node->data();})
             | ranges::to<ext::string>();
 }
