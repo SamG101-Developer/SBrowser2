afe1104e1dc9fd84fdef1e65b7672a3571061028
Sam <samuelgardner101@gmail.com>
2022-07-18 23:47:59 +0100

    Incorporated new `ext` classes (containers) that are high-performance and heavily optimized (better than STL)

--- src/ext/pair.hpp
+++ src/ext/pair.hpp
@@ -1,34 +1,9 @@
 #ifndef SBROWSER2_PAIR_HPP
 #define SBROWSER2_PAIR_HPP
 
-#include <tuple>
+#include <tuplet/tuple.hpp>
 
-namespace ext {struct pair_name_equal_to_fn;}
-namespace ext {struct pair_value_equal_to_fn;}
-
-
-struct ext::pair_name_equal_to_fn
-{
-    template <typename K, typename V>
-    constexpr auto operator()(std::pair<K, V>&& _Pair, V&& _ValCompare)
-    {
-        return _Pair.first == std::forward<V>(_ValCompare);
-    }
-};
-
-
-struct ext::pair_value_equal_to_fn
-{
-    template <typename K, typename V>
-    constexpr auto operator()(std::pair<K, V>&& _Pair, V&& _ValCompare)
-    {
-        return _Pair.second == std::forward<V>(_ValCompare);
-    }
-};
-
-
-namespace ext {constexpr pair_name_equal_to_fn pair_name_equal_to;}
-namespace ext {constexpr pair_value_equal_to_fn pair_value_equal_to_fn;}
+namespace ext {template <typename _Vt0, typename _Vt1> using pair = tuplet::pair<_Vt0, _Vt1>;}
 
 
 #endif //SBROWSER2_PAIR_HPP
