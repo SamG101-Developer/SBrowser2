3ace8d1a24d7f213498c198eab5a8e8e1652a18a
Sam <samuelgardner101@gmail.com>
2022-07-18 23:48:39 +0100

    Moved concept into `template`, because `T` is needed for concise perfect-forwarding

--- src/javascript/web_apis/dom/mixins/parentable_node.hpp
+++ src/javascript/web_apis/dom/mixins/parentable_node.hpp
@@ -13,30 +13,30 @@ namespace dom::nodes {class node;}
 
 
 class dom::mixins::parentable_node
-        : public virtual web_apis::dom_object
+        : public virtual dom_object
 {
 public constructors:
     parentable_node();
 
 public js_methods:
-    unscopable auto prepend(type_is<nodes::node*, ext::string> auto&&... nodes) -> nodes::node*;
-    unscopable auto append(type_is<nodes::node*, ext::string> auto&&... nodes) -> nodes::node*;
-    unscopable auto replace_children(type_is<nodes::node*, ext::string> auto&&... nodes) -> nodes::node*;
+    unscopable template <type_is<nodes::node*, ext::string> ...T> auto prepend(T&&... nodes) -> nodes::node*;
+    unscopable template <type_is<nodes::node*, ext::string> ...T> auto append(T&&... nodes) -> nodes::node*;
+    unscopable template <type_is<nodes::node*, ext::string> ...T> auto replace_children(T&&... nodes) -> nodes::node*;
 
     auto query_selector(ext::string_view selectors);
     auto query_selector_all(ext::string_view selectors);
 
 public js_properties:
-    ext::property<ranges::any_view<nodes::element*>> children;
+    ext::property<ranges::any_view<nodes::element*, ranges::category::sized | ranges::category::forward>> children;
     ext::property<nodes::element*> first_element_child;
     ext::property<nodes::element*> last_element_child;
     ext::property<size_t> child_element_count;
 
 public cpp_methods:
-    auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
+    auto to_v8(v8::Isolate* isolate) const && -> ext::any override;
 
 private cpp_accessors:
-    [[nodiscard]] auto get_children() const -> ranges::any_view<nodes::element*>;
+    [[nodiscard]] auto get_children() const -> ranges::any_view<nodes::element*, ranges::category::sized | ranges::category::forward>;
     [[nodiscard]] auto get_first_element_child() const -> nodes::element*;
     [[nodiscard]] auto get_last_element_child() const -> nodes::element*;
     [[nodiscard]] auto get_child_element_count() const -> size_t;
