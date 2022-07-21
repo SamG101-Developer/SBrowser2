2a1cdcd9e954768f322db8b78e2938363ea2d3d5
Sam <samuelgardner101@gmail.com>
2022-07-21 23:19:12 +0100

    New properties not finished)

--- src/javascript/web_apis/url/url.hpp
+++ src/javascript/web_apis/url/url.hpp
@@ -5,7 +5,8 @@
 namespace url {class url_object;}
 
 
-class url::url_object : public virtual web_apis::dom_object
+class url::url_object
+        : public virtual dom_object
 {
 public constructors:
     url_object(ext::string_view url_string = "");
@@ -17,13 +18,23 @@ public js_methods:
 
 public js_properties:
     ext::property<ext::string> scheme;
+    ext::property<ext::string> username;
+    ext::property<ext::string> password;
+    ext::property<ext::string> host;
+    ext::property<ext::string> port;
 
 public cpp_operators:
     auto operator=(ext::string_view url) -> url_object&;
+    operator bool() const {return !m_url.empty();}
+    auto operator!() const -> ext::boolean {return m_url.empty();}
+    auto operator==(const url::url_object& other) const -> ext::boolean;
 
 public cpp_methods:
     auto to_v8(v8::Isolate* isolate) const && -> ext::any override;
     auto is_opaque() const -> ext::boolean; // TODO : move to `detail` helper
+
+private cpp_properties:
+    ext::string m_url;
 };
 
 
