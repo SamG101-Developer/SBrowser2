e9b5fedc0b998c9aab50353697a48ce0b52e221a
Sam <samuelgardner101@gmail.com>
2022-07-21 23:14:28 +0100

    Fixed templating

--- src/ext/detail/guard_property.hpp
+++ src/ext/detail/guard_property.hpp
@@ -1,7 +1,7 @@
 #ifndef SBROWSER2_GUARD_PROPERTY_HPP
 #define SBROWSER2_GUARD_PROPERTY_HPP
 
-namespace ext::detail {template <typename _Tx> struct guard_property;}
+namespace ext::detail {template <typename _Tx, bool c> struct guard_property;}
 
 #include "ext/detail/meta_property.hpp"
 
@@ -9,15 +9,15 @@ namespace ext::detail {template <typename _Tx> struct guard_property;}
 #define property_guard(p) ext::detail::guard_property _PropertyGuard{p}
 
 
-template <typename _Tx>
+template <typename _Tx, bool c>
 struct ext::detail::guard_property
 {
 public:
-    explicit guard_property(const ext::property<_Tx>& _PropertyToGuard): _Property(_PropertyToGuard) {unlock_property(_PropertyToGuard);}
+    explicit guard_property(ext::property<_Tx, c>& _PropertyToGuard): _Property(_PropertyToGuard) {unlock_property(_PropertyToGuard);}
     ~guard_property() {lock_property(_Property);}
 
 private:
-    ext::property<_Tx>& _Property;
+    ext::property<_Tx, c>& _Property;
 };
 
 #endif //SBROWSER2_GUARD_PROPERTY_HPP
