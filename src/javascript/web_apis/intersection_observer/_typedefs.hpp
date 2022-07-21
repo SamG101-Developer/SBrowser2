50cb8001f4b9e43de33379e2fa164413e4323b7b
Sam <samuelgardner101@gmail.com>
2022-07-21 23:06:13 +0100

    Created for common types across the `intersection_observer` spec

---
+++ src/javascript/web_apis/intersection_observer/_typedefs.hpp
@@ -0,0 +1,15 @@
+#ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_INTERSECTION_OBSERVER__TYPEDEFS_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_INTERSECTION_OBSERVER__TYPEDEFS_HPP
+
+#include "ext/functional.hpp"
+#include "ext/vector.hpp"
+namespace intersection_observer {class intersection_observer;}
+namespace intersection_observer {class intersection_observer_entry;}
+
+
+namespace intersection_observer
+{
+    using intersection_observer_callback = ext::function<void(ext::vector<intersection_observer_entry*> entries, intersection_observer* observer)>;
+}
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_INTERSECTION_OBSERVER__TYPEDEFS_HPP
