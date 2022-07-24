e56f803d92f99b298ac580b466a37ed7af507db1
Sam <samuelgardner101@gmail.com>
2022-07-24 22:31:05 +0100

    Template parameter names (formatting)

--- src/ext/map.hpp
+++ src/ext/map.hpp
@@ -3,16 +3,17 @@
 #define SBROWSER2_MAP_HPP
 
 #include "ext/keywords.hpp"
+#include "ext/pair.hpp"
 #include <tsl/robin_map.h>
 
 
 _EXT_BEGIN
 
-template <typename _Vt1, typename _Vt2>
-using map = tsl::robin_map<_Vt1, _Vt2>;
+template <typename _Vt0, typename _Vt1>
+using map = tsl::robin_map<_Vt0, _Vt1, std::hash<_Vt0>, std::equal_to<_Vt0>, std::allocator<ext::pair<_Vt0, _Vt1>>>;
 
-template <typename _Vt1, typename _Vt2>
-using map_view = const _EXT map<_Vt1, _Vt2>&;
+template <typename _Vt0, typename _Vt1>
+using map_view = const _EXT map<_Vt0, _Vt1>&;
 
 _EXT_END
 
