d07ab16fb96a8e28f580e533d689e5447faeb89e
Sam <samuelgardner101@gmail.com>
2022-07-24 22:31:45 +0100

    Made class more complete

--- src/javascript/web_apis/permissions_policy/permissions_policy.hpp
+++ src/javascript/web_apis/permissions_policy/permissions_policy.hpp
@@ -3,19 +3,29 @@
 #define SBROWSER2_PERMISSIONS_POLICY_HPP
 
 #include "dom_object.hpp"
-namespace permissions_policy {class permissions_policy;}
+namespace permissions_policy {class permissions_policy_object;}
 
 #include "ext/vector.hpp"
+namespace dom::nodes {class node;}
 
 
-class permissions_policy::permissions_policy
-        : public virtual web_apis::dom_object
+class permissions_policy::permissions_policy_object
+        : public virtual dom_object
 {
 public js_methods:
-    auto allows_feature(ext::string_view feature, ext::string_view origin = "") -> ext::boolean;
-    auto features() -> ext::string_vector;
-    auto allowed_features() -> ext::string_vector;
-    auto get_allowlist_for_feature(ext::string_view feature) -> ext::string_vector;
+    auto allows_feature(ext::string&& feature, ext::string&& origin = "") -> ext::boolean;
+    auto features() -> ext::set<ext::string>;
+    auto allowed_features() -> ext::set<ext::string>;
+    auto get_allowlist_for_feature(ext::string&& feature) -> ext::set<ext::string>;
+
+public cpp_methods:
+    auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
+
+private cpp_methods:
+    auto m_default_origin() const -> ext::string;
+
+private cpp_properties:
+    dom::nodes::node* m_associated_node;
 };
 
 
