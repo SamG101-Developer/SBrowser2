7ac40c64bc06d9f613f442109992f5588b030d87
Sam <samuelgardner101@gmail.com>
2022-08-07 22:32:46 +0100

    Changed to have a base implementation which declares the `auto operator[](...)` as a pure virtual method, and a `..._linked` which allows for a container reference to be attached

--- src/ext/map_like.hpp
+++ src/ext/map_like.hpp
@@ -9,11 +9,42 @@
 
 _EXT_BEGIN
 
+/**
+ * The `map_like` class is inherited as a mixin into any other class that wishes to use the [] operator to retrieve data
+ * in some form. This can be from another map, a set of attributes, calling another method etc, so a pure virtual method
+ * is defined, forcing the [] operator to be implemented.
+ * @tparam _Tx1 The type that the keys of the map are
+ * @tparam _Tx2 The type that the vals of the map are
+ */
 template <typename _Tx1, typename _Tx2>
 class map_like
 {
 public cpp_operators:
     virtual auto operator[](const _Tx1& _Key) -> _Tx2 = 0;
+};
+
+
+/**
+ * The `map_like_linked` class is an extension to the inherited `nap_like` clas, as there is a constructor that allows
+ * for a reference to a map to be linked to, and the [] operator is defined to access this linked map. In other words,
+ * it is syntactic sugar for using the `map_like` class, and defining the [] operator in every class to use a `map`
+ * attribute
+ * @tparam _Tx1 The type that the keys of the map are
+ * @tparam _Tx2 The type that the vals of the map are
+ */
+template <typename _Tx1, typename _Tx2>
+class map_like_linked : public map_like<_Tx1, _Tx2>
+{
+public constructors:
+    explicit map_like_linked(map<_Tx1, _Tx2>& map)
+            : m_linked_map(map)
+    {}
+
+public cpp_operators:
+    auto operator[](const _Tx1& _Key) -> _Tx2 override {return m_linked_map.at(_Key);}
+
+private cpp_properties:
+    const map<_Tx1, _Tx2>& m_linked_map;
 };
 
 _EXT_END
--- src/ext/vector_like.hpp
+++ src/ext/vector_like.hpp
@@ -10,24 +10,33 @@
 
 _EXT_BEGIN
 
-template <typename _Tx>
+
+template <typename T>
 class vector_like
 {
+public cpp_operators:
+    virtual auto operator[](const number<size_t>& _Idx) -> T& = 0;
+};
+
+template <typename _Tx>
+class vector_like_linked : public vector_like<_Tx>
+{
 public constructors:
-    explicit vector_like(_EXT vector<_Tx>* _Container) : _LinkedContainer{_Container} {bind_get(length);};
+    explicit vector_like_linked(vector<_Tx>* _Container) : _LinkedContainer{_Container} {bind_get(length);};
 
 public cpp_properties:
     property<number<size_t>> length;
 
 public cpp_operators:
-    auto operator[](const number<size_t>& _Idx) -> optional<_Tx&> {return _LinkedContainer->at(_Idx);};
+    auto operator[](const number<size_t>& _Idx) -> _Tx& {return _LinkedContainer->at(_Idx);};
 
 private cpp_properties:
-    std::unique_ptr<_EXT vector<_Tx>> _LinkedContainer;
+    std::unique_ptr<vector<_Tx>> _LinkedContainer;
 
 private cpp_accessors:
-    [[nodiscard]] auto get_length() const -> _EXT number<size_t> {return _LinkedContainer->size();};
+    [[nodiscard]] auto get_length() const -> number<size_t> {return _LinkedContainer->size();};
 };
+
 
 _EXT_END
 
