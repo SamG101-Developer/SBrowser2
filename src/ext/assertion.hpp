523f70ca7cbd1f6031091b61a08a8dae1f3a3f60
Sam <samuelgardner101@gmail.com>
2022-08-08 10:49:22 +0100

    Use new assert library

---
+++ src/ext/assertion.hpp
@@ -0,0 +1,15 @@
+#ifndef SBROWSER2_ASSERTION_HPP
+#define SBROWSER2_ASSERTION_HPP
+
+#include "ext/keywords.hpp"
+#include <assert.hpp>
+
+
+_EXT_BEGIN
+
+using namespace libassert;
+
+_EXT_END
+
+
+#endif //SBROWSER2_ASSERTION_HPP
