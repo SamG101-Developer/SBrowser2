1ac88cb568f70bb547e5ecb761a25d4aeb6ac8df
Sam <samuelgardner101@gmail.com>
2022-08-09 23:56:38 +0100

    Implemented

--- src/javascript/web_apis/mediacapture_main/mixins/constrainable.cpp
+++ src/javascript/web_apis/mediacapture_main/mixins/constrainable.cpp
@@ -1,1 +1,76 @@
 #include "constrainable.hpp"
+
+#include "ext/casting.hpp"
+
+#include "dom_object.hpp"
+#include "mediacapture_main/media_stream_track.hpp"
+#include "mediacapture_main/details/constrain_internals.hpp"
+
+#include <future>
+
+
+auto mediacapture::main::mixins::constrainable::get_capabilities()
+        const -> ext::map<ext::string, ext::any>
+{
+    // get the data in the [[capabilities]] internal slot
+    return s_capabilities;
+}
+
+
+auto mediacapture::main::mixins::constrainable::get_constraints()
+        const -> ext::map<ext::string, ext::any>
+{
+    // get the data in the [[constraints]] internal slot
+    return s_constraints;
+}
+
+
+auto mediacapture::main::mixins::constrainable::get_settings()
+        const -> ext::map<ext::string, ext::any>
+{
+    // get the base MediaStreamTrack object from this mixin
+    auto* base = dom_cross_cast<const main::media_stream_track*>(this);
+
+    // if the track has ended, then return a dictionary containing certain properties, otherwise return the data in the
+    // [[settings]] internal slot
+    return base->ready_state() == "ended"
+            ? ext::map<ext::string, ext::any>{
+                {"deviceId", base->device_id()},
+                {"facingMode", base->facing_mode},
+                {"groupId", base->group_id()}}
+            : s_settings;
+}
+
+
+auto mediacapture::main::mixins::constrainable::apply_constraints(
+        ext::map_view<ext::string, ext::any> constraints)
+        -> std::promise<void>
+{
+    // get the base MediaStreamTrack object from this mixin
+    auto* base = dom_cross_cast<const main::media_stream_track*>(this);
+
+    if (base->ready_state() == "ended")
+    {
+        std::promise<void> promise;
+        promise.set_value();
+        return promise;
+    }
+
+    return detail::constrain_internals::apply_constrains_template_algorithm(this);
+}
+
+
+auto mediacapture::main::mixins::constrainable::to_v8(
+        v8::Isolate* isolate)
+        const && -> ext::any
+{
+    return v8pp::class_<constrainable>{isolate}
+            .inherit<dom_object>()
+            .function("getCapabilities", &constrainable::get_capabilities)
+            .function("getConstraints", &constrainable::get_constraints)
+            .function("getSettings", &constrainable::get_settings)
+            .slot("capabilities", &constrainable::s_capabilities)
+            .slot("constraints", &constrainable::s_constraints)
+            .slot("settings", &constrainable::s_settings)
+            .auto_wrap_objects();
+}
