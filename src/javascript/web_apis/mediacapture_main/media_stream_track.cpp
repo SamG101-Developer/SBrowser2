652214df528677761f9d552003eaa8307fd58c67
Sam <samuelgardner101@gmail.com>
2022-08-09 23:58:39 +0100

    Declared another method

--- src/javascript/web_apis/mediacapture_main/media_stream_track.cpp
+++ src/javascript/web_apis/mediacapture_main/media_stream_track.cpp
@@ -42,6 +42,19 @@ auto mediacapture::main::media_stream_track::clone()
 }
 
 
+auto mediacapture::main::media_stream_track::stop() -> void
+{
+    // if the track has already ended, then this method will do nothing, so return early at the return-guard, otherwise
+    // set the ready state to ended, so that the track knows it has ended, and progress with the rest of the method
+    return_if(ready_state() == "ended");
+    ready_state = "ended";
+
+    // notify the source that the track has ended so that the source can be stopped, and then fire an "ended" event at
+    // the 'track', so that any listeners that want to know when a track has ended can be executed
+    // TODO : notify source that the track has ended
+}
+
+
 auto mediacapture::main::media_stream_track::set_muted(
         ext::boolean_view val)
         -> void
