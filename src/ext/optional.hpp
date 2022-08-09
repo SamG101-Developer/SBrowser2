27f516a93ba1161648f7a3cca6fdb2d9358b2304
Sam <samuelgardner101@gmail.com>
2022-08-09 23:58:52 +0100

    Integrated new `tl::optional<T>` library

--- src/ext/optional.hpp
+++ src/ext/optional.hpp
@@ -3,116 +3,12 @@
 #define SBROWSER2_OPTIONAL_HPP
 
 #include "ext/keywords.hpp"
-#include <optional>
+#include <tl/optional.hpp>
 
 
 _EXT_BEGIN
 
-constexpr std::nullopt_t null {std::nullopt_t::_Tag{}};
-
-
-template <typename T>
-class optional final
-{
-public constructors:
-    optional() = default;
-
-    optional(const optional&) = delete;
-    optional(optional&&) noexcept = default;
-    auto operator=(const optional&) -> optional& = delete;
-    auto operator=(optional&&) noexcept -> optional& = delete;
-
-    explicit optional(const T& value) : internal_optional{std::make_optional<T>(value)} {}
-    explicit optional(T&& value) noexcept : internal_optional{std::make_optional<T>(std::forward<T>(value))} {}
-    auto operator=(const T& value) -> optional& {internal_optional.emplace(value); return *this;}
-    auto operator=(T&& value) -> optional& {internal_optional.emplace(std::forward<T>(value)); return *this;}
-
-    optional(const std::nullopt_t&) {}
-    auto operator=(const std::nullopt_t&) -> optional& {internal_optional.reset();}
-
-public cpp_methods:
-    [[nodiscard]] constexpr auto empty() const -> boolean {return not internal_optional.has_value();}
-    [[nodiscard]] constexpr auto has_value() const -> boolean {return internal_optional.has_value();}
-
-    constexpr auto value() const;
-    constexpr auto value_or(T&& other) const;
-    constexpr auto not_value_or(T&& other) const;
-    template <typename U> constexpr auto value_to_or(U&& other) const -> U requires (type_is<T, any>);
-
-    template <typename U> auto has_value_and_equals(U&& other) const -> boolean;
-    template <typename U> auto has_value_and_not_equals(U&& other) const -> boolean;
-    template <typename U> auto empty_or_equals(U&& other) const -> boolean;
-    template <typename U> auto empty_or_not_equals(U&& other) const -> boolean;
-    
-public cpp_operators:
-    auto operator->() const -> auto {return value();}
-    template <typename T> // TODO : constrain against ext::optional<T>
-    auto operator==(T&& other) const -> ext::boolean {return has_value_and_equals(std::forward<T>(other));}
-
-private cpp_properties:
-    std::optional<T> internal_optional;
-};
-
-
-template <typename T>
-constexpr auto optional<T>::value() const
-{
-    return internal_optional.value();
-}
-
-
-template <typename T>
-constexpr auto optional<T>::value_or(T&& other) const
-{
-    return internal_optional.value_or(std::forward<T>(other));
-}
-
-
-template <typename T>
-constexpr auto optional<T>::not_value_or(T&& other) const
-{
-    return empty() ? null : std::forward<T>(other);
-}
-
-
-template <typename T>
-template <typename U>
-constexpr auto optional<T>::value_to_or(U&& other) const -> U requires (type_is<T, any>)
-{
-    return empty() ? std::forward<U>(other) : value().template to<U>();
-}
-
-
-template <typename T>
-template <typename U>
-auto optional<T>::has_value_and_equals(U&& other) const -> boolean
-{
-    return has_value() && value() == std::forward<U>(other);
-}
-
-
-template <typename T>
-template <typename U>
-auto optional<T>::has_value_and_not_equals(U&& other) const -> boolean
-{
-    return has_value() && value() != std::forward<U>(other);
-}
-
-
-template <typename T>
-template <typename U>
-auto optional<T>::empty_or_equals(U&& other) const -> boolean
-{
-    return empty() || value() == std::forward<U>(other);
-}
-
-
-template <typename T>
-template <typename U>
-auto optional<T>::empty_or_not_equals(U&& other) const -> boolean
-{
-    return empty() || value() != std::forward<U>(other);
-}
+using namespace tl;
 
 _EXT_END
 
