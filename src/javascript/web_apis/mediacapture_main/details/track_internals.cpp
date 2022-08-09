78defe830262556ef97adabfcc6353b061e54e10
Sam <samuelgardner101@gmail.com>
2022-08-09 23:59:48 +0100

    Corrected task queuing

--- src/javascript/web_apis/mediacapture_main/details/track_internals.cpp
+++ src/javascript/web_apis/mediacapture_main/details/track_internals.cpp
@@ -2,7 +2,7 @@
 
 #include "dom/detail/event_internals.hpp"
 #include "dom/detail/observer_internals.hpp"
-#include "html/detail/task_internals.hpp"
+
 #include "mediacapture_main/media_stream_track.hpp"
 
 
@@ -10,22 +10,8 @@ auto mediacapture::detail::track_internals::track_ends_for_any_reason_except_sto
         main::media_stream_track* track)
         -> void
 {
-    auto task = [track]
-    {
-        // if the track has already ended, then this method will do nothing, so return early at the return-guard, otherwise
-        // set the ready state to ended, so that the track knows it has ended, and progress with the rest of the method
-        return_if(track->ready_state() == "ended");
-        track->ready_state = "ended";
-
-        // notify the source that the track has ended so that the source can be stopped, and then fire an "ended" event at
-        // the 'track', so that any listeners that want to know when a track has ended can be executed
-        // TODO : notify source that the track has ended
-        dom::detail::event_internals::fire_event("ended", track);
-    };
-
-    // queue the above steps onto the dom manipulation task source TODO : is this the correct task source, and queue
-    //  method?
-    dom::detail::observer_internals::queue_task(
-            html::detail::task_internals::dom_manipulation_task_source(),
-            task);
+    // queue steps to stop the track and fire an "ended" event at the track
+    dom::detail::observer_internals::queue_microtask(
+            [&track]
+            {track->stop(); dom::detail::event_internals::fire_event("ended", track);});
 }
