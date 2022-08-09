1ada676dbe9d5e23c7908fd7782325b21629c681
Sam <samuelgardner101@gmail.com>
2022-08-09 10:33:15 +0100

    Declared other methods

--- src/javascript/web_apis/mediacapture_main/media_stream_track.cpp
+++ src/javascript/web_apis/mediacapture_main/media_stream_track.cpp
@@ -1,8 +1,9 @@
 #include "media_stream_track.hpp"
 
-#include "mediacapture_main/details/source_internals.hpp"
-
 #include "ext/uuid.hpp"
+
+#include "dom/detail/event_internals.hpp"
+#include "mediacapture_main/details/source_internals.hpp"
 
 
 template <inherit<mediacapture::main::media_stream_track> T>
@@ -16,7 +17,41 @@ mediacapture::main::media_stream_track::media_stream_track(
         , ready_state{"live"}
         , enabled{ext::boolean::TRUE_()}
         , muted{source.muted}
+        , m_source{source}
 {
     if (tie_source_to_context) detail::source_internals::tie_track_source_to_context(source);
     source.source_specific_construction_steps(this);
+}
+
+
+auto mediacapture::main::media_stream_track::clone()
+        const -> media_stream_track
+{
+    // create a new MediaStreamTrack object that has the same source as this track, and clone the '[[capabilities]],
+    // [[constraints]], and the [[settings]], by using the copy constructor of the `ext::map<ext::string, ext::any>`
+    // objects
+    media_stream_track track_clone{m_source, ext::boolean::FALSE_()};
+    track_clone.s_capabilities = ext::map<ext::string, ext::any>{s_capabilities};
+    track_clone.s_constraints = ext::map<ext::string, ext::any>{s_constraints};
+    track_clone.s_settings = ext::map<ext::string, ext::any>{s_settings};
+
+    // run the coning steps on the track, wit the current track and newly cloned track as the two parameters, and then
+    // return the newly cloned track
+    m_source.source_specific_cloning_steps(this, &track_clone);
+    return track_clone;
+}
+
+
+auto mediacapture::main::media_stream_track::set_muted(
+        ext::boolean_view val)
+        -> void
+{
+    // don't do anything if the 'new_state' is the same as the current state, because otherwise multiple "mute" /
+    // "unmute" event would be received when there is no change to the data
+    return_if(muted() == val);
+    property_guard(muted);
+    *muted = val;
+
+    // fire an event that is either "mute" or "unmute" depending on the 'new_state', at this track
+    dom::detail::event_internals::fire_event(val ? "mute" : "unmute", this);
 }
