9f73906626571868833037b9c959cb908227c663
Sam <samuelgardner101@gmail.com>
2022-08-11 12:28:50 +0100

    Added new functors for range usage (`pred` etc)

--- src/ext/functional.hpp
+++ src/ext/functional.hpp
@@ -14,6 +14,21 @@ _EXT_BEGIN
 
 using namespace fu2;
 
+
+struct identity
+{
+    template <typename T>
+    constexpr auto operator()(T&& object) const {return std::forward<T>(object);}
+};
+
+
+struct invoke
+{
+    template <typename T>
+    constexpr auto operator()(T&& object) const {return object();}
+};
+
+
 // bind arguments to the back of a method, so that when the partial-method is called with arguments, they will the front
 // n arguments (using because std::bind_back isn't available yet on MSVC)
 template <typename F, typename ...Ts>
@@ -22,7 +37,7 @@ struct bind_back
 public:
     explicit bind_back(const F& function, Ts&&... fixed_args)
             : m_function{function}
-            , m_back_args{tuplet::make_tuple(std::forward<Ts>(fixed_args)...)}
+            , m_back_args{ext::make_tuple(std::forward<Ts>(fixed_args)...)}
     {
         // initialize the function and the back arguments, by forwarding the arguments into a tuple and setting this as
         // 'm_back_args' TODO: ext::forward_as_tuple doesn't work here
@@ -33,8 +48,8 @@ public:
     {
         // forward the new arguments into a tuple, concatenate this to the beginning of the 'm_front_args', and apply this
         // tuple to the 'm_function', so that the function is invoked with all the arguments
-        auto m_front_args = tuplet::make_tuple(std::forward<Ts1>(variable_args)...);
-        return tuplet::apply(m_function, tuplet::tuple_cat(m_front_args, m_back_args));
+        auto m_front_args = ext::make_tuple(std::forward<Ts1>(variable_args)...);
+        return ext::apply(m_function, ext::tuple_cat(m_front_args, m_back_args));
     }
 
 private:
@@ -51,7 +66,7 @@ struct bind_front
 public:
     explicit bind_front(const F& function, Ts&&... fixed_args)
             : m_function{function}
-            , m_front_args{tuplet::make_tuple(std::forward<Ts>(fixed_args)...)}
+            , m_front_args{ext::make_tuple(std::forward<Ts>(fixed_args)...)}
     {
         // initialize the function and the front arguments, by forwarding the arguments into a tuple and setting this as
         // as 'm_front_args' TODO: ext::forward_as_tuple doesn't work here
@@ -62,8 +77,8 @@ public:
     {
         // forward the new arguments into a tuple, concatenate this to the end of the 'm_back_args', and apply this tuple
         // to the 'm_function', so that the function is invoked with all the arguments
-        auto m_back_args = tuplet::make_tuple(std::forward<Ts1>(variable_args)...);
-        return tuplet::apply(m_function, tuplet::tuple_cat(m_front_args, m_back_args));
+        auto m_back_args = ext::make_tuple(std::forward<Ts1>(variable_args)...);
+        return ext::apply(m_function, ext::tuple_cat(m_front_args, m_back_args));
     }
 
 private:
