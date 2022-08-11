b2921301f2689010145c26c988aad292576e5c73
Sam <samuelgardner101@gmail.com>
2022-08-11 12:31:53 +0100

    Added methods/properties from other specs

--- src/javascript/web_apis/html/other/navigator.hpp
+++ src/javascript/web_apis/html/other/navigator.hpp
@@ -3,13 +3,34 @@
 
 #include "dom_object.hpp"
 namespace html::other {class navigator;}
+
+#include "ext/map.hpp"
+namespace dom::other {class dom_exception;}
 namespace mediacapture::main {class media_devices;}
+namespace mediacapture::main {class media_stream;}
+namespace permissions {class permission;}
 
 
 class html::other::navigator
 {
+private aliases:
+    using navigator_user_media_success_callback = ext::function<void(mediacapture::main::media_stream*)>;
+    using navigator_user_media_error_callback   = ext::function<void(dom::other::dom_exception*)>;
+
+public js_methods:
+    /* MEDIACAPTURE_MAIN */
+    auto get_user_media(
+            ext::map<ext::string, ext::any>&& constraints,
+            navigator_user_media_success_callback&& success_callback,
+            navigator_user_media_error_callback&& error_callback)
+            const -> void;
+
+public js_properties:
     /* MEDIACAPTURE_MAIN */
     ext::property<mediacapture::main::media_devices*> media_devices;
+
+    /* PERMISSIONS */
+    ext::property<permissions::permission*> permissions;
 
 };
 
