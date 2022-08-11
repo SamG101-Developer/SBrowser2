63e9064b58869e178118965d1d6a0c72a6f75428
Sam <samuelgardner101@gmail.com>
2022-08-11 12:33:58 +0100

    Formatting

--- src/ext/vector.hpp
+++ src/ext/vector.hpp
@@ -5,29 +5,28 @@
 #include <veque.hpp>
 #include "ext/view.hpp"
 
+#define vector_view_iterator typename vector<T>::iterator
+
 
 _EXT_BEGIN
 
-template <typename Ts>
-using vector = veque::veque<Ts>;
-
-
-#define vector_view_iterator typename vector<T>::pointer
+template <typename T>
+using vector = veque::veque<T>;
 
 // an `ext::vector_view<T>` is a view whose iterator is the iterator of an `ext::vector<T>` with template arguments. it
 // differs from the base `ext::view<T>` because there is a custom constructor that converts the normal `ext::vector<T>`
 // to the 'ext::vector_view<T>`
 template <typename T>
-struct vector_view : public view<T>
+struct vector_view : public view<vector_view_iterator>
 {
     using view<vector_view_iterator>::view;
 
-    explicit vector_view(vector<T>&& other)
-            : view<vector_view_iterator>(&std::move(other.begin()), &std::move(other.end()))
+    vector_view(vector<T>&& other)
+            : view<vector_view_iterator>(std::move(other.begin()), std::move(other.end()))
     {}
 
-    explicit vector_view(const vector<T>& other)
-            : view<vector_view_iterator>{&other.begin(), other.end()}
+    vector_view(const vector<T>& other)
+            : view<vector_view_iterator>{other.begin(), other.end()}
     {}
 };
 
