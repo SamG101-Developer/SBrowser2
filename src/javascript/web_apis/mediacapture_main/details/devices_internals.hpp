703a28f5ea9b22cf345691c3d07c187101e82be1
Sam <samuelgardner101@gmail.com>
2022-08-11 12:27:27 +0100

    Defined new methods

--- src/javascript/web_apis/mediacapture_main/details/devices_internals.hpp
+++ src/javascript/web_apis/mediacapture_main/details/devices_internals.hpp
@@ -1,14 +1,76 @@
 #pragma once
+#include "mediacapture_main/media_devices.hpp"
 #ifndef SBROWSER2_DEVICES_INTERNALS_HPP
 #define SBROWSER2_DEVICES_INTERNALS_HPP
 
-
+#include "ext/boolean.hpp"
+#include "ext/vector.hpp"
+namespace dom::nodes {class document;}
 namespace mediacapture::main {class media_devices;}
+namespace mediacapture::main {class media_device_info;}
 
 namespace mediacapture::detail::devices_internals
 {
     auto on_page_load(
             main::media_devices* media_devices)
+            -> void;
+
+    auto device_change_notification_steps()
+            -> void; // TODO
+
+    auto all_available_devices()
+            -> ext::vector<main::media_devices*>;
+
+    auto is_system_default_microphone(
+            main::media_devices* device)
+            -> ext::boolean;
+
+    auto is_system_default_camera(
+            main::media_devices* device)
+            -> ext::boolean;
+
+    auto is_system_default_audio(
+            main::media_devices* device)
+            -> ext::boolean;
+
+    auto is_microphone(
+            main::media_devices* device)
+            -> ext::boolean;
+
+    auto is_camera(
+            main::media_devices* device)
+            -> ext::boolean;
+
+    auto create_list_of_device_info_objects(
+            ext::vector_view<main::media_devices*> device_list,
+            dom::nodes::document* document)
+            -> ext::vector<main::media_device_info*>;
+
+    auto create_device_info_object(
+            main::media_devices* device)
+            -> main::media_device_info;
+
+    auto device_enumeration_can_proceed()
+            -> ext::boolean;
+
+    auto device_information_can_be_exposed()
+            -> ext::boolean;
+
+    auto camera_information_can_be_exposed()
+            -> ext::boolean;
+
+    auto microphone_information_can_be_exposed()
+            -> ext::boolean;
+
+    auto other_information_can_be_exposed(
+            main::media_devices* device,
+            ext::vector_view<main::media_devices*> microphone_list,
+            ext::vector_view<main::media_devices*> camera_list)
+            -> ext::boolean;
+
+    auto set_device_information_exposure(
+            ext::set<ext::string>&& requested_types,
+            ext::boolean&& value)
             -> void;
 };
 
--- src/javascript/web_apis/permissions/detail/permission_internals.hpp
+++ src/javascript/web_apis/permissions/detail/permission_internals.hpp
@@ -9,13 +9,13 @@
 #include "ext/map.hpp"
 #include "ext/string.hpp"
 
+#include "permissions/_typedefs.hpp"
+
 #include <v8-object.h>
 namespace permissions {class permission_status;}
 
 namespace permissions::detail::permission_internals
 {
-    enum class permission_state_t {DENIED, GRANTED, PROMPT};
-
     struct powerful_feature_t;
 
     auto get_current_permission_state(
@@ -24,36 +24,43 @@ namespace permissions::detail::permission_internals
             -> permission_state_t;
 
     auto permission_state(
-            ext::map<ext::string, ext::any>&& permission_descriptor,
+            permissions_descriptor_t&& permission_descriptor,
             ext::optional<v8::Local<v8::Object>> environment_settings_object)
             -> permission_state_t;
 
     auto request_permission_to_use(
-            ext::map<ext::string, ext::any>&& permission_descriptor)
+            permissions_descriptor_t&& permission_descriptor)
             -> permission_state_t;
 
     auto prompt_user_to_choose(
-            ext::map<ext::string, ext::any>&& permission_descriptor,
+            permissions_descriptor_t&& permission_descriptor,
             ext::boolean_view allow_multiple = ext::boolean::FALSE_())
             -> ext::variant<ext::vector<permissions_policy::detail::feature_t>, permission_state_t>;
+
+    auto extra_permissions_data()
+            -> permissions_descriptor_t;
+
+    auto default_permission_query_algorithm(
+            permissions_descriptor_t&& permission_descriptor,
+            permissions_result_t* status)
+            -> void;
+
+    auto react_to_user_revoking_permission()
+            -> void;
 };
 
 
 struct permissions::detail::permission_internals::powerful_feature_t
 {
-    powerful_feature_t();
+    ext::string name;
 
-    using permissions_descriptor_type = ext::map<ext::string, ext::any>;
-    using permissions_result_type = permission_status;
-    using extra_permission_data_type = permission_state_t;
-
-    ext::map<ext::string, ext::any> permission_state_constraints;
-    ext::function<void(permissions_descriptor_type&& permission_descriptor, permissions_result_type* status)> permission_query_algorithm;
     ext::number<long double> permission_life; // TODO : steps when it expires
     permission_state_t default_permission_state;
 
-    auto extra_permissions_data() -> ext::map<ext::string, ext::any>;
-    auto extra_permissions_data_constraints() -> ext::map<ext::string, ext::any>;
+    auto permission_state_constraints() -> permissions_descriptor_t;
+    auto extra_permissions_data_constraints() -> permissions_descriptor_t;
+    auto permission_query_algorithm(permissions_descriptor_t&& permission_descriptor, permissions_result_t* status) -> void;
+    auto permission_revocation_algorithm() -> void;
 };
 
 
