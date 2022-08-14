9aba4814046bdf8ad06077aef1eceb90d97d5e57
Sam <samuelgardner101@gmail.com>
2022-08-15 00:19:34 +0100

    Removed block comment (messy)

--- src/ext/map.hpp
+++ src/ext/map.hpp
@@ -15,13 +15,7 @@ using namespace tsl;
 template <typename K, typename V>
 using map = tsl::robin_map<K, V>;
 
-/**
- * An `ext::map_view<K, V>` is a view whose iterator is the iterator of an `ext::map<K, V>` with template arguments. It
- * differs from the base `ext::view<T>` because there is a custom constructor that converts the normal `ext::map<K, V>`
- * to the 'ext::map_view<K, V>`.
- * @tparam K The type that the keys of the map are
- * @tparam V The type that the vals of the map are
- */
+
 template <typename K, typename V>
 class map_view : public view<map_view_iterator>
 {
