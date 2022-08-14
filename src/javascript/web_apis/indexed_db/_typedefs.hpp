dca47d42f9cfc5f649f17871aa7e8b67827ef7ad
Sam <samuelgardner101@gmail.com>
2022-08-14 13:41:30 +0100

    Moved types from other `detail::xxx` namespaces into the `_typedefs.hpp` file

--- src/javascript/web_apis/indexed_db/_typedefs.hpp
+++ src/javascript/web_apis/indexed_db/_typedefs.hpp
@@ -11,6 +11,12 @@
 #include "ext/variant.hpp"
 #include "ext/vector.hpp"
 
+#include "dom/nodes/node.hpp"
+#include "dom/events/event.hpp"
+namespace indexed_db {class idb_object_store;}
+namespace indexed_db {class idb_index;}
+
+
 namespace indexed_db::detail
 {
     using number_key_t = ext::number<double>;
@@ -45,6 +51,69 @@ namespace indexed_db::detail
     enum class transaction_state_t {ACTIVE, INACTIVE, COMMITTING, FINISHED};
     enum class durability_hint_t {STRICT, RELAXED, DEFAULT};
     enum class cursor_direction_t {NEXT, NEXT_UNIQUE, PREV, PREV_UNIQUE};
+
+    using database_name_t = ext::string;
+    using database_version_t = ext::number<ulonglong>;
+    using key_path_t = ext::vector<ext::string>;
+
+    struct record_t;
+    struct key_generator_t;
+    struct connection_t;
+    struct key_range_t;
+
+    template <typename T>
+    concept source_like_c = requires
+    {
+        requires type_is<T, indexed_db::idb_object_store*, indexed_db::idb_index*>;
+        {T::m_records} -> std::same_as<ext::vector<record_t*>&>;
+    };
 }
+
+
+struct indexed_db::detail::record_t
+{
+public constructors:
+    ~record_t();
+
+public cpp_operators:
+    auto operator==(const record_t& other) const;
+    auto operator<=>(const record_t& other) const -> int;
+
+public cpp_properties:
+    any_key_t key;
+    ext::any value;
+};
+
+
+struct indexed_db::detail::key_generator_t
+{
+public cpp_properties:
+    ext::number<long> current_number = 1;
+};
+
+
+struct indexed_db::detail::connection_t
+        : protected dom::nodes::event_target
+{
+public constructors:
+    ~connection_t() {close_pending_flag = true;}
+
+public cpp_properties:
+    ext::number<int> version;
+    ext::boolean close_pending_flag;
+    ext::set<idb_object_store*> object_store_set;
+
+protected cpp_methods:
+    auto get_the_parent(dom::events::event* event) -> dom::nodes::event_target* override {return nullptr;}
+};
+
+
+struct indexed_db::detail::key_range_t
+{
+    std::unique_ptr<record_t> lower_bound;
+    std::unique_ptr<record_t> upper_bound;
+    ext::boolean lower_open_flag;
+    ext::boolean upper_open_flag;
+};
 
 #endif //SBROWSER2_JAVASCRIPT_WEB_APIS_INDEXED_DB__TYPEDEFS_HPP
