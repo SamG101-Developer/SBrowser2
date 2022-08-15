f5f51cd645b25c7b6cd428c55026146c01f7d35f
Sam <samuelgardner101@gmail.com>
2022-08-16 00:29:50 +0100

    Implemented methods

--- src/javascript/web_apis/streams/readable/readable_stream_byob_request.cpp
+++ src/javascript/web_apis/streams/readable/readable_stream_byob_request.cpp
@@ -1,4 +1,39 @@
 #include "readable_stream_byob_request.hpp"
 
+#include "dom/detail/exception_internals.hpp"
 
 
+auto streams::readable::readable_stream_byob_request::respond(
+        const ext::number<ulonglong>& bytes_written)
+        -> void
+{
+    dom::detail::exception_internals::throw_v8_exception<V8_TYPE_ERROR>(
+    [this] {return s_view.ByteLength() == 0 || s_view.Buffer()->ByteLength() == 0},
+            "Cannot enqueue to a ReadableByteStreamController with an view");
+
+    dom::detail::exception_internals::throw_v8_exception<V8_TYPE_ERROR>(
+            [this] {return !s_controller;},
+            "Cannot enqueue to a ReadableByteStreamController that doesn't have a controller");
+
+    dom::detail::exception_internals::throw_v8_exception<V8_TYPE_ERROR>(
+            [this] {return s_view.Buffer().IsEmpty();}, // TODO : IsEmpty() correct?
+            "Cannot enqueue to a ReadableByteStreamController that's [[stream]] isn't in the READABLE state");
+
+    detail::abstract_operations_internals::readable_byte_stream_controller_response(s_controller, bytes_written);
+}
+
+
+auto streams::readable::readable_stream_byob_request::respond_with_new_view(
+        v8::Local<v8::ArrayBufferView> view)
+        -> void
+{
+    dom::detail::exception_internals::throw_v8_exception<V8_TYPE_ERROR>(
+            [this] {return !s_controller;},
+            "Cannot enqueue to a ReadableByteStreamController that doesn't have a controller");
+
+    dom::detail::exception_internals::throw_v8_exception<V8_TYPE_ERROR>(
+            [this] {return s_view.Buffer().IsEmpty();}, // TODO : IsEmpty() correct?
+            "Cannot enqueue to a ReadableByteStreamController that's [[stream]] isn't in the READABLE state");
+
+    return detail::abstract_operations_internals::readable_byte_stream_controller_response_with_new_view(s_controller, view);
+}
