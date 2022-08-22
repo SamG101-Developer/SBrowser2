aa071d87688be182fe1fee9bd80c4ab38dd4afdc
Sam <samuelgardner101@gmail.com>
2022-08-22 14:58:14 +0100

    Renamed template arguments

--- src/ext/functional.hpp
+++ src/ext/functional.hpp
@@ -31,59 +31,59 @@ struct invoke
 
 // bind arguments to the back of a method, so that when the partial-method is called with arguments, they will the front
 // n arguments (using because std::bind_back isn't available yet on MSVC)
-template <typename F, typename ...Ts>
+template <typename F, typename ...Args0>
 struct bind_back
 {
 public:
-    explicit bind_back(const F& function, Ts&&... fixed_args)
+    explicit bind_back(const F& function, Args0&&... fixed_args)
             : m_function{function}
-            , m_back_args{ext::make_tuple(std::forward<Ts>(fixed_args)...)}
+            , m_back_args{ext::make_tuple(std::forward<Args0>(fixed_args)...)}
     {
         // initialize the function and the back arguments, by forwarding the arguments into a tuple and setting this as
         // 'm_back_args' TODO: ext::forward_as_tuple doesn't work here
     }
 
-    template <typename ...Ts1>
-    constexpr auto operator()(Ts1&&... variable_args) const
+    template <typename ...Args1>
+    constexpr auto operator()(Args1&&... variable_args) const
     {
         // forward the new arguments into a tuple, concatenate this to the beginning of the 'm_front_args', and apply this
         // tuple to the 'm_function', so that the function is invoked with all the arguments
-        auto m_front_args = ext::make_tuple(std::forward<Ts1>(variable_args)...);
+        auto m_front_args = ext::make_tuple(std::forward<Args1>(variable_args)...);
         return ext::apply(m_function, ext::tuple_cat(m_front_args, m_back_args));
     }
 
 private:
     const F& m_function;
-    tuple<Ts...> m_back_args;
+    tuple<Args0...> m_back_args;
 };
 
 
 // bind arguments to the front of a method, so that when the partial-method is called with arguments, they will the back
 // n arguments (using because std::bind_back isn't available yet on MSVC, so conform with ext::bind_back)
-template <typename F, typename ...Ts>
+template <typename F, typename ...Args0>
 struct bind_front
 {
 public:
-    explicit bind_front(const F& function, Ts&&... fixed_args)
+    explicit bind_front(const F& function, Args0&&... fixed_args)
             : m_function{function}
-            , m_front_args{ext::make_tuple(std::forward<Ts>(fixed_args)...)}
+            , m_front_args{ext::make_tuple(std::forward<Args0>(fixed_args)...)}
     {
         // initialize the function and the front arguments, by forwarding the arguments into a tuple and setting this as
         // as 'm_front_args' TODO: ext::forward_as_tuple doesn't work here
     };
 
-    template <typename ...Ts1>
-    constexpr auto operator()(Ts1&&... variable_args) const
+    template <typename ...Args1>
+    constexpr auto operator()(Args1&&... variable_args) const
     {
         // forward the new arguments into a tuple, concatenate this to the end of the 'm_back_args', and apply this tuple
         // to the 'm_function', so that the function is invoked with all the arguments
-        auto m_back_args = ext::make_tuple(std::forward<Ts1>(variable_args)...);
+        auto m_back_args = ext::make_tuple(std::forward<Args1>(variable_args)...);
         return ext::apply(m_function, ext::tuple_cat(m_front_args, m_back_args));
     }
 
 private:
     const F& m_function;
-    tuple<Ts...> m_front_args;
+    tuple<Args0...> m_front_args;
 };
 
 _EXT_END
