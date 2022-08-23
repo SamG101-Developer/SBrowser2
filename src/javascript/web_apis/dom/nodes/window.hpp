f1d8f24387e0e85d300799025d42daa11d66e664
Sam <samuelgardner101@gmail.com>
2022-08-23 02:56:45 +0100

    Added other spec methods

--- src/javascript/web_apis/dom/nodes/window.hpp
+++ src/javascript/web_apis/dom/nodes/window.hpp
@@ -4,6 +4,8 @@
 #include "dom/nodes/event_target.hpp"
 namespace dom::nodes {class window;}
 
+#include USE_INNER_TYPES(background_tasks)
+namespace background_tasks {class idle_deadline;}
 namespace dom::nodes {class document;}
 
 
@@ -11,10 +13,27 @@ class dom::nodes::window final
         : public event_target
 {
 public js_properties:
+    /* HTML */
     ext::property<std::unique_ptr<document>> document;
 
+    /* BACKGROUND_TASKS */
+    auto request_idle_callback(
+            background_tasks::detail::idle_request_callback_t&& callback,
+            background_tasks::detail::idle_request_options_t&& options)
+            -> ext::number<ulong>;
+
+    auto cancel_idle_task(
+            const ext::number<ulong>& handle)
+            -> void;
+
 private cpp_properties:
+    /* HTML */
     ext::boolean m_has_dispatched_scroll_event = false;
+
+    /* BACKGROUND_TASKS */
+    ext::vector<background_tasks::idle_deadline*> m_idle_request_callbacks;
+    ext::vector<background_tasks::idle_deadline*> m_runnable_idle_callbacks;
+    ext::number<size_t> m_idle_callback_identifier;
 };
 
 
