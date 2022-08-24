73c96f68247c718dd2678b83bb1ceddcedb063a9
Sam <samuelgardner101@gmail.com>
2022-08-25 00:08:47 +0100

    Commented method implementation

--- src/javascript/web_apis/battery/detail/algorithm_internals.cpp
+++ src/javascript/web_apis/battery/detail/algorithm_internals.cpp
@@ -19,13 +19,19 @@ auto battery::algorithm_internals::update_battery_status_and_notify(
         ext::string&& event_name)
         -> void
 {
+    // If the global object isn't a window, then return from the method, because the rest of the method relies on the
+    // global object being a Window
     JS_REALM_GET_CURRENT;
     auto* window = v8pp::from_v8<dom::nodes::window*>(current_agent, current_global_object);
     return_if (!window);
 
+    // If there is no Window.[[BatteryManager]], then return from the method, because the rest of the method relied on
+    // the battery manager existing
     decltype(auto) battery_manager = window->navigator()->s_battery_manager();
     return_if (!battery_manager);
 
+    // Queue a task on the idle task source to set the slot value, and fire the 'event_name' event at the BatteryManager
+    // object
     dom::detail::queue_task(
             html::detail::idle_task_source(),
             [battery_manager, &slot, event_name = std::move(event_name), new_value = std::forward<T>(new_value)] mutable
