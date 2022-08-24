2a0f29e2fa937c02672587bbc49dbbbea68512ce
Sam <samuelgardner101@gmail.com>
2022-08-24 21:34:35 +0100

    Implemented

--- src/javascript/web_apis/badging/mixins/navigator_badge.cpp
+++ src/javascript/web_apis/badging/mixins/navigator_badge.cpp
@@ -1,1 +1,13 @@
 #include "navigator_badge.hpp"
+
+
+auto badging::mixins::navigator_badge::to_v8(
+        v8::Isolate* isolate)
+        const && -> ext::any
+{
+    return v8pp::class_<navigator_badge>{isolate}
+        .inherit<dom_object>()
+        .function("setAppBadge", &navigator_badge::set_app_badge)
+        .function("clearAppBadge", &navigator_badge::clear_app_badge)
+        .auto_wrap_objects();
+}
