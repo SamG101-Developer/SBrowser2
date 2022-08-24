357df1d6fbbdedc7e48bae30b41b2309fa5de82a
Sam <samuelgardner101@gmail.com>
2022-08-24 15:04:26 +0100

    Added new type

--- src/javascript/web_apis/background_tasks/_typedefs.hpp
+++ src/javascript/web_apis/background_tasks/_typedefs.hpp
@@ -5,13 +5,15 @@
 #include "ext/functional.hpp"
 #include "ext/map.hpp"
 #include "ext/string.hpp"
+#include USE_INNER_TYPES(high_resolution_time)
 
 namespace background_tasks {class idle_deadline;}
 
 namespace background_tasks::detail
 {
     using idle_request_options_t = ext::map<ext::string, ext::any>;
-    using idle_request_callback_t = ext::function<void(idle_deadline*)>;
+    using idle_request_callback_t = ext::function<void(idle_deadline&&)>;
+    using deadline_time_algorithm_t = ext::function<high_resolution_time::detail::dom_high_res_time_stamp_t()>;
 }
 
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_BACKGROUND_TASKS__TYPEDEFS_HPP
