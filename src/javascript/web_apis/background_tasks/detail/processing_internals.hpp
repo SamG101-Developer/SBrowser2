422859c601acbb622d5007c78e438ba6b5418c6a
Sam <samuelgardner101@gmail.com>
2022-08-24 12:49:53 +0100

    Added missing parameter

--- src/javascript/web_apis/background_tasks/detail/processing_internals.hpp
+++ src/javascript/web_apis/background_tasks/detail/processing_internals.hpp
@@ -1,7 +1,8 @@
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_BACKGROUND_TASKS_DETAIL_PROCESSING_INTERNALS_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_BACKGROUND_TASKS_DETAIL_PROCESSING_INTERNALS_HPP
 
-#include "ext/concepts.hpp"
+#include "background_tasks/_typedefs.hpp"
+#include USE_INNER_TYPES(background_tasks)
 #include USE_INNER_TYPES(high_resolution_time)
 namespace dom::nodes {class window;}
 
@@ -16,11 +17,13 @@ namespace background_tasks::detail
 
     template <callable F>
     auto invoke_idle_callbacks_algorithm(
+            detail::idle_request_callback_t&& handle,
             dom::nodes::window* window,
             F&& get_deadline)
             -> high_resolution_time::detail::dom_high_res_time_stamp_t;
 
     auto invoke_idle_callback_timeout_algorithm(
+            detail::idle_request_callback_t&& handle,
             dom::nodes::window* window)
             -> void;
 }
