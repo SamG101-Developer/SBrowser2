d82d542bccb3e28f2844e27196557d6feb764ed0
Sam <samuelgardner101@gmail.com>
2022-08-26 17:00:53 +0100

    Fixed types

--- src/javascript/web_apis/svg/elements/svg_element.hpp
+++ src/javascript/web_apis/svg/elements/svg_element.hpp
@@ -5,20 +5,20 @@
 namespace svg::elements {class svg_element;}
 
 namespace svg::elements {class svg_svg_element;}
-namespace svg::attribute_types {class svg_animated_string;}
+namespace svg::types {class svg_animated_string;}
 
 
 class svg::elements::svg_element
         : public dom::nodes::element
-        , svg::mixins::svg_element_instance
-        , html::mixins::html_or_svg_element
+        , public svg::mixins::svg_element_instance
+        , public html::mixins::html_or_svg_element
 {
 public constructors:
     DOM_CTORS(svg_element);
     svg_element() = default;
 
 public js_properties:
-    ext::property<attribute_types::svg_animated_string> class_name;
+    ext::property<types::svg_animated_string> class_name;
     ext::property<svg_svg_element*> owner_svg_element;
     ext::property<svg_element*> viewport_element;
 
