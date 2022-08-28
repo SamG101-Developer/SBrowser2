ec9f991c099e737e76ffca3f68a9fb5f6907a99e
Sam <samuelgardner101@gmail.com>
2022-08-28 23:15:44 +0100

    Added missing method

--- src/javascript/web_apis/edit_context/events/text_update_event.hpp
+++ src/javascript/web_apis/edit_context/events/text_update_event.hpp
@@ -22,6 +22,9 @@ public js_properties:
     ext::property<ext::number<ulong>> composition_start;
     ext::property<ext::number<ulong>> composition_end;
     ext::property<ext::string> text;
+
+public cpp_properties:
+    auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
 };
 
 
