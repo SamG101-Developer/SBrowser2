04c02edc3535c44b91db24d91791beb5febd8b14
Sam <samuelgardner101@gmail.com>
2022-08-28 16:32:48 +0100

    Simplified getters

--- src/javascript/web_apis/battery/battery_manager.cpp
+++ src/javascript/web_apis/battery/battery_manager.cpp
@@ -9,38 +9,6 @@ battery::battery_manager::battery_manager()
 {}
 
 
-auto battery::battery_manager::get_charging()
-        const -> decltype(this->charging)::value_t
-{
-    // The charging getter steps are to return this.[[Charging]].
-    return s_charging();
-}
-
-
-auto battery::battery_manager::get_charging_time()
-        const -> decltype(this->charging_time)::value_t
-{
-    // The chargingTime getter steps are to return this.[[ChargingTime]].
-    return s_charging_time();
-}
-
-
-auto battery::battery_manager::get_discharging_time()
-        const -> decltype(this->discharging_time)::value_t
-{
-    // The dischargingTime getter steps are to return this.[[DischargingTime]].
-    return s_discharging_time();
-}
-
-
-auto battery::battery_manager::get_level()
-        const -> decltype(this->level)::value_t
-{
-    // The level getter steps are to return this.[[Level]].
-    return s_level();
-}
-
-
 auto battery::battery_manager::to_v8(
         v8::Isolate* isolate)
         const && -> ext::any
