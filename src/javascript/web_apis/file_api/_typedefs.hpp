e05ae12dbf87efe67d7cf6a8cf5d998120d0e95b
Sam <samuelgardner101@gmail.com>
2022-08-29 12:19:48 +0100

    Added missing types

--- src/javascript/web_apis/file_api/_typedefs.hpp
+++ src/javascript/web_apis/file_api/_typedefs.hpp
@@ -13,9 +13,11 @@ namespace file_api::detail
     struct blob_url_entry_t;
 
     enum class endings_t {TRANSPARENT, NATIVE};
+    enum class state_t {EMPTY, LOADING, DONE};
 
     using blob_url_store_t = ext::map<ext::string, blob_url_entry_t>;
     using blob_part_t = ext::variant<blob*, ext::string, v8::ArrayBuffer>;
+    using file_property_bag_t = ext::map<ext::string, ext::any>;
 }
 
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_FILE_API__TYPEDEFS_HPP
