3f25fefea963af490a563db8df186fbcd6788bb4
Sam <samuelgardner101@gmail.com>
2022-08-29 02:12:34 +0100

    Added missing properties

--- src/javascript/web_apis/file_api/blob.hpp
+++ src/javascript/web_apis/file_api/blob.hpp
@@ -2,26 +2,24 @@
 #define SBROWSER2_BLOB_HPP
 
 #include "dom_object.hpp"
+#include "html/mixins/serializable.hpp"
 namespace file_api {class blob;}
 
 #include "ext/map.hpp"
 #include "ext/optional.hpp"
 #include "ext/variant.hpp"
 #include "ext/vector.hpp"
-#include <future>
-#include <v8-array-buffer.h>
-#include <v8pp/convert.hpp>
+#include <v8-forward.h>
+#include USE_INNER_TYPES(file_api)
 namespace streams::readable {class readable_stream;}
 
 
 class file_api::blob
         : public virtual dom_object
+        , public html::mixins::serializable
 {
-public aliases:
-    using blob_part_t = ext::variant<blob, ext::string_view, v8::ArrayBufferView>;
-
 public constructors:
-    blob(ext::vector_view<blob_part_t> blob_parts, ext::map<ext::string, ext::any>&& options = {});
+    blob(ext::vector<detail::blob_part_t>&& blob_parts = {}, ext::map<ext::string, ext::any>&& options = {});
 
 public js_methods:
     auto slice(ext::optional<ext::number<longlong>> start, ext::optional<ext::number<longlong>> end, ext::optional<ext::string> content_type) -> blob;
@@ -35,6 +33,14 @@ public js_properties:
 
 public cpp_methods:
     auto to_v8(v8::Isolate* isolate) const && -> ext::any override;
+
+protected cpp_methods:
+    auto m_serialize  (ext::map<ext::string, ext::any>& serialized, ext::boolean&& for_storage) -> ext::string;
+    auto m_deserialize(ext::map<ext::string, ext::any>& serialized, ext::boolean&& for_storage) -> blob*;
+
+private js_slots:
+    ext::slot<ext::string> s_snapshot_state;
+    ext::slot<ext::string> s_byte_sequence;
 
 private cpp_properties:
     ext::string m_snapshot_state;
