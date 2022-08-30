b5fcefffd577dbc6578746e13f3073efad9d8bf7
Sam <samuelgardner101@gmail.com>
2022-08-30 17:28:16 +0100

    Fixed parameter types

--- src/javascript/web_apis/html/detail/media_internals.hpp
+++ src/javascript/web_apis/html/detail/media_internals.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include "html/basic_media/text_track.hpp"
+#include "html/basic_media/text_track_cue.hpp"
+#include "html/elements/html_track_element.hpp"
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_DETAIL_MEDIA_INTERNALS_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_DETAIL_MEDIA_INTERNALS_HPP
 
@@ -9,6 +12,8 @@
 #include USE_INNER_TYPES(fetch)
 #include USE_INNER_TYPES(html)
 #include USE_INNER_TYPES(url)
+
+namespace html::basic_media {class text_track;}
 namespace html::elements {class html_media_element;}
 namespace html::elements {class html_video_element;}
 
@@ -16,15 +21,15 @@ namespace html::elements {class html_video_element;}
 namespace html::detail
 {
     auto poster_frame(
-            html::elements::html_video_element* element)
+            elements::html_video_element* element)
             -> auto; // TODO : return type
 
     auto media_element_load_algorithm(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto resource_selection_algorithm(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto dedicated_media_source_failure_steps(
@@ -33,105 +38,156 @@ namespace html::detail
 
     auto verify_media_response(
             const fetch::detail::response_t& response,
-            html::elements::html_media_element* resource,
+            elements::html_media_element* resource,
             const ext::any& byte_range)
             -> ext::boolean;
 
     auto resource_fetch_algorithm(
-            html::elements::html_media_element* element,
+            elements::html_media_element* element,
             const url::detail::url_t& url)
             -> void;
 
     auto media_data_processing_steps_list(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto forget_media_element_media_resource_specific_tracks(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto earliest_possible_position(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::number<double>;
 
     auto when_network_state_changes(
-            html::elements::html_media_element* element,
+            elements::html_media_element* element,
             const ext::number<ushort>& previous_ready_state,
             const ext::number<ushort>& new_ready_state)
             -> void;
 
     auto when_current_playback_position_at_end(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto is_blocked_media_element(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::boolean;
 
     auto is_potentially_playing(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::boolean;
 
     auto is_eligible_for_autoplay(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::boolean;
 
     auto is_allowed_to_play(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::boolean;
 
     auto has_ended_playback(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::boolean;
 
     auto has_stopped_due_to_errors(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::boolean;
 
     auto has_paused_for_use_interaction(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::boolean;
 
     auto has_paused_for_in_band_content(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::boolean;
 
     auto take_pending_play_promises(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> ext::vector<ext::promise<>>;
 
     auto resolve_pending_play_promises(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto reject_pending_play_promises(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto notify_about_playing(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto internal_play_steps(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto internal_pause_steps(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto direction_of_playback(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> media_playback_direction_t;
 
     auto time_marches_on(
-            html::elements::html_media_element* element)
+            elements::html_media_element* element)
             -> void;
 
     auto seek(
-            html::elements::html_media_element* element,
+            elements::html_media_element* element,
             const ext::number<double>& new_playback_position,
             ext::boolean&& approximate_for_speed_flag)
+            -> void;
+
+    auto populate_pending_text_tracks(
+            elements::html_media_element* element)
+            -> void;
+
+    auto media_element_text_tracks_ready(
+            elements::html_media_element* element)
+            -> void;
+
+    auto when_text_track_mode_changes(
+            elements::html_media_element* element,
+            basic_media::text_track* text_track)
+            -> void;
+
+    auto is_unbounded_text_track_cue(
+            basic_media::text_track_cue* text_track_cue)
+            -> ext::boolean;
+
+    auto is_media_resource_specific_text_track(
+            basic_media::text_track* text_track)
+            -> ext::boolean;
+
+    auto expose_media_resource_specific_text_track(
+            ext::any&& relevant_data)
+            -> void;
+
+    auto when_text_track_added_to_media_elements_text_tracks(
+            elements::html_media_element* element,
+            basic_media::text_track* text_track)
+            -> void;
+
+    auto honor_user_preferences_for_automatic_text_track_selection(
+            elements::html_media_element* element)
+            -> void;
+
+    auto perform_automatic_text_track_selection(
+            ext::vector<text_track_kind_t> text_track_kinds)
+            -> void;
+
+    auto start_track_processing_model(
+            basic_media::text_track* text_track,
+            html::elements::html_track_element* track_element)
+            -> void;
+
+    auto earliest_possible_position_when_script_started(
+            /* TODO */)
+            -> void;
+
+    auto active_flag_was_set_when_script_started(
+            /* TODO */)
             -> void;
 };
 
