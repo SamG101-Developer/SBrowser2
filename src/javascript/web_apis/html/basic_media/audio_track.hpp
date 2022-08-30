83e8b0fd4060d14d2cbfe0d043e617632585bb78
Sam <samuelgardner101@gmail.com>
2022-08-30 17:27:59 +0100

    Finished defining class

--- src/javascript/web_apis/html/basic_media/audio_track.hpp
+++ src/javascript/web_apis/html/basic_media/audio_track.hpp
@@ -2,16 +2,22 @@
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_MEDIA_AUDIO_TRACK_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_MEDIA_AUDIO_TRACK_HPP
 
-#include "dom_object.hpp"
+#include "html/basic_media/abstract_track.hpp"
 namespace html::basic_media {class audio_track;}
 
 namespace media::source {class source_buffer;}
 
 
 class html::basic_media::audio_track
-        : public dom_object
+        : public abstract_track
 {
+public constructors:
+    DOM_CTORS(audio_track);
+    audio_track() = default;
+
 public js_properties:
+    ext::property<ext::boolean> enabled;
+
     ext::property<std::unique_ptr<media::source::source_buffer>> source_buffer;
 
 private cpp_accessors:
