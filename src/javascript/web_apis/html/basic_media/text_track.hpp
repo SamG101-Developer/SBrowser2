ee10fffb7e782cedeb8a7f3ee74d79a4ec005f3b
Sam <samuelgardner101@gmail.com>
2022-08-30 17:28:26 +0100

    Implemented methods

--- src/javascript/web_apis/html/basic_media/text_track.hpp
+++ src/javascript/web_apis/html/basic_media/text_track.hpp
@@ -2,17 +2,37 @@
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_BASIC_MEDIA_TEXT_TRACK_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_BASIC_MEDIA_TEXT_TRACK_HPP
 
-#include "dom_object.hpp"
+#include "html/basic_media/abstract_track.hpp"
 namespace html::basic_media {class text_track;}
 
+#include USE_INNER_TYPES(html)
 namespace media::source {class source_buffer;}
+namespace html::basic_media {class text_track_cue;}
 
 
 class html::basic_media::text_track
-        : public dom_object
+        : public abstract_track
 {
+public constructors:
+    DOM_CTORS(text_track);
+    text_track() = default;
+
+public js_methods:
+    auto add_cue(text_track_cue* cue) -> void;
+    auto remove_cue(text_track_cue* cue) -> void;
+
 public js_properties:
+    ext::property<ext::string> in_band_dispatch_type;
+    ext::property<detail::track_readiness_state_t> readiness_state;
+    ext::property<detail::track_mode_t> mode;
+
+    ext::property<std::unique_ptr<ext::vector<text_track_cue*>>> cues;
+    ext::property<std::unique_ptr<ext::vector<text_track_cue*>>> active_cues;
+
     ext::property<std::unique_ptr<media::source::source_buffer>> source_buffer;
+
+private cpp_properties:
+    ext::vector<text_track_cue*> m_text_track_cues;
 
 private cpp_accessors:
     DEFINE_GETTER(source_buffer);
