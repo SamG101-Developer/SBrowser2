5c44dcee4e8d9cd63733d8270ea6f097249c2cb3
Sam <samuelgardner101@gmail.com>
2022-08-30 15:46:24 +0100

    Implemented

--- src/javascript/web_apis/html/elements/html_video_element.hpp
+++ src/javascript/web_apis/html/elements/html_video_element.hpp
@@ -7,8 +7,23 @@ namespace html::elements {class html_video_element;}
 
 
 class html::elements::html_video_element
+        : public html_media_element
 {
+public constructors:
+    DOM_CTORS(html_video_element);
+    html_video_element() = default;
 
+public js_properties:
+    ext::property<ext::number<ulong>> width;
+    ext::property<ext::number<ulong>> height;
+    ext::property<ext::number<ulong>> video_width;
+    ext::property<ext::number<ulong>> video_height;
+    ext::property<ext::string> poster;
+    ext::property<ext::boolean> plays_inline;
+
+private cpp_accessors:
+    DEFINE_GETTER(video_width);
+    DEFINE_GETTER(video_height);
 };
 
 
