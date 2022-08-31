cebc2ff55e21f5387b41db3b1874c5e09d63732d
Sam <samuelgardner101@gmail.com>
2022-08-31 12:22:11 +0100

    Added more types

--- src/javascript/web_apis/html/_typedefs.hpp
+++ src/javascript/web_apis/html/_typedefs.hpp
@@ -12,6 +12,7 @@
 #include USE_INNER_TYPES(fetch)
 
 namespace file_api {class blob;}
+namespace file_api {class file;}
 namespace html::elements {class html_element;}
 namespace html::elements {class html_option_element;}
 namespace html::elements {class html_opt_group_element;}
@@ -21,41 +22,17 @@ namespace media::source {class media_source;}
 
 namespace html::detail
 {
-    enum class history_handling_behaviour_t {
-        DEFAULT, /* TODO */};
-
-    enum class directionality_t {
-        LTR, RTL, AUTO};
-
-    enum class bidirectional_char_t: char32_t {
-        L = 0x200e, R = 0x200f, AL = 0x061c, EN, ES, ET, AN, CS, NSM, BN, B, S, WS, ON, LRE, LRO, RLE, PDF, LRI, RLI,
-        FSI, PDI};
-
-    enum class state_t {
-        UNAVAILABLE, PARTIALLY_AVAILABLE, COMPLETELY_AVAILABLE, BROKEN};
-
-    enum class phase_t {
-        PRE_MEDIA, MEDIA};
-
-    enum class embedder_policy_value_t {
-        UNSAFE_NONE, REQUIRE_CORP, CREDENTIALLESS};
-
-    enum class cross_origin_opener_policy_value_t{
-        UNSAFE_NONE, SAME_ORIGIN_ALLOW_POPUPS, SAME_ORIGIN, SAME_ORIGIN_PLUS_COEP};
-
-    enum class lazy_loading_t {
-        EAGER, LAZY};
-
-    enum class rel_t {
-        ALTERNATE, DNS_PREFETCH, ICON, MANIFEST, MODULE_PRELOAD, NEXT, PINGBACK, PRECONNECT, PREFETCH, PRELOAD,
-        PRERENDER, SEARCH, STYLESHEET, CANONICAL};
-
-    enum class http_equiv_t {
-        CONTENT_LANGUAGE, CONTENT_TYPE, DEFAULT_STYLE, REFRESH, SET_COOKIE, X_UA_COMPATIBLE, CONTENT_SECURITY_POLICY};
-
-    enum class can_play_type_result_t {
-        MAYBE, PROBABLY};
-
+    enum class history_handling_behaviour_t {DEFAULT, /* TODO */};
+    enum class directionality_t {LTR, RTL, AUTO};
+    enum class bidirectional_char_t: char32_t {L = 0x200e, R = 0x200f, AL = 0x061c, EN, ES, ET, AN, CS, NSM, BN, B, S, WS, ON, LRE, LRO, RLE, PDF, LRI, RLI, FSI, PDI};
+    enum class state_t {UNAVAILABLE, PARTIALLY_AVAILABLE, COMPLETELY_AVAILABLE, BROKEN};
+    enum class phase_t {PRE_MEDIA, MEDIA};
+    enum class embedder_policy_value_t {UNSAFE_NONE, REQUIRE_CORP, CREDENTIALLESS};
+    enum class cross_origin_opener_policy_value_t{UNSAFE_NONE, SAME_ORIGIN_ALLOW_POPUPS, SAME_ORIGIN, SAME_ORIGIN_PLUS_COEP};
+    enum class lazy_loading_t {EAGER, LAZY};
+    enum class rel_t {ALTERNATE, DNS_PREFETCH, ICON, MANIFEST, MODULE_PRELOAD, NEXT, PINGBACK, PRECONNECT, PREFETCH, PRELOAD, PRERENDER, SEARCH, STYLESHEET, CANONICAL};
+    enum class http_equiv_t {CONTENT_LANGUAGE, CONTENT_TYPE, DEFAULT_STYLE, REFRESH, SET_COOKIE, X_UA_COMPATIBLE, CONTENT_SECURITY_POLICY};
+    enum class can_play_type_result_t {MAYBE, PROBABLY};
     enum class text_track_mode_t {DISABLED, HIDDEN, SHOWING};
     enum class text_track_kind_t {SUBTITLES, CAPTIONS, DESCRIPTIONS, CHAPTERS, METADATA};
     enum class media_playback_direction_t {FORWARDS, BACKWARDS};
@@ -79,6 +56,7 @@ namespace html::detail
     struct policy_container_t;
     struct embedder_policy_t;
     struct cross_origin_opener_policy_t;
+    struct validity_state_t;
 
     using name_value_group_t = ext::pair<ext::vector<elements::html_element*>, ext::vector<elements::html_element*>>;
     using name_value_groups_t = ext::vector<name_value_group_t>;
@@ -89,6 +67,8 @@ namespace html::detail
     using task_queue_t = ext::queue<task_t>;
     using media_provider_t = ext::variant<mediacapture::main::media_stream*, media::source::media_source*, file_api::blob*>;
     using option_like_element_t = ext::variant<elements::html_option_element*, elements::html_opt_group_element*>;
+    using entry_t = ext::tuple<ext::string, ext::variant<ext::string, file_api::file*>>;
+    using entry_list_t = ext::vector<entry_t>;
 
 }
 
