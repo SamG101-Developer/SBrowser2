7005baf2f25fd5bbfb77bc6c6a2015224e5f5530
Sam <samuelgardner101@gmail.com>
2022-09-01 23:42:21 +0100

    Created

---
+++ src/javascript/web_apis/html/other/location.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_OTHER_LOCATION_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_OTHER_LOCATION_HPP
+
+#include "dom_object.hpp"
+namespace html::other {class location;}
+
+#include USE_INNER_TYPES(html)
+
+
+class html::other::location
+        : public virtual dom_object
+{
+private js_slots:
+    ext::slot<detail::cross_origin_property_descriptor_map_t> s_cross_origin_property_descriptor_map;
+};
+
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_OTHER_LOCATION_HPP
