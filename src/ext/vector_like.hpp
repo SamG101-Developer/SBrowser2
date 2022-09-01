b9bc0d8f22f0952c88e025b27c157ca7eac4e5fa
Sam <samuelgardner101@gmail.com>
2022-09-01 23:43:27 +0100

    Fixed lots of issues

--- src/ext/vector_like.hpp
+++ src/ext/vector_like.hpp
@@ -14,9 +14,19 @@ _EXT_BEGIN
 template <typename T>
 class vector_like
 {
+public constructors:
+    vector_like()
+    {bind_get(length);}
+
 public cpp_operators:
     virtual auto operator[](const number<size_t>& index) -> T& = 0;
     virtual auto operator[](ext::string_view index) -> T& = 0;
+
+public cpp_properties:
+    property<number<size_t>> length;
+
+protected cpp_accessors:
+    virtual DEFINE_GETTER(length) = 0;
 };
 
 
@@ -24,12 +34,8 @@ template <typename T>
 class vector_like_linked : public vector_like<T>
 {
 public constructors:
-    explicit vector_like_linked(vector<T>* container)
-            : linked_vector{container}
+    explicit vector_like_linked(vector<T>* container) : linked_vector{container}
     {bind_get(length);}
-
-public cpp_properties:
-    property<number<size_t>> length;
 
 public cpp_operators:
     auto operator[](const number<size_t>& index) -> T& override {return linked_vector->at(index);}
@@ -39,7 +45,7 @@ private cpp_properties:
     std::unique_ptr<vector<T>> linked_vector;
 
 private cpp_accessors:
-    [[nodiscard]] auto get_length() const -> number<size_t> {return linked_vector->size();}
+    DEFINE_GETTER(length) override {return linked_vector->size();}
 };
 
 _EXT_END
