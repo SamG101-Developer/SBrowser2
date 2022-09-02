2af6394b367cdbf6d3b341b4ff143ad39d24c823
Sam <samuelgardner101@gmail.com>
2022-09-02 16:53:22 +0100

    Defined enum-classes and typedefs for the mediacapture-handle spec

---
+++ src/javascript/web_apis/mediacapture_handle/_typedefs.hpp
@@ -0,0 +1,16 @@
+#ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_MEDIACAPTURE_HANDLE__TYPEDEFS_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_MEDIACAPTURE_HANDLE__TYPEDEFS_HPP
+
+#include "ext/any.hpp"
+#include "ext/map.hpp"
+#include "ext/string.hpp"
+
+namespace mediacapture::detail
+{
+    enum class capture_action_t {NEXT, PREVIOUS, FIRST, LAST};
+
+    using capture_handle_config_t = ext::map<ext::string, ext::any>;
+    using capture_handle_t = ext::map<ext::string, ext::any>;
+}
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_MEDIACAPTURE_HANDLE__TYPEDEFS_HPP
