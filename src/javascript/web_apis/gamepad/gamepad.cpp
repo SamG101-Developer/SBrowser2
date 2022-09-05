424a863f21ea19c90dbee39194482eadc5e8e0ca
Sam <samuelgardner101@gmail.com>
2022-09-05 17:31:45 +0100

    Implemented

--- src/javascript/web_apis/gamepad/gamepad.cpp
+++ src/javascript/web_apis/gamepad/gamepad.cpp
@@ -15,4 +15,38 @@ gamepad::gamepad::gamepad()
         , s_timestamp{high_resolution_time::performance{}.now()}
         , s_axes{detail::initialize_axes(this)}
         , s_buttons{detail::initialize_buttons(this)}
-{}
+{
+    bind_get(connected);
+    bind_get(timestamp);
+    bind_get(axes);
+    bind_get(buttons);
+}
+
+
+auto gamepad::gamepad::to_v8(
+        v8::Isolate* isolate)
+        const && -> ext::any
+{
+    return v8pp::class_<gamepad>{isolate}
+        .ctor<>()
+        .inherit<dom_object>()
+        .var("id", &gamepad::id, true)
+        .var("index", &gamepad::index, true)
+        .var("connected", &gamepad::connected, true)
+        .var("timestamp", &gamepad::timestamp, true)
+        .var("mapping", &gamepad::mapping, true)
+        .var("axes", &gamepad::axes, true)
+        .var("buttons", &gamepad::buttons, true)
+        .slot("Connected", &gamepad::s_connected)
+        .slot("Timestamp", &gamepad::s_timestamp)
+        .slot("Axes", &gamepad::s_axes)
+        .slot("Buttons", &gamepad::s_buttons)
+        .slot("Exposed", &gamepad::s_exposed)
+        .slot("AxisMapping", &gamepad::s_axis_mapping)
+        .slot("AxisMinimums", &gamepad::s_axis_minimums)
+        .slot("AxisMaximums", &gamepad::s_axis_maximums)
+        .slot("ButtonMapping", &gamepad::s_button_mapping)
+        .slot("ButtonMinimums", &gamepad::s_button_minimums)
+        .slot("ButtonMaximums", &gamepad::s_button_maximums)
+        .auto_wrap_objects();
+}
--- src/javascript/web_apis/gamepad/gamepad_event.cpp
+++ src/javascript/web_apis/gamepad/gamepad_event.cpp
@@ -1,1 +1,23 @@
 #include "gamepad_event.hpp"
+
+#include "gamepad/gamepad.hpp"
+
+
+gamepad::gamepad_event::gamepad_event(
+        ext::string&& event_type,
+        ext::map<ext::string, ext::any>&& event_init)
+        : dom::events::event(std::move(event_type), std::move(event_init))
+        , SET_PROPERTY_FROM_OPTIONS(event_init, gamepad, nullptr)
+{}
+
+
+auto gamepad::gamepad_event::to_v8(
+        v8::Isolate* isolate)
+        const && -> ext::any
+{
+    return v8pp::class_<gamepad_event>{isolate}
+        .ctor<ext::string&&, ext::map<ext::string, ext::any>&&>()
+        .inherit<dom::events::event>()
+        .var("gamepad", &gamepad_event::gamepad, true)
+        .auto_wrap_objects();
+}
