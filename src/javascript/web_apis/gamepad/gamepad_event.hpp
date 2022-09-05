39a8a9602281c168eb682070e30970560e1eb20c
Sam <samuelgardner101@gmail.com>
2022-09-05 17:32:22 +0100

    Fixed file names and classes

--- src/javascript/web_apis/gamepad/gamepad_event.hpp
+++ src/javascript/web_apis/gamepad/gamepad_event.hpp
@@ -5,29 +5,21 @@
 #include "dom/events/event.hpp"
 namespace gamepad {class gamepad_event;}
 
+namespace gamepad {class gamepad;}
 
-class gamepad_event
+
+class gamepad::gamepad_event
         : public dom::events::event
 {
 public constructors:
-    DOM_CTORS(gamepad_event);
     gamepad_event() = default;
     gamepad_event(ext::string&& event_type, ext::map<ext::string, ext::any>&& event_init = {});
 
 public js_properties:
-    ext::property<ext::boolean> pressed;
-    ext::property<ext::boolean> touched;
-    ext::property<ext::number<double>> value;
+    ext::property<std::unique_ptr<gamepad>> gamepad;
 
-private js_slots:
-    ext::slot<ext::boolean> s_pressed;
-    ext::slot<ext::boolean> s_touched;
-    ext::slot<ext::number<double>> s_value;
-
-private cpp_accessors:
-    DEFINE_GETTER(pressed);
-    DEFINE_GETTER(touched);
-    DEFINE_GETTER(value);
+public cpp_methods:
+    auto to_v8(v8::Isolate* isolate) const && -> ext::any override;
 };
 
 
