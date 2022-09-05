4e9950c7b3abfe6834cbd476a5f5d69eebfaed6e
Sam <samuelgardner101@gmail.com>
2022-09-05 17:31:20 +0100

    Fixed enum value

--- src/javascript/web_apis/gamepad/_typedefs.hpp
+++ src/javascript/web_apis/gamepad/_typedefs.hpp
@@ -3,7 +3,7 @@
 
 namespace gamepad::detail
 {
-    enum class gamepad_mapping_type_t {EMPTY_, STANDARD, XR_STANDARD};
+    enum class gamepad_mapping_type_t {_, STANDARD, XR_STANDARD};
 }
 
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_GAMEPAD__TYPEDEFS_HPP
