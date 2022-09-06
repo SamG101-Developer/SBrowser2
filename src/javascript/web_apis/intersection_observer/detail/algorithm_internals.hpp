bb6926aea65b51b88c2c5dece06a0ea9c98c2402
Sam <samuelgardner101@gmail.com>
2022-09-06 17:54:17 +0100

    Defined more methods

--- src/javascript/web_apis/intersection_observer/detail/algorithm_internals.hpp
+++ src/javascript/web_apis/intersection_observer/detail/algorithm_internals.hpp
@@ -1,16 +1,20 @@
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_INTERSECTION_OBSERVER_DETAIL_ALGORITHM_INTERNALS_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_INTERSECTION_OBSERVER_DETAIL_ALGORITHM_INTERNALS_HPP
 
+#include "ext/array.hpp"
 #include "ext/boolean.hpp"
 #include "ext/map.hpp"
+#include "ext/optional.hpp"
 #include "ext/string.hpp"
 #include USE_INNER_TYPES(intersection_observer)
+
 namespace dom::nodes {class document;}
 namespace dom::nodes {class element;}
-namespace intersection_observers {class intersection_observer;}
+namespace dom::nodes {class node;}
+namespace intersection_observer {class intersection_observer;}
 
 
-namespace intersection_observers::detail::algorithm_internals
+namespace intersection_observer::detail
 {
     auto queue_intersection_observer_task(
             dom::nodes::document* document)
@@ -28,7 +32,7 @@ namespace intersection_observers::detail::algorithm_internals
     auto compute_intersection_of_target_element_and_root(
             dom::nodes::element* target_element,
             dom::nodes::element* intersection_root)
-            -> geometry::dom_rect;
+            -> css::geometry::dom_rect;
 
     auto run_update_intersection_observations_steps(
             dom::nodes::document* document,
@@ -38,7 +42,35 @@ namespace intersection_observers::detail::algorithm_internals
     auto pending_initial_intersection_observer_targets(
             dom::nodes::document* document)
             -> ext::boolean;
+
+    auto intersection_root(
+            intersection_observer* observer)
+            -> dom::nodes::node*;
+
+    auto implicit_root(
+            intersection_observer* observer)
+            -> dom::nodes::node*;
+
+    auto is_explicit_root_observer(
+            intersection_observer* observer)
+            -> ext::boolean;
+
+    auto is_implicit_root_observer(
+            intersection_observer* observer)
+            -> ext::boolean;
+
+    auto parse_root_margin(
+            ext::string_view margin_string)
+            -> ext::optional<ext::array<int, 4>>;
 }
+
+
+struct intersection_observer::detail::intersection_observer_registration_t
+{
+    intersection_observer* observer;
+    ext::number<int> previous_threshold_index;
+    ext::boolean previous_is_intersecting;
+};
 
 
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_INTERSECTION_OBSERVER_DETAIL_ALGORITHM_INTERNALS_HPP
