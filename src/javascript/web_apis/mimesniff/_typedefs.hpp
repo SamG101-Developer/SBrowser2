2ab42c3ffb712925a10a85289d710c4f2e5dbca8
Sam <samuelgardner101@gmail.com>
2022-09-06 02:50:10 +0100

    Moved struct definitions into separate `detail` files.

--- src/javascript/web_apis/mimesniff/_typedefs.hpp
+++ src/javascript/web_apis/mimesniff/_typedefs.hpp
@@ -15,23 +15,4 @@ namespace mimesniff::detail
 }
 
 
-struct mimesniff::detail::mime_type_t
-{
-    ext::string type;
-    ext::string sub_type;
-    ext::map<ext::string, ext::string> parameters;
-
-    auto operator==(const mime_type_t&) const -> bool = default;
-    auto operator==(ext::string_view) const -> bool;
-};
-
-
-struct mimesniff::detail::resource_t
-{
-    mime_type_t supplied_mime_type;
-    mime_type_t computed_mime_type;
-    ext::boolean check_for_apache_bug_flag;
-    ext::boolean no_sniff_flag;
-};
-
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_MIMESNIFF__TYPEDEFS_HPP
