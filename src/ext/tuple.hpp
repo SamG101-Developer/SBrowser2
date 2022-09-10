71be02835b1f6ae9f5d017e28a4716430ef68395
Sam <samuelgardner101@gmail.com>
2022-09-10 23:44:06 +0100

    Implemented the `ext::make_from_tuple<...>(...)` method (missing from `tuplet::...`)

--- src/ext/tuple.hpp
+++ src/ext/tuple.hpp
@@ -6,16 +6,43 @@
 #include "ext/view.hpp"
 
 #include <functional>
+#include <initializer_list>
 #include <iterator>
 #include <tuplet/tuple.hpp>
 
 #include <range/v3/view/any_view.hpp>
+#include <type_traits>
 
 
 _EXT_BEGIN
 
 
 using namespace tuplet;
+
+
+// Missing from tuplet
+
+namespace detail {
+template <class T, class Tuple, size_t... Indices>
+constexpr auto make_from_tuple(
+        Tuple&& tuple,
+        std::index_sequence<Indices...>)
+        -> T
+{
+    return T{ext::get<Indices>(std::forward<Tuple>(tuple))...};
+}
+}
+
+template <class T, class Tuple>
+_EXT_NODISCARD constexpr auto make_from_tuple(
+        Tuple&& tuple)
+        -> T
+{
+    return detail::make_from_tuple<T>(
+            std::forward<Tuple>(tuple),
+            std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<Tuple>>>{});
+}
+
 
 //template <typename ...Args>
 //auto make_tuple(ranges::any_view<Args...>&& range) -> ext::tuple<Args...>
