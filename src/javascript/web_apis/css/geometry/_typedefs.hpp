9bc5319d86f8167a0c773fc4807328db2e4cd9ef
Sam <samuelgardner101@gmail.com>
2022-09-11 23:02:56 +0100

    Fixed map types

--- src/javascript/web_apis/css/geometry/_typedefs.hpp
+++ src/javascript/web_apis/css/geometry/_typedefs.hpp
@@ -12,6 +12,8 @@ namespace css::detail
     using dom_matrix_init_t = ext::map<ext::string, ext::any>;
     using dom_rect_init_t = ext::map<ext::string, ext::any>;
     using dom_quad_init_t = ext::map<ext::string, ext::any>;
+    using dom_matrix_init_t = ext::map<ext::string, ext::any>;
+    using dom_matrix2d_init_t = ext::map<ext::string, ext::any>;
 }
 
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_GEOMETRY__TYPEDEFS_HPP
