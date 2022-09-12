c9a5acf70d9c5af287a1e9f97669855603bb2272
Sam <samuelgardner101@gmail.com>
2022-09-12 12:26:23 +0100

    Implemented

--- src/javascript/web_apis/html/canvasing/mixins/canvas_rect.cpp
+++ src/javascript/web_apis/html/canvasing/mixins/canvas_rect.cpp
@@ -1,1 +1,72 @@
 #include "canvas_rect.hpp"
+
+#include "ext/variant.hpp"
+#include "html/detail/canvas_internals.hpp"
+
+#include USE_INNER_TYPES(html)
+
+#include "html/canvasing/image_bitmap.hpp"
+#include "html/canvasing/path_2d.hpp"
+#include "html/canvasing/mixins/canvas_path_drawing_styles.hpp"
+#include "webgl2/contexts/webgl_rendering_context.hpp"
+#include "webgl2/contexts/webgl2_rendering_context.hpp"
+
+#include <qbrush.h>
+#include <qpainter.h>
+
+
+auto html::canvasing::mixins::canvas_rect::clear_rect(
+        ext::number<double> x,
+        ext::number<double> y,
+        ext::number<double> w,
+        ext::number<double> h)
+        -> void
+{
+    decltype(auto) bitmap = ext::variant_transform<detail::rendering_context_t, image_bitmap*>(
+            [](auto&& context) {return context->canvas()->m_image_bitmap;});
+
+    auto painter = QPainter{&bitmap->m_bitmap_data};
+    painter.eraseRect(*x, *y, *w, *h);
+    painter.end();
+}
+
+
+auto html::canvasing::mixins::canvas_rect::fill_rect(
+        ext::number<double> x,
+        ext::number<double> y,
+        ext::number<double> w,
+        ext::number<double> h)
+        -> void
+{
+    decltype(auto) fill_style = ext::variant_transform<detail::rendering_context_t, ext::string>(
+            [](auto&& context) {return context->m_fill_style;});
+
+    decltype(auto) bitmap = ext::variant_transform<detail::rendering_context_t, image_bitmap*>(
+            [](auto&& context) {return context->canvas()->m_image_bitmap;});
+
+
+    auto painter = QPainter{&bitmap->m_bitmap_data};
+    painter.fillRect(*x, *y, *w, *h, QBrush{QColor{fill_style.c_str()}});
+    painter.end();
+}
+
+
+auto html::canvasing::mixins::canvas_rect::stroke_rect(
+        ext::number<double> x,
+        ext::number<double> y,
+        ext::number<double> w,
+        ext::number<double> h)
+        -> void
+{
+    decltype(auto) stroke_style = ext::variant_transform<detail::rendering_context_t, ext::string>(
+            [](auto&& context) {return context->m_stroke_style;});
+
+    decltype(auto) bitmap = ext::variant_transform<detail::rendering_context_t, image_bitmap*>(
+            [](auto&& context) {return context->canvas()->m_image_bitmap;});
+
+
+    auto painter = QPainter{&bitmap->m_bitmap_data};
+    auto path = detail::trace_path(ext::cross_cast<html::canvasing::mixins::canvas_path_drawing_styles*>(this));
+    painter.strokePath(path.m_path, stroke_style);
+    painter.end();
+}
