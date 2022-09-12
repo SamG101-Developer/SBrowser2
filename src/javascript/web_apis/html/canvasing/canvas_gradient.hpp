d81c425dbb2fc61fd0fd2d5164271e8e6863d017
Sam <samuelgardner101@gmail.com>
2022-09-12 03:28:38 +0100

    Added methods and properties

--- src/javascript/web_apis/html/canvasing/canvas_gradient.hpp
+++ src/javascript/web_apis/html/canvasing/canvas_gradient.hpp
@@ -5,16 +5,27 @@
 #include "dom_object.hpp"
 namespace html::canvasing {class canvas_gradient;}
 
+#include "ext/variant.hpp"
+#include <QtGui/QLinearGradient>
+#include <QtGui/QRadialGradient>
+#include <QtGui/QConicalGradient>
+
 
 class html::canvasing::canvas_gradient
         : public virtual dom_object
 {
 public constructors:
-    DOM_CTORS(canvas_gradient);
-    canvas_gradient() = default;
+    canvas_gradient();
+    ~canvas_gradient() override;
 
 public js_methods:
     auto add_color_stop(ext::number<double> offset, ext::string&& color);
+
+public cpp_methods:
+    auto to_v8(v8::Isolate *isolate) const && -> ext::any override;
+
+private cpp_properties:
+    ext::variant<QLinearGradient, QRadialGradient, QConicalGradient> m_gradient;
 };
 
 
