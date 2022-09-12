9a216e798a075fde85da832ada65404f11286706
Sam <samuelgardner101@gmail.com>
2022-09-12 03:28:16 +0100

    Made enum-class local to class, fixed parameter names and types

--- src/javascript/web_apis/html/canvasing/mixins/canvas_fill_stroke_style.hpp
+++ src/javascript/web_apis/html/canvasing/mixins/canvas_fill_stroke_style.hpp
@@ -13,14 +13,17 @@ namespace html::canvasing {class canvas_pattern;}
 class html::canvasing::mixins::canvas_fill_stroke_style
         : public virtual dom_object
 {
+public enums:
+    enum class canvas_pattern_repeat_t {_, REPEAT, REPEAT_X, REPEAT_Y, NO_REPEAT};
+
 public constructors:
     ~canvas_fill_stroke_style();
 
 public js_methods:
     auto create_linear_gradient(ext::number<double> x0, ext::number<double> x1, ext::number<double> y0, ext::number<double> y1) -> canvas_gradient;
     auto create_radial_gradient(ext::number<double> x0, ext::number<double> x1, ext::number<double> y0, ext::number<double> y1, ext::number<double> r0, ext::number<double> r1) -> canvas_gradient;
-    auto create_conic_gradient(ext::number<double> angle, ext::number<double> x, ext::number<double> y) -> canvas_gradient;
-    auto create_pattern(detail::canvas_image_source_t&& image, ext::string&& repetition) -> canvas_pattern;
+    auto create_conic_gradient(ext::number<double> start_angle, ext::number<double> x, ext::number<double> y) -> canvas_gradient;
+    auto create_pattern(detail::canvas_image_source_t&& image, canvas_pattern_repeat_t repetition) -> canvas_pattern;
 
 public js_properties:
     ext::property<ext::variant<ext::string, canvas_gradient*, canvas_pattern*>> fill_style;
