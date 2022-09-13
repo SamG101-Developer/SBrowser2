4494dc3a25faaa3cb1279d0665b15a9d56db0694
Sam <samuelgardner101@gmail.com>
2022-09-13 13:56:24 +0100

    Formatted, added accessor

--- src/javascript/web_apis/media_source/media_source.hpp
+++ src/javascript/web_apis/media_source/media_source.hpp
@@ -11,11 +11,21 @@ namespace media::source {class media_source;}
 namespace media::source {class media_source_handle;}
 namespace media::source {class source_buffer;}
 
+
 class media::source::media_source
         : public dom::nodes::event_target
 {
 public constructors:
     media_source();
+
+public js_methods:
+    auto add_source_buffer(const ext::string& type) -> source_buffer;
+    auto remove_source_buffer(source_buffer* source_buffer) -> void;
+    auto end_of_stream(ext::optional<detail::end_of_stream_error_t> error) -> void;
+    auto set_live_seekable_range(ext::number<double> start, ext::number<double> end) -> void;
+    auto clear_live_seekable_range() -> void;
+    static auto is_type_supported(ext::string_view type) -> ext::boolean;
+
 
 public js_properties:
     ext::property<std::unique_ptr<media_source_handle>> handle;
@@ -23,21 +33,16 @@ public js_properties:
     ext::property<ext::vector<source_buffer*>> active_source_buffers;
     ext::property<detail::ready_state_t> ready_state;
     ext::property<ext::number<double>> duration;
-    ext::property<ext::boolean> can_construct_in_dedicated_worker;
-
-public js_methods:
-    auto add_source_buffer(const ext::string& type) -> source_buffer;
-    auto remove_source_buffer(source_buffer* source_buffer) -> void;
-    auto end_of_stream(ext::optional<detail::end_or_stream_error> error) -> void;
-    auto set_live_seekable_range(ext::number<double> start, ext::number<double> end) -> void;
-    static auto is_type_supported(ext::string_view type) -> ext::boolean;
+    static ext::property<ext::boolean> can_construct_in_dedicated_worker;
 
 private js_slots:
     // TODO : [[live_seekable_range]]
 
 private cpp_accessors:
-    DEFINE_GETTER(duration)
-    DEFINE_SETTER(duration)
+    DEFINE_GETTER(handle);
+    DEFINE_GETTER(duration);
+
+    DEFINE_SETTER(duration);
 };
 
 
