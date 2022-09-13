9a2414d23cd13d332b8c04be5b3db79abbcb34e6
Sam <samuelgardner101@gmail.com>
2022-09-13 18:19:52 +0100

    Formatting

--- src/javascript/web_apis/mediacapture_automation/_typedefs.hpp
+++ src/javascript/web_apis/mediacapture_automation/_typedefs.hpp
@@ -7,12 +7,12 @@
 
 namespace mediacapture::detail
 {
+    enum class mock_capture_prompt_result_t {GRANTED, DENIED};
+
     using mock_capture_prompt_result_configuration_t = ext::map<ext::string, ext::any>;
     using mock_capture_device_configuration_t = ext::map<ext::string, ext::any>;
     using mock_camera_configuration_t = ext::map<ext::string, ext::any>;
     using mock_microphone_configuration_t = ext::map<ext::string, ext::any>;
-
-    enum class mock_capture_prompt_result_t {GRANTED, DENIED};
 }
 
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_MEDIA_AUTOMATION__TYPEDEFS_HPP
