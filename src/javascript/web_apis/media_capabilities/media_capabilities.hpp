e81276455d012f725b3a451d5fe80f136599cfde
Sam <samuelgardner101@gmail.com>
2022-09-13 02:01:44 +0100

    Defined more functions

--- src/javascript/web_apis/media_capabilities/media_capabilities.hpp
+++ src/javascript/web_apis/media_capabilities/media_capabilities.hpp
@@ -13,15 +13,11 @@ class media::capabilities::media_capabilities
         : public virtual dom_object
 {
 public constructors:
-    DOM_CTORS(media_capabilities);
+    media_capabilities() = default;
 
-    auto decoding_info(
-            detail::media_decoding_configuration_t&& configuration)
-            -> ext::promise<detail::media_capabilities_decoding_info_t>;
-
-    auto encoding_info(
-            detail::media_decoding_configuration_t&& configuration)
-            -> ext::promise<detail::media_capabilities_decoding_info_t>;
+public js_methods:
+    auto decoding_info(detail::media_decoding_configuration_t&& configuration) -> ext::promise<detail::media_capabilities_decoding_info_t>;
+    auto encoding_info(detail::media_decoding_configuration_t&& configuration) -> ext::promise<detail::media_capabilities_decoding_info_t>;
 };
 
 
