7fedfd9b9e8a27bf29654f3ba40b3e6aa4d23de7
Sam <samuelgardner101@gmail.com>
2022-09-13 18:20:41 +0100

    Added new shorthand MACRO for re-throwing a JavaScript exception if it already threw (and was caught)

--- src/javascript/interop/annotations.hpp
+++ src/javascript/interop/annotations.hpp
@@ -10,5 +10,7 @@
 #define JS_EXCEPTION_MESSAGE _js_exception_handler.Message()->Get()
 #define JS_EXCEPTION_RETHROW _js_exception_handler.ReThrow()
 #define JS_EXCEPTION _js_exception_handler.Exception()
+#define JS_EXCEPTION_RETHROW_IF_THREW if (JS_EXCEPTION_HAS_THROWN) JS_EXCEPTION_RETHROW
+
 
 #endif //SBROWSER2_ANNOTATIONS_HPP
