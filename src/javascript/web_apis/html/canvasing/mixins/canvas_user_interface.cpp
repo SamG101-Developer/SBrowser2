68f0f72e71812716d0a879a959eab040143f2af9
Sam <samuelgardner101@gmail.com>
2022-09-13 02:00:41 +0100

    Implemented

--- src/javascript/web_apis/html/canvasing/mixins/canvas_user_interface.cpp
+++ src/javascript/web_apis/html/canvasing/mixins/canvas_user_interface.cpp
@@ -1,1 +1,15 @@
 #include "canvas_user_interface.hpp"
+
+#include "html/detail/focus_internals.hpp"
+
+#include <qpainter.h>
+
+
+auto html::canvasing::mixins::canvas_user_interface::draw_if_focus_needed(
+        dom::nodes::element* element)
+        -> void
+{
+    return_if (!html::detail::is_focused(element));
+
+    // TODO
+}
