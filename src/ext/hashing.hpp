159ecf6fb7f1cd9ec2676a71d87e431e928f625e
Sam <samuelgardner101@gmail.com>
2022-09-13 18:22:03 +0100

    Moved hashing functors into this file from `ext/hash.hpp`.

--- src/ext/hashing.hpp
+++ src/ext/hashing.hpp
@@ -1,7 +1,14 @@
 #ifndef SBROWSER2_HASHING_HPP
 #define SBROWSER2_HASHING_HPP
 
-#include "ext/keywords.hpp"
+#include "ext/any.hpp"
+#include "ext/boolean.hpp"
+#include "ext/functional.hpp"
+#include "ext/map.hpp"
+#include "ext/number.hpp"
+#include "ext/set.hpp"
+#include "ext/tuple.hpp"
+#include <type_traits>
 
 
 _EXT_BEGIN
@@ -33,6 +40,45 @@ struct hash
 };
 
 _EXT_END
+
+
+_STD_BEGIN
+
+template <>
+struct hash<_EXT any>
+{constexpr auto operator()(const _EXT any& value) const noexcept -> size_t {return *value.m_hash;}};
+
+
+template <>
+struct hash<_EXT boolean>
+{constexpr auto operator()(const _EXT boolean& value) const noexcept -> size_t {return value ? 1 : 0;}};
+
+
+template <typename ...Ts>
+struct hash<_EXT function<Ts...>>
+{constexpr auto operator()(const _EXT function<Ts...>& value) const noexcept -> size_t {return _STD hash<size_t>{}(static_cast<size_t>(&value));}};
+
+
+template <typename K, typename V>
+struct hash<_EXT map<K, V>>
+{constexpr auto operator()(const _EXT map<K, V>& value) const noexcept -> size_t {return _STD hash<size_t>{}(&value);}};
+
+
+template <typename T>
+struct hash<_EXT number<T>>
+{constexpr auto operator()(const _EXT number<T>& value) const noexcept -> size_t {return _STD hash<T>{}(*value);}};
+
+
+template <typename T>
+struct hash<_EXT set<T>>
+{constexpr auto operator()(const _EXT set<T>& value) const noexcept -> size_t {return _STD hash<size_t>{}(&value);}};
+
+
+template <typename ...Ts>
+struct hash<_EXT tuple<Ts...>>
+{constexpr auto operator()(const _EXT tuple<Ts...>& value) const noexcept -> size_t {return 0;}};
+
+_STD_END
 
 
 #endif //SBROWSER2_HASHING_HPP
