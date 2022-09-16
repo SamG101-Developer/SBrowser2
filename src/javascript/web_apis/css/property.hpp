439a674b745dafedf3f9f58aa84430492d71e84b
Sam <samuelgardner101@gmail.com>
2022-09-16 11:37:51 +0100

    Fixed css property abstract base class

--- src/javascript/web_apis/css/property.hpp
+++ src/javascript/web_apis/css/property.hpp
@@ -1,37 +1,46 @@
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_PROPERTY_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_PROPERTY_HPP
 
+#include "ext/assertion.hpp"
 #include "ext/boolean.hpp"
 #include "ext/concepts.hpp"
+#include "ext/functional.hpp"
 #include "ext/property.hpp"
 #include "ext/detail/meta_property.hpp"
-namespace css {template <type_is_enum T> class css_property;}
-namespace css {template <type_is_enum T> class css_meta_property;}
+
+#include <qpainter.h>
+#include <qwidget.h>
 
 
-template <type_is_enum T>
-class css::css_property : public ext::property<T>
+namespace css::detail
 {
-public:
-    // Access to the value type
-    friend struct ::access_meta;
-    using value_t = T;
+    enum class percent_t {NA};
+    enum class computed_value_t {SPECIFIED_KEYWORDS};
+    enum class canonical_order_t {PER_GRAMMAR};
+    enum class animation_type_t {DISCRETE};
+}
 
-    css_property() = default;
 
-private:
-    css::css_meta_property<T> m_meta;
+namespace css
+{
+
+template <
+        type_is_enum allowed_values_t,
+        bool inherited = false,
+        css::detail::percent_t percent = detail::percent_t::NA,
+        css::detail::computed_value_t computed_value = detail::computed_value_t::SPECIFIED_KEYWORDS,
+        css::detail::canonical_order_t canonical_order = detail::canonical_order_t::PER_GRAMMAR,
+        css::detail::animation_type_t animation_type = detail::animation_type_t::DISCRETE>
+struct abstract_property
+{
+    using values_t = allowed_values_t;
+
+    virtual auto name() -> ext::string = 0;
+    virtual auto initial() -> ext::string = 0;
+    virtual auto paint(QPainter* painter, QWidget* widget, values_t value) -> void = 0;
 };
 
-
-template <type_is_enum T>
-class css::css_meta_property : public ext::detail::meta_property<T>
-{
-private:
-    ext::string m_name;
-    T m_default_value;
-    ext::boolean inherited;
-};
+}
 
 
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_PROPERTY_HPP
