e5ec875f4bd37b073239d9980a90693ffbd21809
Sam <samuelgardner101@gmail.com>
2022-09-17 02:04:05 +0100

    Changed binders to hugely more optimal lambdas (MACRO invocation)

--- src/ext/functional.hpp
+++ src/ext/functional.hpp
@@ -112,68 +112,14 @@ struct ne
 }
 
 
-// bind arguments to the back of a method, so that when the partial-method is called with arguments, they will the front
-// n arguments (using because std::bind_back isn't available yet on MSVC)
-template <callable F, typename ...Args0>
-struct bind_back
-{
-public:
-    explicit bind_back(F&& function, Args0&&... fixed_args)
-            : m_function{std::forward<F>(function)}
-            , m_back_args{_EXT make_tuple(std::forward<Args0>(fixed_args)...)}
-    {
-        // initialize the function and the back arguments, by forwarding the arguments into a tuple and setting this as
-        // 'm_back_args'
-    }
+#define BIND_FRONT(function, ...)                 \
+    [&]<typename ...Args>(Args&&... args) mutable \
+    {return function(__VA_ARGS__, std::forward<Args>(args)...);}
 
-    template <typename ...Args1>
-    constexpr auto operator()(Args1&&... variable_args) const
-    {
-        // forward the new arguments into a tuple, concatenate this to the front of the 'm_front_args', and apply this
-        // tuple to the 'm_function', so that the function is invoked with all the arguments
-        return _EXT apply(std::forward<F>(m_function), _EXT tuple_cat(_EXT forward_as_tuple(std::forward<Args1>(variable_args)...), std::move(m_back_args)));
-    }
+#define BIND_BACK(function, ...)                  \
+    [&]<typename ...Args>(Args&&... args) mutable \
+    {return function(std::forward<Args>(args...), __VA_ARGS__);}
 
-private:
-    F&& m_function;
-    _EXT tuple<Args0...> m_back_args;
-};
-
-
-template <callable F, typename ...Args0>
-bind_back(F&&, Args0&&...) -> bind_back<F, Args0...>;
-
-
-// bind arguments to the front of a method, so that when the partial-method is called with arguments, they will the back
-// n arguments (using because std::bind_back isn't available yet on MSVC, so conform with ext::bind_back)
-template <typename F, typename ...Args0>
-struct bind_front
-{
-public:
-    explicit bind_front(F&& function, Args0&&... fixed_args)
-            : m_function{std::forward<F>(function)}
-            , m_front_args{_EXT make_tuple(std::forward<Args0>(fixed_args)...)}
-    {
-        // initialize the function and the front arguments, by forwarding the arguments into a tuple and setting this as
-        // as 'm_front_args'
-    };
-
-    template <typename ...Args1>
-    constexpr auto operator()(Args1&&... variable_args) const
-    {
-        // forward the new arguments into a tuple, concatenate this to the back of the 'm_back_args', and apply this
-        // tuple to the 'm_function', so that the function is invoked with all the arguments
-        return _EXT apply(std::forward<F>(m_function), _EXT tuple_cat(std::move(m_front_args), _EXT forward_as_tuple(std::forward<Args1>(variable_args)...)));
-    }
-
-private:
-    F&& m_function;
-    _EXT tuple<Args0...> m_front_args;
-};
-
-
-template <callable F, typename ...Args0>
-bind_front(F&&, Args0&&...) -> bind_front<F, Args0...>;
 
 _EXT_END
 
