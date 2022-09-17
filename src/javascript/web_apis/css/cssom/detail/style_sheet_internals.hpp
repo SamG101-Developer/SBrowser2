9169b05216891b7a3756ae40ef51a6338204cd46
Sam <samuelgardner101@gmail.com>
2022-09-17 02:05:11 +0100

    Implemented

--- src/javascript/web_apis/css/cssom/detail/style_sheet_internals.hpp
+++ src/javascript/web_apis/css/cssom/detail/style_sheet_internals.hpp
@@ -3,9 +3,12 @@
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_CSSOM_DETAIL_STYLE_SHEET_INTERNALS_HPP
 
 #include "ext/boolean.hpp"
+#include "ext/set.hpp"
 #include "ext/string.hpp"
 #include "ext/vector.hpp"
+
 #include USE_INNER_TYPES(css/cssom)
+#include USE_INNER_TYPES(fetch)
 #include USE_INNER_TYPES(url)
 
 namespace dom::nodes {class document;}
@@ -14,6 +17,53 @@ namespace dom::nodes {class node;}
 
 namespace css::detail
 {
+    auto create_css_style_sheet()
+            -> css_style_sheet_t;
+
+    auto add_css_style_sheet()
+            -> void;
+
+    auto remove_css_style_sheet(
+            const css_style_sheet_t& style_sheet)
+            -> void;
+
+    auto persistent_css_style_sheet(
+            const css_style_sheet_t& style_sheet)
+            -> ext::boolean;
+
+    auto is_css_style_sheet_set(
+            const ext::set<css_style_sheet_t*>& style_sheets)
+            -> ext::boolean;
+
+    auto css_style_sheet_set_name(
+            const ext::set<css_style_sheet_t*>& style_sheets)
+            -> ext::string_view;
+
+    auto enable_style_sheet_set(
+            ext::string_view name)
+            -> void;
+
+    auto select_style_sheet_set(
+            ext::string_view name)
+            -> void;
+
+    auto change_preferred_css_style_sheet_set_name(
+            ext::string_view name)
+            -> void;
+
+    auto fetch_css_style_sheets(
+            const url::detail::url_t& parsed_url,
+            ext::variant<fetch::detail::referrer_t, url::detail::url_t*> referrer,
+            dom::nodes::node* document,
+            ext::set<ext::string> parameters)
+            -> void;
+
+    auto associated_css_style_sheet(
+            dom::nodes::node* node)
+            -> css_style_sheet_t&;
+
+    ext::string last_css_style_sheet_set_name; // TODO : global variable :(
+    ext::string preferred_css_style_sheet_set_name; // TODO : global variable :(
 }
 
 
@@ -31,6 +81,7 @@ struct css::detail::css_style_sheet_t
     ext::vector<ext::string> media;
     ext::boolean alternate_flag;
     ext::boolean disabled_flag;
+    ext::boolean origin_clean_flag;
     ext::boolean constructed_flag;
     ext::boolean disallow_modifications_flag;
 
--- src/javascript/web_apis/css/cssom/style_sheets/style_sheet.hpp
+++ src/javascript/web_apis/css/cssom/style_sheets/style_sheet.hpp
@@ -2,14 +2,44 @@
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_CSSOM_STYLESHEET_STYLE_SHEET_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_CSSOM_STYLESHEET_STYLE_SHEET_HPP
 
-#include "dom_object"
+#include "dom_object.hpp"
 namespace css::cssom::style_sheets {class style_sheet;}
+
+#include USE_INNER_TYPES(css/cssom)
+namespace css::cssom::style_sheets {class css_style_sheet;}
+namespace dom::nodes {class element;}
+namespace dom::nodes {class processing_instruction;}
 
 
 class css::cssom::style_sheets::style_sheet
         : public dom_object
 {
+public constructors:
+    style_sheet();
 
+public js_properties:
+    ext::property<ext::string> type;
+    ext::property<ext::string> href;
+    ext::property<ext::string> title;
+
+    ext::property<dom::nodes::node*> owner_node;
+    ext::property<css_style_sheet*> parent_style_sheet;
+    ext::property<ext::vector<ext::string>> media;
+    ext::property<ext::boolean> disabled;
+
+protected cpp_properties:
+    std::unique_ptr<detail::css_style_sheet_t> m_style_sheet;
+
+private cpp_accessors:
+    DEFINE_GETTER(type);
+    DEFINE_GETTER(href);
+    DEFINE_GETTER(title);
+    DEFINE_GETTER(owner_node);
+    DEFINE_GETTER(parent_style_sheet);
+    DEFINE_GETTER(media);
+    DEFINE_GETTER(disabled);
+
+    DEFINE_SETTER(disabled);
 };
 
 
