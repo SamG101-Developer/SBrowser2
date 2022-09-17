8d98805a387cb617f0679171247be202fc66c945
Sam <samuelgardner101@gmail.com>
2022-09-17 02:02:43 +0100

    Defined another type

--- src/javascript/web_apis/css/cssom/_typedefs.hpp
+++ src/javascript/web_apis/css/cssom/_typedefs.hpp
@@ -1,11 +1,17 @@
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_CSSOM__TYPEDEFS_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CSS_CSSOM__TYPEDEFS_HPP
 
+#include "ext/any.hpp"
+#include "ext/map.hpp"
+#include "ext/string.hpp"
+
 
 namespace css::detail
 {
     struct css_rule_t;
     struct css_style_sheet_t;
+
+    using css_style_sheet_init_t = ext::map<ext::string, ext::any>;
 }
 
 
