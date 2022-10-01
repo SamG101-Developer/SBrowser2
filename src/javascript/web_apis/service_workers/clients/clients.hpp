5960d33632b5f1af9ee0ba1a439cb6fe301ac0d7
Sam <samuelgardner101@gmail.com>
2022-10-01 01:15:26 +0100

    Adjusted for PIMPL (huge class design overhaul)

--- src/javascript/web_apis/service_workers/clients/clients.hpp
+++ src/javascript/web_apis/service_workers/clients/clients.hpp
@@ -12,17 +12,20 @@ namespace service_workers::clients {class clients;}
 namespace service_workers::clients {class client;}
 namespace service_workers::clients {class window_client;}
 
+#include "clients_private.hpp"
+
 class service_workers::clients::clients
         : public virtual dom_object
 {
 public constructors:
-    DOM_CTORS(clients);
-    clients() = default;
+    clients();
+    MAKE_PIMPL(clients);
+    MAKE_V8_AVAILABLE;
 
 public js_methods:
-    auto get(ext::string_view id) -> ext::variant<ext::promise<void>, ext::promise<client>>;
+    auto get(ext::string_view id) -> ext::variant<ext::promise<void>, ext::promise<client*>>;
     auto match_all(detail::client_query_options_t&& options = {}) -> ext::promise<const ext::vector<client*>>;
-    auto open_window(ext::string_view url) -> ext::promise<window_client>;
+    auto open_window(ext::string_view url) -> ext::promise<window_client*>;
     auto claim() -> ext::promise<void>;
 };
 
---
+++ src/javascript/web_apis/service_workers/clients/clients_private.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_SERVICE_WORKERS_CLIENTS_CLIENTS_PRIVATE_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_SERVICE_WORKERS_CLIENTS_CLIENTS_PRIVATE_HPP
+
+#include "ext/pimpl.hpp"
+#include "dom_object_private.hpp"
+
+
+DEFINE_PRIVATE_CLASS(service_workers::clients, clients) : virtual dom_object_private {};
+
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_SERVICE_WORKERS_CLIENTS_CLIENTS_PRIVATE_HPP
