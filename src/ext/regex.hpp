1118aea948a147e8a0da57224f22a7ef5047ff50
Sam <samuelgardner101@gmail.com>
2022-10-07 20:28:04 +0100

    Enabled regex library

---
+++ src/ext/regex.hpp
@@ -0,0 +1,20 @@
+#ifndef SBROWSER2_SRC_EXT_REGEX_HPP
+#define SBROWSER2_SRC_EXT_REGEX_HPP
+
+#include "ext/keywords.hpp"
+#include <ctre.hpp>
+
+
+_EXT_BEGIN
+
+namespace regex = ctre;
+
+_EXT_END
+
+
+_EXT_LITERALS_BEGIN
+using namespace ctre::literals;
+_EXT_LITERALS_END
+
+
+#endif //SBROWSER2_SRC_EXT_REGEX_HPP
