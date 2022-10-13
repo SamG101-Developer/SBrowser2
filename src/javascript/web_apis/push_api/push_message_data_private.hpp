a000d2798871a74ae08e5342042517c75d977b0f
Sam <samuelgardner101@gmail.com>
2022-10-13 11:33:27 +0100

    Created

---
+++ src/javascript/web_apis/push_api/push_message_data_private.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_PUSH_API_PUSH_MESSAGE_DATA_PRIVATE_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_PUSH_API_PUSH_MESSAGE_DATA_PRIVATE_HPP
+
+#include "ext/pimpl.hpp"
+#include "dom_object_private.hpp"
+
+#include "ext/string.hpp"
+
+
+DEFINE_PRIVATE_CLASS(push_api, push_message_data) : virtual dom_object_private
+{
+    ext::string bytes;
+};
+
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_PUSH_API_PUSH_MESSAGE_DATA_PRIVATE_HPP
