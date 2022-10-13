def6927c79d987a892e5f55ab16943463b2818d9
Sam <samuelgardner101@gmail.com>
2022-10-13 11:32:26 +0100

    Added more types and enumerations

--- src/javascript/web_apis/push_api/_typedefs.hpp
+++ src/javascript/web_apis/push_api/_typedefs.hpp
@@ -9,10 +9,13 @@
 #include <v8-forward.h>
 
 #include INCLUDE_INNER_TYPES(url)
+#include INCLUDE_INNER_TYPES(web_idl)
 
 
 namespace push_api::detail
 {
+    enum class push_encryption_key_name_t {P256DH, AUTH};
+
     using push_message_t = ext::string;
     using push_endpoint_t = url::detail::url_t;
     using push_subscription_options_t = ext::map<ext::string, ext::any>;
@@ -21,6 +24,7 @@ namespace push_api::detail
     using push_subscription_json_t = ext::map<ext::string, ext::any>;
     using authentication_secret_t = ext::octet_string;
     using application_server_key_t = ext::variant<ext::variant_monostate_t, ext::string, v8::Local<v8::ArrayBuffer>>;
+    using byte_sequence_container_t = ext::variant<ext::string_view, v8::Local<v8::BufferSource>>;
 }
 
 
