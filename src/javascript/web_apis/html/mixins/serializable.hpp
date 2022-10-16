3c6c5981d69a6720f1b81a64e1c261c6da3fdfc7
Sam <samuelgardner101@gmail.com>
2022-10-16 15:46:35 +0100

    Removed unused `#include`s

--- src/javascript/web_apis/html/mixins/serializable.hpp
+++ src/javascript/web_apis/html/mixins/serializable.hpp
@@ -2,6 +2,10 @@
 #ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_MIXINS_SERIALIZABLE_HPP
 #define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_MIXINS_SERIALIZABLE_HPP
 
+#include "ext/any.hpp"
+#include "ext/map.hpp"
+#include "ext/string.hpp"
+
 
 #define MAKE_SERIALIZABLE \
     auto _serialize(ext::map<ext::string, ext::any>& serialized, ext::boolean for_storage) -> void; \
