5d7ca51d9eba540f88d1860dbfb173e5938a9a7b
Sam <samuelgardner101@gmail.com>
2022-10-17 21:19:06 +0100

    Implemented

--- src/javascript/web_apis/html/mixins/content_editable.cpp
+++ src/javascript/web_apis/html/mixins/content_editable.cpp
@@ -1,6 +1,7 @@
 #include "content_editable.hpp"
 #include "content_editable_private.hpp"
 
+#include "dom/detail/customization_internals.hpp"
 #include "html/detail/editing_internals.hpp"
 
 
@@ -13,29 +14,60 @@ auto html::mixins::content_editable::get_is_content_editable() const -> ext::boo
 
 auto html::mixins::content_editable::get_content_editable() const -> ext::string
 {
-    ACCESS_PIMPL(const content_editable);
-
-    return_if (!d->content_editable.has_value()) u8"inherited";
-    return *d->content_editable ? u8"true" : u8"false";
+    CE_REACTIONS_METHOD_DEF
+        ACCESS_PIMPL(const content_editable);
+        return_if (!d->content_editable.has_value()) u8"inherited";
+        return *d->content_editable ? u8"true" : u8"false";
+    CE_REACTIONS_METHOD_EXE
 }
 
 
 auto html::mixins::content_editable::get_enter_key_hit() const -> detail::editable_enter_key_hit_t
 {
-    ACCESS_PIMPL(const content_editable);
-    return d->enter_key_hit;
+    CE_REACTIONS_METHOD_DEF
+        ACCESS_PIMPL(const content_editable);
+        return d->enter_key_hit;
+    CE_REACTIONS_METHOD_EXE
 }
 
 
 auto html::mixins::content_editable::get_input_mode() const -> detail::editable_input_mode_t
 {
-    ACCESS_PIMPL(const content_editable);
-    return d->input_mode;
+    CE_REACTIONS_METHOD_DEF
+        ACCESS_PIMPL(const content_editable);
+        return d->input_mode;
+    CE_REACTIONS_METHOD_EXE
 }
 
 
-auto html::mixins::content_editable::set_content_editable(ext::string new_content_editable) -> ext::string
+auto html::mixins::content_editable::set_content_editable(
+        ext::string new_content_editable)
+        -> ext::string
 {
-    ACCESS_PIMPL(content_editable);
-    return d->content_editable = new_content_editable == u8"inherited" ? ext::nullopt : std::lexical_cast<bool>(new_content_editable);
+    CE_REACTIONS_METHOD_DEF
+        ACCESS_PIMPL(content_editable);
+        return d->content_editable = new_content_editable == u8"inherited" ? ext::nullopt : std::lexical_cast<bool>(new_content_editable);
+    CE_REACTIONS_METHOD_EXE
+}
+
+
+auto html::mixins::content_editable::set_enter_key_hit(
+        detail::editable_enter_key_hit_t new_enter_key_hit)
+        -> detail::editable_enter_key_hit_t
+{
+    CE_REACTIONS_METHOD_DEF
+        ACCESS_PIMPL(content_editable);
+        return d->enter_key_hit = new_enter_key_hit;
+    CE_REACTIONS_METHOD_EXE
+}
+
+
+auto html::mixins::content_editable::set_input_mode(
+        detail::editable_input_mode_t new_input_mode)
+        -> detail::editable_input_mode_t
+{
+    CE_REACTIONS_METHOD_DEF
+        ACCESS_PIMPL(content_editable);
+        return d->input_mode = new_input_mode;
+    CE_REACTIONS_METHOD_EXE
 }
