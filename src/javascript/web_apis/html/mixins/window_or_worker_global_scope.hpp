856b9777a9cfc9f6c7c4f6fcea8ba96f1a6ce631
Sam <samuelgardner101@gmail.com>
2022-10-17 21:20:02 +0100

    Adjusted for PIMPL (huge class design overhaul)

--- src/javascript/web_apis/html/mixins/window_or_worker_global_scope.hpp
+++ src/javascript/web_apis/html/mixins/window_or_worker_global_scope.hpp
@@ -4,7 +4,9 @@
 
 #include "dom_object.hpp"
 namespace html::mixins {class window_or_worker_global_scope;}
+namespace html::mixins {class window_or_worker_global_scope_private;}
 
+#include INCLUDE_INNER_TYPES(html)
 namespace indexed_db {class idb_factory;}
 namespace service_workers::caches {class cache_storage;}
 namespace web_crypto {class crypto;}
@@ -13,15 +15,24 @@ namespace web_crypto {class crypto;}
 class html::mixins::window_or_worker_global_scope
         : public virtual dom_object
 {
+public constructors:
+    MAKE_PIMPL(window_or_worker_global_scope);
+    MAKE_V8_AVAILABLE;
+
+public js_methods:
+    auto report_error(ext::any e) -> void;
+    auto btoa(ext::string_view data) -> ext::string;
+    auto atob(ext::string_view data) -> ext::byte_string;
+
 private js_properties:
-    /* INDEX_DB */
-    ext::property<indexed_db::idb_factory*> indexed_db;
+    /* [INDEX-DB] */
+    DEFINE_GETTER(indexed_db, indexed_db::idb_factory*);
 
-    /* SERVICE_WORKERS */
-    ext::property<std::unique_ptr<service_workers::caches::cache_storage>> caches;
+    /* [SERVICE-WORKERS] */
+    DEFINE_GETTER(cache, service_workers::caches::cache_storage);
 
-    /* WEB_CRYPTO */
-    ext::property<std::unique_ptr<web_crypto::crypto>> crypto;
+    /* [WEB-CRYPTO] */
+    DEFINE_GETTER(crypto, web_crypto::crypto*);
 };
 
 
