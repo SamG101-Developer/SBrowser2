e7b5b8a7086c14f865bcc600ff0d8b96536bff39
Sam <samuelgardner101@gmail.com>
2022-10-19 00:43:49 +0100

    Created

---
+++ src/javascript/web_apis/html/detail/worker_internals.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_DETAIL_WORKER_INTERNALS_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_DETAIL_WORKER_INTERNALS_HPP
+
+#include "ext/boolean.hpp"
+#include "ext/number.hpp"
+#include INCLUDE_INNER_TYPES(html)
+namespace html::mixins {class window_or_worker_global_scope;}
+
+
+namespace html::detail
+{
+    template <typename ...Args>
+    auto timer_initialization_steps(
+            mixins::window_or_worker_global_scope* global,
+            detail::timer_handler_t&& handler,
+            ext::number<long> timeout,
+            Args&&... arguments,
+            ext::boolean repeat,
+            ext::number<int> previous_id = 0)
+            -> ext::number<long>;
+}
+
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_DETAIL_WORKER_INTERNALS_HPP
