aadefceb516da8efe7b32458fd159ab7c14a54a4
Sam <samuelgardner101@gmail.com>
2022-10-20 12:23:14 +0100

    Fixed types

--- src/javascript/web_apis/html/elements/html_body_element.cpp
+++ src/javascript/web_apis/html/elements/html_body_element.cpp
@@ -1,18 +1,20 @@
 #include "html_body_element.hpp"
+#include "html_body_element_private.hpp"
 
 
 html::elements::html_body_element::html_body_element()
 {
+    INIT_PIMPL(html_body_element)
     HTML_CONSTRUCTOR
 }
 
 
-auto html::elements::html_body_element::to_v8(
-        v8::Isolate* isolate)
-        const&& -> ext::any
+auto html::elements::html_body_element::to_v8(v8::Isolate* isolate) -> v8pp::class_<self_t>
 {
-    return v8pp::class_<html_body_element>{isolate}
+    decltype(auto) conversion = v8pp::class_<html_body_element>{isolate}
             .ctor<>()
             .inherit<html_element>()
             .auto_wrap_objects();
+
+    return std::move(conversion);
 }
---
+++ src/javascript/web_apis/html/elements/html_body_element_private.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_ELEMENTS_HTML_BODY_ELEMENT_PRIVATE_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_ELEMENTS_HTML_BODY_ELEMENT_PRIVATE_HPP
+
+#include "ext/pimpl.hpp"
+#include "html/elements/html_element_private.hpp"
+
+
+DEFINE_PRIVATE_CLASS(html::elements, html_body_element_private) : html::elements::html_element_private {};
+
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_HTML_ELEMENTS_HTML_BODY_ELEMENT_PRIVATE_HPP
