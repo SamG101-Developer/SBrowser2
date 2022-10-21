d04223d64201e6512ee2ad07097a036275c15793
Sam <samuelgardner101@gmail.com>
2022-10-21 15:43:38 +0100

    Adjusted for PIMPL (huge class design overhaul)

--- src/javascript/web_apis/html/elements/html_hr_element.cpp
+++ src/javascript/web_apis/html/elements/html_hr_element.cpp
@@ -7,12 +7,12 @@ html::elements::html_hr_element::html_hr_element()
 }
 
 
-auto html::elements::html_hr_element::to_v8(
-        v8::Isolate* isolate)
-        const&& -> ext::any
+auto html::elements::html_hr_element::to_v8(v8::Isolate* isolate) -> v8pp::class_<self_t>
 {
-    return v8pp::class_<html_hr_element>{isolate}
-            .ctor<>()
-            .inherit<html_element>()
-            .auto_wrap_objects();
+    decltype(auto) conversion = v8pp::class_<html_hr_element>{isolate}
+        .ctor<>()
+        .inherit<html_element>()
+        .auto_wrap_objects();
+
+    return std::move(conversion);
 }
