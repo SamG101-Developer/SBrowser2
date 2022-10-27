ffa0b34ce8d3d688a9bd50434062324a915675b4
Sam <samuelgardner101@gmail.com>
2022-10-27 13:35:41 +0100

    Begin mass commenting & formatting of comments

--- src/javascript/web_apis/dom/nodes/cdata_section.cpp
+++ src/javascript/web_apis/dom/nodes/cdata_section.cpp
@@ -12,15 +12,14 @@ dom::nodes::character_data::character_data()
 
 auto dom::nodes::cdata_section::get_node_name() const -> ext::string
 {
+    // The 'node_name' getter returns the fixed string "#cdata-section". Apply custom element reactions to this getter.
     CE_REACTIONS_METHOD_DEF
-        return "#cdata-section";
+        return u8"#cdata-section";
     CE_REACTIONS_METHOD_EXE
 }
 
 
-auto dom::nodes::cdata_section::to_v8(
-        v8::Isolate* isolate)
-        -> v8pp::class_<self_t>
+auto dom::nodes::cdata_section::to_v8(v8::Isolate* isolate) -> v8pp::class_<self_t>
 {
     decltype(auto) conversion = v8pp::class_<cdata_section>{isolate}
         .inherit<text>()
--- src/javascript/web_apis/dom/nodes/character_data.cpp
+++ src/javascript/web_apis/dom/nodes/character_data.cpp
@@ -12,21 +12,20 @@ dom::nodes::character_data::character_data()
 }
 
 
-auto dom::nodes::character_data::substring_data(
-        ext::number<ulong> offset,
-        ext::number<ulong> count)
-        const -> ext::string
+auto dom::nodes::character_data::substring_data(ext::number<ulong> offset, ext::number<ulong> count) const -> ext::string
 {
-    // substring the data: substring 'count' characters from the index 'offset'
+    // To substring the data, substring 'count' characters from the index 'offset' (from the 'data' attribute stored in
+    // the private class). Return the result of substringing the data (the private-class'data' attribute remains the
+    // same hence the const-function-qualifier, so don't change and return it).
     return detail::substring_data(this, offset, count);
 }
 
 
-auto dom::nodes::character_data::append_data(
-        ext::string&& new_data)
-        -> ext::string
+auto dom::nodes::character_data::append_data(ext::string&& new_data) -> ext::string
 {
-    // append data: replace 0 characters after the length of this node with the 'new_data'
+    // To append data: replace 0 characters after the length of this node with the 'new_data' (from the 'data' attribute
+    // stored in the private class). This is semantically the same as appending data, but uses a common detail procedure
+    // to multiple other class methods to reduce code duplication. Return the 'data'.
     ACCESS_PIMPL(character_data);
     detail::replace_data(this, detail::length(this), 0, std::move(new_data));
     return d->data;
@@ -38,7 +37,9 @@ auto dom::nodes::character_data::insert_data(
         ext::string&& new_data)
         -> ext::string
 {
-    // insert data: replace 0 characters after 'offset' with 'new_data'
+    // To insert data: replace 0 characters after 'offset' with 'new_data' (from the 'data' attribute stored in the
+    // private class). This is semantically the same as inserting data, but uses a common detail procedure to multiple
+    // other class methods to reduce code duplication. Return the 'data'.
     ACCESS_PIMPL(character_data);
     detail::replace_data(this, offset, 0, std::move(new_data));
     return d->data;
@@ -50,9 +51,11 @@ auto dom::nodes::character_data::delete_data(
         ext::number<ulong> count)
         -> ext::string
 {
-    // delete data: replace 'count' characters after 'offset' with nothing
+    // To delete data: replace 'count' characters after 'offset' with nothing (from the 'data' attribute stored in the
+    // private class). This is semantically the same as deleting data, but uses a common detail procedure to multiple
+    // other class methods to reduce code duplication. Return the 'data'.
     ACCESS_PIMPL(character_data);
-    detail::replace_data(this, offset, count, "");
+    detail::replace_data(this, offset, count, u8"");
     return d->data;
 }
 
@@ -63,7 +66,9 @@ auto dom::nodes::character_data::replace_data(
         ext::string&& new_data)
         -> ext::string
 {
-    // replace data: replace 'count' characters after 'offset' with 'new_data'
+    // To replace data: replace 'count' characters after 'offset' with 'new_data' (from the 'data' attribute stored in
+    // the private class). This is semantically the same as replacing data, but uses a common detail procedure to
+    // multiple other class methods to reduce code duplication. Return the 'data'.
     ACCESS_PIMPL(character_data);
     detail::replace_data(this, offset, count, std::move(new_data));
     return d->data;
@@ -72,6 +77,8 @@ auto dom::nodes::character_data::replace_data(
 
 auto dom::nodes::character_data::get_node_value() const -> ext::string
 {
+    // The 'node_value' getter returns the equivalent 'data' attribute value that is stored in the private class. Apply
+    // custom element reactions to this getter.
     CE_REACTIONS_METHOD_DEF
         ACCESS_PIMPL(const character_data);
         return d->data;
@@ -81,6 +88,8 @@ auto dom::nodes::character_data::get_node_value() const -> ext::string
 
 auto dom::nodes::character_data::get_text_content() const -> ext::string
 {
+    // The 'text_content' getter returns the equivalent 'data' attribute value that is stored in the private class.
+    // Apply custom element reactions to this getter.
     CE_REACTIONS_METHOD_DEF
         ACCESS_PIMPL(const character_data);
         return d->data;
@@ -90,24 +99,32 @@ auto dom::nodes::character_data::get_text_content() const -> ext::string
 
 auto dom::nodes::character_data::set_node_value(ext::string new_node_value) -> ext::string
 {
+    // The 'node_value' setter sets the equivalent 'data' attribute value that is stored in the private class to the
+    // result of applying the 'replace_data(...)' method with the 'new_node_value', to handle bounds checks and live
+    // Range updates. Apply custom element reactions to this setter.
     CE_REACTIONS_METHOD_DEF
         ACCESS_PIMPL(character_data);
-        replace_data(0, d->data.length(), std::move(new_node_value));
+        return replace_data(0, d->data.length(), std::move(new_node_value));
     CE_REACTIONS_METHOD_EXE;
 }
 
 
 auto dom::nodes::character_data::set_text_content(ext::string new_text_content) -> ext::string
 {
+    // The 'text_content' setter sets the equivalent 'data' attribute value that is stored in the private class to the
+    // result of applying the 'replace_data(...)' method with the 'new_node_value', to handle bounds checks and live
+    // Range updates. Apply custom element reactions to this setter.
     CE_REACTIONS_METHOD_DEF
         ACCESS_PIMPL(character_data);
-        replace_data(0, d->data.length(), std::move(new_text_content));
+        return replace_data(0, d->data.length(), std::move(new_text_content));
     CE_REACTIONS_METHOD_EXE;
 }
 
 
 auto dom::nodes::character_data::get_data() const -> ext::string
 {
+    // The 'data' getter returns the equivalent 'data' attribute value that is stored in the private class. Apply custom
+    // element reactions to this getter.
     ACCESS_PIMPL(const character_data);
     return d->data;
 }
@@ -115,6 +132,8 @@ auto dom::nodes::character_data::get_data() const -> ext::string
 
 auto dom::nodes::character_data::get_length() const -> ext::number<ulong>
 {
+    // The 'length' getter returns the length of the 'data' attribute value that is stored in the private class. Apply
+    // custom element reactions to this getter.
     ACCESS_PIMPL(const character_data);
     return d->data.length();
 }
@@ -122,14 +141,15 @@ auto dom::nodes::character_data::get_length() const -> ext::number<ulong>
 
 auto dom::nodes::character_data::set_data(ext::string new_data) -> ext::string
 {
+    // The 'data' setter sets the equivalent 'data' attribute value that is stored in the private class to the result
+    // of applying the 'replace_data(...)' method with the 'new_data', to handle bounds checks and live Range updates.
+    // Apply custom element reactions to this setter.
     ACCESS_PIMPL(character_data);
-    d->data = std::move(new_data);
+    return replace_data(0, d->data.length(), std::move(new_data));
 }
 
 
-auto dom::nodes::character_data::to_v8(
-        v8::Isolate* isolate)
-        -> v8pp::class_<self_t>
+auto dom::nodes::character_data::to_v8(v8::Isolate* isolate) -> v8pp::class_<self_t>
 {
     decltype(auto) conversion = v8pp::class_<character_data>{isolate}
         .inherit<node>()
