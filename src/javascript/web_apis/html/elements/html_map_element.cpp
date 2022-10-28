8a246cb881932910ee92b01d61d2cbecc617948a
Sam <samuelgardner101@gmail.com>
2022-10-28 02:17:01 +0100

    Adjusted for PIMPL (huge class design overhaul)

--- src/javascript/web_apis/html/elements/html_map_element.cpp
+++ src/javascript/web_apis/html/elements/html_map_element.cpp
@@ -1,1 +1,32 @@
 #include "html_map_element.hpp"
+#include "html_map_element_private.hpp"
+
+#include "dom/detail/customization_internals.hpp"
+#include "dom/detail/tree_internals.hpp"
+
+#include "html/elements/html_area_element.hpp"
+
+
+auto html::elements::html_map_element::get_name() const -> ext::string_view
+{
+    CE_REACTIONS_METHOD_DEF
+        ACCESS_PIMPL(const html_map_element);
+        return d->name;
+    CE_REACTIONS_METHOD_EXE
+}
+
+
+auto html::elements::html_map_element::get_areas() const -> ranges::any_helpful_view<dom::nodes::element*>
+{
+    ACCESS_PIMPL(const html_map_element);
+    return dom::detail::descendants(this) | ranges::views::cast_all_to<html_area_element*>();
+}
+
+
+auto html::elements::html_map_element::set_name(ext::string new_name) -> ext::string
+{
+    CE_REACTIONS_METHOD_DEF
+        ACCESS_PIMPL(html_map_element);
+        return d->name = std::move(new_name);
+    CE_REACTIONS_METHOD_EXE
+}
--- src/javascript/web_apis/html/elements/html_map_element.hpp
+++ src/javascript/web_apis/html/elements/html_map_element.hpp
@@ -4,6 +4,9 @@
 
 #include "html/elements/html_element.hpp"
 namespace html::elements {class html_map_element;}
+namespace html::elements {class html_map_element_private;}
+
+#include "ext/ranges.hpp"
 
 
 class html::elements::html_map_element
@@ -11,14 +14,14 @@ class html::elements::html_map_element
 {
 public constructors:
     DOM_CTORS(html_map_element);
-    html_map_element();
+    MAKE_PIMPL(html_map_element);
+    MAKE_V8_AVAILABLE;
 
 private js_properties:
-    ext::property<ext::string> name;
-    ext::property<std::unique_ptr<ext::vector<dom::nodes::element*>>> areas;
+    DEFINE_GETTER(name, ext::string_view);
+    DEFINE_GETTER(areas, ranges::any_helpful_view<dom::nodes::element*>);
 
-public cpp_methods:
-    auto to_v8(v8::Isolate* isolate) const && -> ext::any override;
+    DEFINE_SETTER(name, ext::string);
 };
 
 
