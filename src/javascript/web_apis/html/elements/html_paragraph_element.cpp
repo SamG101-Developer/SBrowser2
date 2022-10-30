2616ee721c533a6ce495a7942b291bdc22e77705
Sam <samuelgardner101@gmail.com>
2022-10-30 02:16:38 +0000

    Adjusted for PIMPL (huge class design overhaul)

--- src/javascript/web_apis/html/elements/html_paragraph_element.cpp
+++ src/javascript/web_apis/html/elements/html_paragraph_element.cpp
@@ -1,4 +1,5 @@
 #include "html_paragraph_element.hpp"
+#include "html_paragraph_element_private.hpp"
 
 
 html::elements::html_paragraph_element::html_paragraph_element()
@@ -7,12 +8,12 @@ html::elements::html_paragraph_element::html_paragraph_element()
 }
 
 
-auto html::elements::html_paragraph_element::to_v8(
-        v8::Isolate* isolate)
-        const&& -> ext::any
+auto html::elements::html_paragraph_element::to_v8(v8::Isolate* isolate) -> v8pp::class_<self_t>
 {
-    return v8pp::class_<html_paragraph_element>{isolate}
+    decltype(auto) conversion = v8pp::class_<html_paragraph_element>{isolate}
         .ctor<>()
         .inherit<html_element>()
         .auto_wrap_objects();
+
+    return std::move(conversion);
 }
