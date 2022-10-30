9ece1076cd09b7c47007cf4a311438eb9d7f6230
Sam <samuelgardner101@gmail.com>
2022-10-30 14:21:42 +0000

    Use new lambda anonymous-functor pattern

--- src/ext/pair.hpp
+++ src/ext/pair.hpp
@@ -7,31 +7,29 @@
 
 
 _EXT_BEGIN
-
-template <typename K, typename V>
-constexpr auto make_pair(K&& key, V&& val)
-{return _EXT pair<std::remove_cvref_t<K>, std::remove_cvref_t<V>>{std::forward<K>(key), std::forward<V>(val)};}
-
-
-auto pair_key_matches = []<typename T, typename U>(const _EXT pair<T, U>& pair, T&& key) -> ext::boolean
-{return pair.first == std::forward<T>(key);};
-
-
-auto pair_val_matches = []<typename T, typename U>(const _EXT pair<T, U>& pair, U&& val) -> ext::boolean
-{return pair.second == std::forward<U>(val);};
-
-
-auto get_pair_key = []<typename T, typename U>(const _EXT pair<T, U>& pair) -> decltype(pair.first)
-{return pair.first;};
-
-
-auto get_pair_value = []<typename T, typename U>(const _EXT pair<T, U>& pair) -> decltype(pair.second)
-{return pair.second;};
-
-
-auto identity_pair = []<typename T, typename U>(const _EXT pair<T, U>& pair) -> ext::boolean
-{return pair.first == pair.second;};
-
+    template <typename K, typename V>
+    constexpr auto make_pair(K&& key, V&& val)
+    {return _EXT pair<std::remove_cvref_t<K>, std::remove_cvref_t<V>>{std::forward<K>(key), std::forward<V>(val)};}
+    
+    auto pair_key_matches =
+            []<typename K, typename V>(const _EXT pair<K, V>& pair, K&& key) -> ext::boolean
+            {return pair.first == std::forward<K>(key);};
+    
+    auto pair_val_matches =
+            []<typename K, typename V>(const _EXT pair<K, V>& pair, V&& val) -> ext::boolean
+            {return pair.second == std::forward<V>(val);};
+    
+    auto get_pair_key =
+            []<typename K, typename V>(const _EXT pair<K, V>& pair) -> decltype(pair.first)
+            {return pair.first;};
+    
+    auto get_pair_value =
+            []<typename K, typename V>(const _EXT pair<K, V>& pair) -> decltype(pair.second)
+            {return pair.second;};
+    
+    auto identity_pair =
+            []<typename K, typename V>(const _EXT pair<K, V>& pair) -> ext::boolean
+            {return pair.first == pair.second;};
 _EXT_END
 
 
