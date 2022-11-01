e8cce9b112f811555864a922570f1424325095b5
Sam <samuelgardner101@gmail.com>
2022-11-01 13:55:45 +0000

    Adjusted for PIMPL (huge class design overhaul)

--- src/javascript/web_apis/html/elements/html_unknown_element.cpp
+++ src/javascript/web_apis/html/elements/html_unknown_element.cpp
@@ -1,1 +1,9 @@
 #include "html_unknown_element.hpp"
+#include "html_unknown_element_private.hpp"
+
+
+html::elements::html_unknown_element::html_unknown_element()
+{
+    INIT_PIMPL(html_unknown_element);
+    // NOTE : No HTML_CONSTRUCTOR
+}
--- src/javascript/web_apis/html/elements/html_unknown_element.hpp
+++ src/javascript/web_apis/html/elements/html_unknown_element.hpp
@@ -3,11 +3,17 @@
 
 #include "html/elements/html_element.hpp"
 namespace html::elements {class html_unknown_element;}
+namespace html::elements {class html_unknown_element_private;}
 
 
 class html::elements::html_unknown_element
         : public html_element
-{};
+{
+public constructors:
+    DOM_CTORS(html_unknown_element);
+    MAKE_PIMPL(html_unknown_element);
+    MAKE_V8_AVAILABLE;
+};
 
 
 #endif //SBROWSER2_HTML_UNKNOWN_ELEMENT_HPP
