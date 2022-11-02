3618d6bf8cceea2d08d04cb3d7ce793295e6da12
Sam <samuelgardner101@gmail.com>
2022-11-02 20:29:18 +0000

    Renamed MACRO

--- src/javascript/interop/annotations.hpp
+++ src/javascript/interop/annotations.hpp
@@ -10,7 +10,7 @@
 #define JS_EXCEPTION_MESSAGE _js_exception_handler.Message()->Get()
 #define JS_EXCEPTION_RETHROW _js_exception_handler.ReThrow()
 #define JS_EXCEPTION _js_exception_handler.Exception()
-#define JS_EXCEPTION_RETHROW_IF_THREW if (JS_EXCEPTION_HAS_THROWN) JS_EXCEPTION_RETHROW
+#define JS_EXCEPTION_RETHROW_IF_HAS_THROWN if (JS_EXCEPTION_HAS_THROWN) JS_EXCEPTION_RETHROW
 
 #define UNSCOPABLE v8::DontEnum // TODO -> link to JS @@unscopable
 
