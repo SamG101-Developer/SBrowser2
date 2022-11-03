6ec3cb3db7254181a18997030ce64de31ac3ba72
Sam <samuelgardner101@gmail.com>
2022-11-03 02:22:27 +0000

    Adjusted for PIMPL (huge class design overhaul)

--- src/javascript/web_apis/html/other/element_internals.cpp
+++ src/javascript/web_apis/html/other/element_internals.cpp
@@ -16,7 +16,7 @@ auto html::other::element_internals::get_shadow_root() const -> dom::nodes::shad
     return_if (!dom::detail::is_shadow_host(target)) nullptr;
 
     decltype(auto) shadow = target->d_func()->shadow_root.get();
-    return_if (!dom_cast<dom::nodes::shadow_root*>(shadow->d_func()->available_to_element_internals)) nullptr;
+    return_if (!shadow->d_func()->available_to_element_internals) nullptr;
 
     return shadow;
 }
