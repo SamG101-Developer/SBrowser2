c00fbf01dbbae7bbd39b50edf56ce7acd4ebfdfa
Sam <samuelgardner101@gmail.com>
2022-11-07 02:13:52 +0000

    Changed variant to tuple construction

--- src/ext/variadic.hpp
+++ src/ext/variadic.hpp
@@ -2,17 +2,16 @@
 #define SBROWSER2_SRC_EXT_VARIADIC_HPP
 
 #include "ext/tuple.hpp"
-#include "ext/variant.hpp"
 
 
 _EXT_BEGIN
     template <size_t N, typename ...Args>
     auto nth_variadic_value(Args&&... args)
-    {return ext::get<N>(ext::make_tuple<Args...>(std::forward<Args>(args)...));}
+    {return ext::get<N>(_EXT make_tuple<Args...>(std::forward<Args>(args)...));}
 
-    template <size_t Index, typename ...Types>
+    template <size_t Index, typename ...Types> // TODO : variant vs tuple declval() construction time comparison
     struct nth_variadic_type
-    {using type = decltype(_EXT get<Index>(_EXT variant<Types...>{}));};
+    {using type = decltype(_EXT get<Index>(std::declval<_EXT tuple<Types...>>()));};
 
     template <size_t Index, typename ...Types>
     using nth_variadic_type_t = typename nth_variadic_type<Index, Types...>::type;
