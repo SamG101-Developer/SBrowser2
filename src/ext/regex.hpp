3c71e7e2cadfe4b4e5e5709907854d81452e77dd
Sam <samuelgardner101@gmail.com>
2022-11-07 23:58:54 +0000

    Switch regex library to `re2`

--- src/ext/regex.hpp
+++ src/ext/regex.hpp
@@ -2,17 +2,14 @@
 #define SBROWSER2_SRC_EXT_REGEX_HPP
 
 #include "ext/keywords.hpp"
-#include <ctre.hpp>
+#include <re2/re2.h>
+#include <re2/stringpiece.h>
 
 
 _EXT_BEGIN
-    namespace regex = ctre;
+    namespace regex = re2;
+    using regex_utils = RE2;
 _EXT_END
-
-
-_EXT_LITERALS_BEGIN
-    using namespace ctre::literals;
-_EXT_LITERALS_END
 
 
 #endif //SBROWSER2_SRC_EXT_REGEX_HPP
