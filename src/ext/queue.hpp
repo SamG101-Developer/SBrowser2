038e46485e57672730e728d56fb506c58320d4a5
Sam <samuelgardner101@gmail.com>
2022-11-07 23:59:34 +0000

    Add a multi peek to peek at more than 1 element from a queue

--- src/ext/queue.hpp
+++ src/ext/queue.hpp
@@ -4,6 +4,7 @@
 
 #include "ext/keywords.hpp"
 #include "ext/allocator.hpp"
+#include "ext/vector.hpp"
 #include <plf/plf_queue.h>
 
 
@@ -13,9 +14,21 @@ _EXT_BEGIN
 _EXT_END
 
 
+_EXT_BEGIN
+    template <size_t N, typename T>
+    auto multi_peek(_EXT queue<T>& queue) -> _EXT vector<T>
+    {
+        auto v = _EXT vector<T>{};
+        for (auto x = 0; x < N; ++x)
+            v.template emplace_back(*(&queue.front() + x));
+        return v;
+    }
+_EXT_END
+
+
 _EXT_SECURE_BEGIN
     template <typename T>
-    using queue = _EXT queue<T, ext::secure::allocator<T>>;
+    using queue = _EXT queue<T, _EXT secure::allocator<T>>;
 _EXT_SECURE_END
 
 
