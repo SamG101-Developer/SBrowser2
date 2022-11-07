dacb4d8d81c2c3a0bb20fa948ff75266d1fa353c
Sam <samuelgardner101@gmail.com>
2022-11-07 23:57:58 +0000

    Created a struct that extracts sub-ranges of parameter packs (as a tuple)

--- src/ext/variadic.hpp
+++ src/ext/variadic.hpp
@@ -2,14 +2,30 @@
 #define SBROWSER2_SRC_EXT_VARIADIC_HPP
 
 #include "ext/tuple.hpp"
+#include <utility>
 
 
 _EXT_BEGIN
+    // Get the single Nth element from a variadic argument
     template <size_t N, typename ...Args>
     auto nth_variadic_value(Args&&... args)
     {return ext::get<N>(_EXT make_tuple<Args...>(std::forward<Args>(args)...));}
 
-    template <size_t Index, typename ...Types> // TODO : variant vs tuple declval() construction time comparison
+    // Get the Nth elements (I amount) from a variadic argument
+    template <signed Adjust, std::size_t... I, typename... T>
+    auto nth_variadic_values_(std::index_sequence<I...>, T&&... t)
+    {return std::make_tuple(std::get<I + Adjust>(std::make_tuple(std::forward<T>(t)...))...);}
+
+    template <signed Begin, signed Size, typename... T>
+    auto nth_variadic_values(T&&... t)
+    {return nth_variadic_values_<Begin>(std::make_index_sequence<(Size)>(), std::forward<T>(t)...);}
+
+    template <signed Begin, typename... T>
+    auto nth_variadic_values(T&&... t)
+    {return nth_variadic_values_<Begin>(std::make_index_sequence<(sizeof...(T) - Begin)>(), std::forward<T>(t)...);}
+
+    // Get the single Nth type from a variadic argument
+    template <size_t Index, typename ...Types>
     struct nth_variadic_type
     {using type = decltype(_EXT get<Index>(std::declval<_EXT tuple<Types...>>()));};
 
