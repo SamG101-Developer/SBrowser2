514803a6afedc86c238a3d1044e33de69de653a1
Sam <samuelgardner101@gmail.com>
2022-11-08 00:02:19 +0000

    Fixed types

--- src/javascript/web_apis/encoding/_typedefs.hpp
+++ src/javascript/web_apis/encoding/_typedefs.hpp
@@ -14,14 +14,16 @@ namespace encoding::detail
     struct encoder_decoder_t;
     struct encoder_t;
     struct decoder_t;
+    struct utf8_encoder_t;
+    struct utf8_decoder_t;
 
     enum class handle_state_t {FINISHED, ERROR, CONTINUE};
     enum class encoder_error_mode_t {FATAL, HTML};
     enum class decoder_error_mode_t {REPLACEMENT, FATAL};
 
-    using item_t = char;
+    using item_t = char8_t;
     using io_queue_t = ext::queue<item_t>;
-    using handler_algorithm_t = ext::function<ext::tuple<handle_state_t, char>(io_queue_t& io_queue, item_t item)>;
+    using handler_algorithm_t = ext::function<ext::tuple<handle_state_t, ext::string>(io_queue_t& io_queue, item_t item)>;
     using text_decoder_options_t = ext::map<ext::string, ext::any>;
     using text_decode_options_t = ext::map<ext::string, ext::any>;
     using text_encoder_encoder_into_result_t = ext::map<ext::string, ext::any>;
