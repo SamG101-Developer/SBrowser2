cbb465c2e0c2b637bf4fd5528e9d489cadc66e07
Sam <samuelgardner101@gmail.com>
2022-11-09 23:42:41 +0000

    Implemented

--- src/javascript/web_apis/mediacapture_transform/video_track_generator.cpp
+++ src/javascript/web_apis/mediacapture_transform/video_track_generator.cpp
@@ -1,11 +1,18 @@
 #include "video_track_generator.hpp"
+#include "video_track_generator_private.hpp"
 
 #include "mediacapture_main/media_stream_track.hpp"
+#include "mediacapture_main/media_stream_track_private.hpp"
+#include "streams/writable/writable_stream.hpp"
 
 
 mediacapture::transform::video_track_generator::video_track_generator()
-        : track{std::make_unique<main::media_stream_track>()}
-        , writeable{std::make_unique<streams::writable::writable_stream>()}
 {
-    streams::detail::setup_writable(writeable(), )
+    INIT_PIMPL(video_track_generator);
+
+    ACCESS_PIMPL(video_track_generator);
+    d->writable = std::make_unique<streams::writable::writable_stream>();
+    d->track = std::make_unique<main::media_stream_track>();
+    d->track->d_func()->source = this;
+    d->track->d_func()->tie_source_to_context = false;
 }
