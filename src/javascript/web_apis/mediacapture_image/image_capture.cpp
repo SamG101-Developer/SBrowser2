2f2c82458d2de693380a4da8e2a7725fc4456ee7
Sam <samuelgardner101@gmail.com>
2022-11-09 23:47:18 +0000

    Adjusted for PIMPL (huge class design overhaul)

--- src/javascript/web_apis/mediacapture_image/image_capture.cpp
+++ src/javascript/web_apis/mediacapture_image/image_capture.cpp
@@ -1,16 +1,23 @@
 #include "image_capture.hpp"
+#include "image_capture_private.hpp"
 
-#include INCLUDE_INNER_TYPES(dom)
-
+#include "dom/_typedefs.hpp"
 #include "dom/detail/exception_internals.hpp"
+
 #include "mediacapture_main/media_stream_track.hpp"
+#include "mediacapture_main/media_stream_track_private.hpp"
 
 
 mediacapture::image::image_capture::image_capture(
         main::media_stream_track* video_track)
-        : track{std::unique_ptr<main::media_stream_track>(video_track)}
 {
+    INIT_PIMPL(image_capture);
+    using enum dom::detail::dom_exception_error_t;
+
+    ACCESS_PIMPL(image_capture);
+    d->track = video_track;
+
     dom::detail::throw_v8_exception<NOT_SUPPORTED_ERR>(
-            [track = track()] {return track->kind() != "video";},
-            "The 'track' property must have a 'kind' attribute set to \"video\"");
+            [d] {return d->track->d_func()->kind != u"video";},
+            u8"The 'track' property must have a 'kind' attribute set to \"video\"");
 }
