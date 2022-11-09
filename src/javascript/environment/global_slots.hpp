b94d11accceb7849d4abb8f734a1dd59bf735b5f
Sam <samuelgardner101@gmail.com>
2022-11-09 03:09:36 +0000

    Add fixed slot names for access in global object

---
+++ src/javascript/environment/global_slots.hpp
@@ -0,0 +1,15 @@
+#ifndef SBROWSER2_SRC_JAVASCRIPT_ENVIRONMENT_GLOBAL_SLOTS_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_ENVIRONMENT_GLOBAL_SLOTS_HPP
+
+namespace js::global_slots
+{
+    constexpr auto mutation_observers = 0;
+    constexpr auto mutation_observer_microtask_queued = 1;
+    constexpr auto notify_observers = 2;
+    constexpr auto signal_slots = 3;
+    constexpr auto settings = 4;
+    constexpr auto live_ranges = 5;
+    constexpr auto ce_reactions = 6;
+}
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_ENVIRONMENT_GLOBAL_SLOTS_HPP
