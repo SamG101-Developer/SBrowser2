f040dd139ad61244498fa0d7076813627a0ea572
Sam <samuelgardner101@gmail.com>
2022-11-09 03:06:53 +0000

    Implementation

--- src/javascript/web_apis/device_posture/device_posture.cpp
+++ src/javascript/web_apis/device_posture/device_posture.cpp
@@ -2,10 +2,25 @@
 #include "device_posture_private.hpp"
 
 
+device_posture::device_posture::device_posture()
+{
+    INIT_PIMPL(device_posture);
+}
+
+
+auto device_posture::device_posture::get_type() const -> detail::device_posture_type_t
+{
+    ACCESS_PIMPL(const device_posture);
+    return d->current_posture;
+}
+
+
 auto device_posture::device_posture::to_v8(v8::Isolate* isolate) -> v8pp::class_<self_t>
 {
     decltype(auto) conversion = v8pp::class_<device_posture>{isolate}
         .inherit<dom_object>()
         .property("type", &device_posture::get_type)
         .auto_wrap_objects();
+
+    return std::move(conversion);
 }
