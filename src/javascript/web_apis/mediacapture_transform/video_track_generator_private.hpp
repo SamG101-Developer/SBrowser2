0112f2c32cc766ac0659900056b95f87975af6a2
Sam <samuelgardner101@gmail.com>
2022-11-09 23:48:29 +0000

    Add to project (accidentally excluded from Git)

---
+++ src/javascript/web_apis/mediacapture_transform/video_track_generator_private.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_MEDIACAPTURE_TRANSFORM_VIDEO_TRACK_GENERATOR_PRIVATE_HPP
+#define SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_MEDIACAPTURE_TRANSFORM_VIDEO_TRACK_GENERATOR_PRIVATE_HPP
+
+#include "ext/pimpl.hpp"
+#include "dom_object_private.hpp"
+
+#include "ext/boolean.hpp"
+namespace mediacapture::main {class media_stream_track;}
+namespace streams::writable {class writable_stream;}
+
+
+DEFINE_PRIVATE_CLASS(mediacapture::transform, video_track_generator) : virtual dom_object_private
+{
+    std::unique_ptr<streams::writable::writable_stream> writable;
+    std::unique_ptr<main::media_stream_track> track;
+    ext::boolean is_muted;
+};
+
+
+#endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_MEDIACAPTURE_TRANSFORM_VIDEO_TRACK_GENERATOR_PRIVATE_HPP
