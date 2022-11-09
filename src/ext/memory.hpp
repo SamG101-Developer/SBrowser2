93a9f73784b078764ccbd6eb509dca5498691ec5
Sam <samuelgardner101@gmail.com>
2022-11-09 03:03:53 +0000

    Prevent copying and moving -> force `.get()`

--- src/ext/memory.hpp
+++ src/ext/memory.hpp
@@ -25,11 +25,17 @@ public:
     constexpr observer_ptr(nullptr_t) noexcept;
     explicit observer_ptr(pointer) noexcept;
     template <class U> explicit observer_ptr(U*) noexcept;
-    template <class U> observer_ptr(const observer_ptr<U>&) noexcept;
+    template <class U> observer_ptr(const observer_ptr<U>&) noexcept = delete; // Use .get()
+    template <class U> observer_ptr(observer_ptr<U>&&) noexcept = delete; // Use .get()
+    observer_ptr(const observer_ptr&) noexcept = delete; // Use .get()
+    observer_ptr(observer_ptr&&) noexcept = default; // Use .get()
 
     observer_ptr& operator=(nullptr_t) noexcept;
     template <class U> observer_ptr& operator=(U* other) noexcept;
-    template <class U> observer_ptr& operator=(const observer_ptr<U>&) noexcept;
+    template <class U> observer_ptr& operator=(const observer_ptr<U>&) noexcept = delete; // Use .get()
+    template <class U> observer_ptr& operator=(observer_ptr<U>&&) noexcept = delete; // Use .get()
+    observer_ptr& operator=(const observer_ptr&) noexcept = delete; // Use .get()
+    observer_ptr& operator=(observer_ptr&&) noexcept = default; // Use .get()
 
     pointer get() const noexcept;
     reference operator*() const noexcept;
@@ -42,10 +48,6 @@ public:
     pointer release() noexcept;
     void reset(pointer t = nullptr) noexcept;
     void swap(observer_ptr&) noexcept;
-
-    // Additions
-    template <typename U> auto operator==(std::observer_ptr<U>) -> ext::boolean;
-    template <typename U> auto operator==(U*) -> ext::boolean;
 
 private:
     T* raw;
