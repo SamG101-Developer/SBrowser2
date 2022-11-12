d96cde2a27156249a3d51ad74ea2221def696fee
Sam <samuelgardner101@gmail.com>
2022-11-12 23:39:28 +0000

    Fixed parameter- and return-types

--- src/javascript/web_apis/fetch/detail/connection_internals.hpp
+++ src/javascript/web_apis/fetch/detail/connection_internals.hpp
@@ -1,70 +1,92 @@
 #ifndef SBROWSER2_CONNECTION_INTERNALS_HPP
 #define SBROWSER2_CONNECTION_INTERNALS_HPP
 
-
-#include "ext/string.hpp"
-#include "ext/pair.hpp"
 #include "ext/boolean.hpp"
+#include "ext/expected.hpp"
+#include "ext/pair.hpp"
 #include "ext/map.hpp"
+#include "ext/string.hpp"
 
 #include INCLUDE_INNER_TYPES(fetch)
 #include INCLUDE_INNER_TYPES(hr_time)
+#include INCLUDE_INNER_TYPES(html)
 #include INCLUDE_INNER_TYPES(url)
+
+namespace js::env {class settings_t;}
 
 
 namespace fetch::detail
 {
+    auto origin_caches = ext::map<ext::string, ext::vector<ext::string>>{};
+
+    auto is_ip(
+            ext::string_view host)
+            -> ext::boolean;
+
     auto resolve_origin(
-            const network_partition_key_t&,
-            ext::string_view origin)
-            -> ext::string;
+            const network_partition_key_t& key,
+            const html::detail::origin_t& origin)
+            -> ext::expected<ext::vector<ext::string>>;
 
     auto clamp_and_coarsen_connection_timing_information(
             const connection_timing_info_t& timing_information,
-            const hr_time::dom_high_res_time_stamp& default_start_time,
+            hr_time::dom_high_res_time_stamp default_start_time,
             ext::boolean cross_origin_isolated_capability)
-            -> connection_timing_info_t;
+            -> std::unique_ptr<connection_timing_info_t>;
 
     auto obtain_connection(
             const network_partition_key_t& key,
             const url::detail::url_t& url,
-            ext::boolean  credentials,
-            net_connection_setting_t new_ = net_connection_setting_t::NO,
-            ext::boolean  http3_only = false)
-            -> connection_t;
+            ext::boolean credentials,
+            new_connection_setting_t new_ = new_connection_setting_t::NO,
+            ext::boolean http3_only = false)
+            -> ext::expected<std::unique_ptr<connection_t>>;
 
     auto create_connection(
             const network_partition_key_t& key,
-            ext::string_view origin,
+            const html::detail::origin_t& origin,
+            ext::boolean credentials,
             ext::string_view proxy,
+            ext::string_view host,
             const connection_timing_info_t& timing_info,
             ext::boolean http3_only = false)
-            -> connection_t;
+            -> ext::expected<std::unique_ptr<connection_t>>;
 
     auto record_timing_information(
             const connection_t& connection)
             -> void;
 
     auto determine_network_partition_key(
-            v8::Local<v8::Object> environment)
+            js::env::settings_t* environment_settings)
             -> network_partition_key_t;
 
     auto determine_network_partition_key(
-            const request_t& request_object)
+            const request_t& request)
             -> network_partition_key_t;
 
     auto determine_http_cache_partition(
-            const request_t& request_object)
+            const request_t& request)
             -> ext::string;
 
     auto should_block_due_to_bad_port(
-            const request_t& request_obj)
+            const request_t& request)
             -> ext::boolean;
 
     auto should_block_due_to_mime_type(
-            const request_t& request_obj)
+            const request_t& request)
             -> ext::boolean;
 }
+
+
+struct fetch::detail::connection_timing_info_t
+{
+    hr_time::dom_high_res_time_stamp domain_lookup_start_time = 0;
+    hr_time::dom_high_res_time_stamp domain_lookup_end_time = 0;
+    hr_time::dom_high_res_time_stamp connection_start_time = 0;
+    hr_time::dom_high_res_time_stamp connection_end_time = 0;
+    hr_time::dom_high_res_time_stamp secure_connection_start_time = 0;
+    ext::u8string alpn_negotiated_protocol;
+};
 
 
 struct fetch::detail::connection_t
@@ -73,7 +95,7 @@ struct fetch::detail::connection_t
     ext::string origin;
     ext::boolean credentials;
 
-    connection_timing_info_t& timing_info;
+    std::unique_ptr<connection_timing_info_t> timing_info;
 };
 
 
--- src/javascript/web_apis/fetch/detail/fetch_internals.hpp
+++ src/javascript/web_apis/fetch/detail/fetch_internals.hpp
@@ -17,7 +17,7 @@
 namespace fetch::detail
 {
     auto fetch(
-            request_t& request_object,
+            request_t& request,
             ext::function<void(ext::number<int>)>&& process_request_body_chunk_length = nullptr,
             ext::function<void()>&& process_request_end_of_body = nullptr,
             ext::function<void(response_t&)>&& process_early_hints_response = nullptr,
@@ -25,12 +25,12 @@ namespace fetch::detail
             ext::function<void(response_t&)>&& process_response_end_of_body = nullptr,
             ext::function<void(response_t&, ext::string_view)>&& process_response_consume_body = nullptr,
             ext::boolean  use_parallel_queue = false)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto main_fetch(
             fetch_params_t& fetch_params_object,
             ext::boolean recursive = false)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto fetch_response_handover(
             fetch_params_t& fetch_params_object,
@@ -39,45 +39,45 @@ namespace fetch::detail
 
     auto scheme_fetch(
             fetch_params_t& fetch_params_object)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto http_fetch(
             fetch_params_t& fetch_params_object,
             ext::boolean make_cors_preflight = false)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto http_redirect_fetch(
             fetch_params_t& fetch_params_object,
             response_t& response_object)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto http_navigate_redirect_fetch(
             request_t& request_object,
             response_t& response_object)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto http_network_or_cache_fetch(
             fetch_params_t& fetch_params_object,
             ext::boolean is_authentication_fetch = false,
             ext::boolean is_new_connection_fetch = false)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto http_network_fetch(
             fetch_params_t& fetch_params_object,
             ext::boolean include_credentials = false,
             ext::boolean force_new_connection = false)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto cors_preflight_request(
             request_t& request_object)
-            -> response_t;
+            -> std::unique_ptr<response_t>;
 
     auto create_a_new_cache_entry(
             request_t& request_object,
             ext::number<int> max_age,
             ext::string_view method,
             header_name_t header_name)
-            -> cache_entry_t;
+            -> std::unique_ptr<cache_entry_t>;
 
     auto cors_check(
             request_t& request_object,
@@ -98,7 +98,7 @@ struct fetch::detail::cache_entry_t
 {
     network_partition_key_t key;
     ext::string byte_serialized_origin;
-    url::detail::url_t url;
+    std::unique_ptr<url::detail::url_t> url;
     ext::number<int> mag_age;
     ext::boolean credentials;
     ext::string method;
