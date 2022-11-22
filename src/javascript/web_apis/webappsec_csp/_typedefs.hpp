00d64cc56c34bb969326a23ab793c4b9aa8126ba
Sam <samuelgardner101@gmail.com>
2022-11-22 01:11:12 +0000

    Moved struct declarations into correct `detail` files

--- src/javascript/web_apis/webappsec_csp/_typedefs.hpp
+++ src/javascript/web_apis/webappsec_csp/_typedefs.hpp
@@ -12,41 +12,17 @@ namespace dom::nodes {class element;}
 
 namespace webappsec::detail
 {
-    struct content_security_policy_t;
+    struct policy_t;
     struct violation_t;
+    struct directive_value_t;
 
     enum class disposition_t {ENFORCE, REPORT};
     enum class source_t {HEADER, META};
+    enum class violation_resource_t {INLINE, EVAL, WASM_EVAL};
+    enum class security_policy_violation_event_disposition_t {ENFORCE, REPORT};
 
-    using directive_t = ext::pair<ext::string, ext::vector<ext::string>>;
+    using directive_t = ext::pair<ext::string, std::unique_ptr<directive_value_t>>;
 }
-
-
-struct webappsec::detail::content_security_policy_t
-{
-    ext::vector<directive_t*> directive_set;
-    disposition_t disposition;
-    source_t source;
-    ext::string self_origin;
-};
-
-
-struct webappsec::detail::violation_t
-{
-    v8::Local<v8::Object> global_object;
-    url::detail::url_t url;
-    ext::number<int> status;
-    ext::string resource;
-    ext::string referrer;
-    const content_security_policy_t& csp;
-    disposition_t disposition;
-    ext::string effective_directive;
-    url::detail::url_t source_file;
-    ext::number<int> line_number;
-    ext::number<int> column_number;
-    dom::nodes::element* element;
-    ext::string sample;
-};
 
 
 #endif //SBROWSER2_SRC_JAVASCRIPT_WEB_APIS_CONTENT_SECURITY_POLICY__TYPEDEFS_HPP
