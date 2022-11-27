92cac9c9fee6e115705a0f3ece927b5e5b24ec5c
Sam <samuelgardner101@gmail.com>
2022-11-27 14:21:40 +0000

    Added typedef for frozen arrays

--- src/ext/vector.hpp
+++ src/ext/vector.hpp
@@ -4,18 +4,23 @@
 
 #include "ext/allocator.hpp"
 #include <veque.hpp>
-#include <cryptopp/secblock.h>
 
 
 _EXT_BEGIN
-    template <typename T, typename Allocator = std::allocator<T>>
-    using vector = veque::veque<T, veque::fast_resize_traits, Allocator>;
+    template <typename T>
+    using vector = veque::veque<T, veque::fast_resize_traits, _EXT allocator<T>>;
+
+    template <typename T>
+    using frozen_vector = const vector<T>;
 _EXT_END
 
 
 _EXT_SECURE_BEGIN
     template <typename T>
-    using vector = vector<T, _EXT secure::allocator<T>>;
+    using vector = veque::veque<T, veque::fast_resize_traits, _EXT secure::allocator<T>>;
+
+    template <typename T>
+    using frozen_vector = const vector<T>;
 _EXT_SECURE_END
 
 
