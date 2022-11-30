9dacb26ec9ec1a5b8b3be628446cc953cfcdc9dc
Sam <samuelgardner101@gmail.com>
2022-11-30 02:10:03 +0000

    Use an improved `initializer_list<Ts...>` to allow movables. From GitHub (better_braces).

--- src/ext/initializer_list.hpp
+++ src/ext/initializer_list.hpp
@@ -1,19 +1,20 @@
 #ifndef SBROWSER2_SRC_EXT_INITIALIZER_LIST_HPP
 #define SBROWSER2_SRC_EXT_INITIALIZER_LIST_HPP
 
-#include <initializer_list>
+#include <better_braces.hpp>
 #include "ext/variadic.hpp"
 
 
 _EXT_BEGIN
+    template <typename ...Ts>
+    using initializer_list = better_braces::init<Ts...>;
+
     template <typename T>
-    auto make_initializer_list_repeat(T&& argument, size_t repeat) -> std::initializer_list<T>;
+    auto make_initializer_list_repeat(T&& argument, size_t repeat) -> _EXT initializer_list<T>;
 
     template <typename ...Args>
-    auto make_initializer_list(Args&&... arguments) -> std::initializer_list<_EXT nth_variadic_type_t<0, Args...>>
+    auto make_initializer_list(Args&&... arguments) -> _EXT initializer_list<Args...>
     {return {std::forward<Args>(arguments)...};}
-
-    using std::initializer_list;
 _EXT_END
 
 
