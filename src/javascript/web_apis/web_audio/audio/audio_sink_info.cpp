722049ae136af50bf7ef44fae634beb497f01637
Sam <samuelgardner101@gmail.com>
2022-12-15 01:06:44 +0000

    Implement WebAudio API

--- src/javascript/web_apis/web_audio/audio/audio_sink_info.cpp
+++ src/javascript/web_apis/web_audio/audio/audio_sink_info.cpp
@@ -1,1 +1,40 @@
+module;
+#include "ext/macros/pimpl.hpp"
+#include "javascript/macros/expose.hpp"
+#include <v8-isolate.h>
+#include <v8pp/class.hpp>
+
+
 module apis.web_audio.audio_sink_info;
+import apis.web_audio.audio_sink_info_private;
+import apis.web_audio.types;
+
+import ext.tuple;
+import js.env.module_type;
+
+
+web_audio::audio_sink_info::audio_sink_info()
+{
+    INIT_PIMPL;
+}
+
+
+auto web_audio::audio_sink_info::get_type() const -> detail::audio_sink_type_t
+{
+    ACCESS_PIMPL;
+    return d->type;
+}
+
+
+auto web_audio::audio_sink_info::_to_v8(
+        js::env::module_t E,
+        v8::Isolate* isolate)
+        -> ext::tuple<bool, v8pp::class_<this_t>>
+{
+    V8_INTEROP_CREATE_JS_OBJECT
+        .inherit<dom_object>()
+        .property("type", &audio_sink_info::get_type)
+        .auto_wrap_objects();
+
+    return V8_INTEROP_SUCCESSFUL_CONVERSION;
+}
--- src/javascript/web_apis/web_audio/audio/audio_sink_info_private.ixx
+++ src/javascript/web_apis/web_audio/audio/audio_sink_info_private.ixx
@@ -4,6 +4,7 @@ module;
 
 export module apis.web_audio.audio_sink_info_private;
 import apis._.dom_object_private;
+import apis.web_audio.types;
 
 
 DEFINE_PRIVATE_CLASS(web_audio, audio_sink_info)
@@ -11,4 +12,7 @@ DEFINE_PRIVATE_CLASS(web_audio, audio_sink_info)
 {
 public:
     MAKE_QIMPL(audio_sink_info);
+
+public:
+    detail::audio_sink_type_t type;
 };
